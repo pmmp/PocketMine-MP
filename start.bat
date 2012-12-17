d515e44de9bcf21ce2a64350cd0d34a2f941c796
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2012-12-18 00:58:44 +0100

    Updated async I/O bat file

--- start.bat
+++ start.bat
@@ -17,13 +17,12 @@ echo        \    ^|    /
 echo           \ ^| /
 echo.
 echo.
+cd /d %~dp0
 FOR /F "tokens=*" %%i in ('php -r "echo 1;"') do SET PHPOUTPUT=%%i
 if not "%PHPOUTPUT%"=="1" (
-echo [ERROR] Couldn't find PHP in PATH.
-pause
-exit
-)
+echo [ERROR] Couldn't find PHP binary in PATH.
+ping 127.0.0.1 -n 3 -w 1000>nul
+) else (
 START /B CMD /C CALL php server.php
 START /B /WAIT php input.php 1
-pause
-exit
+)
