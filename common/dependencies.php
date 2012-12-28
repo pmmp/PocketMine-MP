2d9b70184748d56f6b7dc8fd79184312053d3502
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2012-12-28 01:02:22 +0100

    pthreads dependency

--- common/dependencies.php
+++ common/dependencies.php
@@ -54,6 +54,11 @@ if(!extension_loaded("sockets")){
 	++$errors;
 }
 
+if(!extension_loaded("pthreads")){
+	console("[ERROR] Unable to find pthreads extension. [https://github.com/krakjoe/pthreads]", true, true, 0);
+	++$errors;
+}
+
 if(!extension_loaded("curl")){
 	console("[ERROR] Unable to find cURL extension", true, true, 0);
 	++$errors;
