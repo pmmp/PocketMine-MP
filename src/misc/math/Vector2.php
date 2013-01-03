d7b1ae4b477ca02fdbe346181b5aa67e4817a1fb
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-01-03 23:39:57 +0100

    Vectors

---
+++ src/misc/math/Vector2.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+
+           -
+         /   \
+      /         \
+   /   PocketMine  \
+/          MP         \
+|\     @shoghicp     /|
+|.   \           /   .|
+| ..     \   /     .. |
+|    ..    |    ..    |
+|       .. | ..       |
+\          |          /
+   \       |       /
+      \    |    /
+         \ | /
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+
+*/
+
+class Vector2{
+	public $x, $y;
+	
+	public function __construct($x = 0, $y = 0){
+		if(($x instanceof Vector2) === true){
+			$this->__construct($x->x, $x->y);
+		}else{
+			$this->x = $x;
+			$this->y = $y;
+		}
+	}
+	
+	public function getX(){
+		return $this->x;
+	}
+	
+	public function getY(){
+		return $this->y;
+	}
+	
+	public function getFloorX(){
+		return (int) $this->x;
+	}
+	
+	public function getFloorY(){
+		return (int) $this->y;
+	}
+	
+	public function add($x = 0, $y = 0){
+		if(($x instanceof Vector2) === true){
+			return $this->add($x->x, $x->y);
+		}else{
+			$this->x += $x;
+			$this->y += $y;
+			return new Vector3($this->x + $x, $this->y + $y);
+		}
+	}
+	
+	public function subtract($x = 0, $y = 0){
+		if(($x instanceof Vector2) === true){
+			return $this->add(-$x->x, -$x->y);
+		}else{
+			return $this->add(-$x, -$y);
+		}
+	}
+	
+	public function ceil(){
+		return new Vector2((int) ($this->x + 1), (int) ($this->y + 1));
+	}
+	
+	public function floor(){
+		return new Vector2((int) $this->x, (int) $this->y);
+	}
+	
+	public function round(){
+		return new Vector2(round($this->x), round($this->y));
+	}
+	
+	public function abs(){
+		return new Vector2(abs($this->x), abs($this->y));
+	}
+	
+	public function distance($x = 0, $y = 0){
+		if(($x instanceof Vector2) === true){
+			return sqrt($this->distanceSquared($x->x, $x->y));
+		}else{
+			return sqrt($this->distanceSquared($x, $y));
+		}
+	}
+	
+	public function distanceSquared($x = 0, $y = 0){
+		if(($x instanceof Vector2) === true){
+			return $this->distanceSquared($x->x, $x->y);
+		}else{
+			return pow($this->x - $x, 2) + pow($this->y - $y, 2);
+		}
+	}
+	
+	public function __toString(){
+		return "Vector2(x=".$this->x.",y=".$this->y.")";
+	}
+	
+}
---
+++ src/misc/math/Vector3.php
@@ -0,0 +1,140 @@
+<?php
+
+/*
+
+           -
+         /   \
+      /         \
+   /   PocketMine  \
+/          MP         \
+|\     @shoghicp     /|
+|.   \           /   .|
+| ..     \   /     .. |
+|    ..    |    ..    |
+|       .. | ..       |
+\          |          /
+   \       |       /
+      \    |    /
+         \ | /
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+
+*/
+
+class Vector3{
+	public $x, $y, $z;
+	
+	public function __construct($x = 0, $y = 0, $z = 0){
+		if(($x instanceof Vector3) === true){
+			$this->__construct($x->x, $x->y, $x->z);
+		}else{
+			$this->x = $x;
+			$this->y = $y;
+			$this->z = $z;
+		}
+	}
+	
+	public function getX(){
+		return $this->x;
+	}
+	
+	public function getY(){
+		return $this->y;
+	}
+	
+	public function getZ(){
+		return $this->z;
+	}
+	
+	public function getFloorX(){
+		return (int) $this->x;
+	}
+	
+	public function getFloorY(){
+		return (int) $this->y;
+	}
+	
+	public function getFloorZ(){
+		return (int) $this->z;
+	}
+	
+	public function getRight(){
+		return $this->getX();
+	}
+	
+	public function getUp(){
+		return $this->getY();
+	}
+	
+	public function getForward(){
+		return $this->getZ();
+	}
+	
+	public function getSouth(){
+		return $this->getX();
+	}
+	
+	public function getWest(){
+		return $this->getZ();
+	}
+	
+	public function add($x = 0, $y = 0, $z = 0){
+		if(($x instanceof Vector3) === true){
+			return $this->add($x->x, $x->y, $x->z);
+		}else{
+			$this->x += $x;
+			$this->y += $y;
+			$this->z += $z;
+			return new Vector3($this->x + $x, $this->y + $y, $this->z + $z);
+		}
+	}
+	
+	public function subtract($x = 0, $y = 0, $z = 0){
+		if(($x instanceof Vector3) === true){
+			return $this->add(-$x->x, -$x->y, -$x->z);
+		}else{
+			return $this->add(-$x, -$y, -$z);
+		}
+	}
+	
+	public function ceil(){
+		return new Vector3((int) ($this->x + 1), (int) ($this->y + 1), (int) ($this->z + 1));
+	}
+	
+	public function floor(){
+		return new Vector3((int) $this->x, (int) $this->y, (int) $this->z);
+	}
+	
+	public function round(){
+		return new Vector3(round($this->x), round($this->y), round($this->z));
+	}
+	
+	public function abs(){
+		return new Vector3(abs($this->x), abs($this->y), abs($this->z));
+	}
+	
+	public function distance($x = 0, $y = 0, $z = 0){
+		if(($x instanceof Vector3) === true){
+			return sqrt($this->distanceSquared($x->x, $x->y, $x->z));
+		}else{
+			return sqrt($this->distanceSquared($x, $y, $z));
+		}
+	}
+	
+	public function distanceSquared($x = 0, $y = 0, $z = 0){
+		if(($x instanceof Vector3) === true){
+			return $this->distanceSquared($x->x, $x->y, $x->z);
+		}else{
+			return pow($this->x - $x, 2) + pow($this->y - $y, 2) + pow($this->z - $z, 2);
+		}
+	}
+	
+	public function __toString(){
+		return "Vector3(x=".$this->x.",y=".$this->y.",z=".$this->z.")";
+	}
+	
+}
