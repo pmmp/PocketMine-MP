3813ef73ea2b382f9aa8168b453d298f886f6a5f
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-01-14 07:24:18 +0100

    Updated start.cmd

--- start.cmd
+++ start.cmd
@@ -26,10 +26,18 @@ if not "%PHPOUTPUT%"=="1" (
 echo [ERROR] Couldn't find PHP binary in PATH.
 ping 127.0.0.1 -n 3 -w 1000>nul
 ) else (
-	if exist bin\ansicon.exe (
-		bin\ansicon.exe php -d enable_dl=On PocketMine-MP.php --enable-ansi
+	if exist php.cmd (
+		if exist bin\ansicon.exe (
+			bin\ansicon.exe php.cmd -d enable_dl=On PocketMine-MP.php --enable-ansi
+		) else (
+			php.cmd -d enable_dl=On PocketMine-MP.php
+		)
 	) else (
-		php -d enable_dl=On PocketMine-MP.php
-	)
+		if exist bin\ansicon.exe (
+			bin\ansicon.exe php -d enable_dl=On PocketMine-MP.php --enable-ansi
+		) else (
+			php -d enable_dl=On PocketMine-MP.php
+		)
+	)	
 )
 pause
