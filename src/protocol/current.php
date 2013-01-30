b697f1441e67a716efccf09277adf41278334494
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-01-30 16:29:00 +0100

    Protocol changes 4

--- src/protocol/current.php
+++ src/protocol/current.php
@@ -41,15 +41,14 @@ define("MC_READY", 0x84);
 define("MC_CHAT", 0x85);
 define("MC_SET_TIME", 0x86);
 define("MC_START_GAME", 0x87);
-
 define("MC_ADD_MOB", 0x88);
 define("MC_ADD_PLAYER", 0x89);
+//define("MC_REMOVE_PLAYER", 0x8a);
 
 define("MC_ADD_ENTITY", 0x8c);
 define("MC_REMOVE_ENTITY", 0x8d);
 define("MC_ADD_ITEM_ENTITY", 0x8e);
 define("MC_TAKE_ITEM_ENTITY", 0x8f);
-
 define("MC_MOVE_ENTITY", 0x90);
 
 define("MC_MOVE_ENTITY_POSROT", 0x93);
@@ -57,25 +56,23 @@ define("MC_MOVE_PLAYER", 0x94);
 define("MC_PLACE_BLOCK", 0x95);
 define("MC_REMOVE_BLOCK", 0x96);
 define("MC_UPDATE_BLOCK", 0x97);
-
+//define("MC_ADD_PAINTING", 0x98);
 define("MC_EXPLOSION", 0x99);
-
 define("MC_LEVEL_EVENT", 0x9a);
-
+//define("MC_TILE_EVENT", 0x9b);
 define("MC_ENTITY_EVENT", 0x9c);
 define("MC_REQUEST_CHUNK", 0x9d);
 define("MC_CHUNK_DATA", 0x9e);
-
 define("MC_PLAYER_EQUIPMENT", 0x9f);
-
-
 //define("MC_SET_ARMOR_SLOT", 0xa0);
 define("MC_INTERACT", 0xa1);
 define("MC_USE_ITEM", 0xa2);
 define("MC_PLAYER_ACTION", 0xa3);
 define("MC_SET_ENTITY_DATA", 0xa4);
 define("MC_SET_ENTITY_MOTION", 0xa5);
-
+//define("MC_UNKNOWN_A5", 0xa5);
+//define("MC_UNKNOWN_A6", 0xa6);
+//define("MC_UNKNOWN_A7", 0xa7);
 define("MC_SET_HEALTH", 0xa8);
 define("MC_SET_SPAWN_POSITION", 0xa9);
 define("MC_ANIMATE", 0xaa);
