084acfab005dddef020107b96df534d4c809787e
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-02-03 21:17:55 +0100

    Fence Gate Placement

--- src/classes/material/block/solid/FenceGate.php
+++ src/classes/material/block/solid/FenceGate.php
@@ -30,5 +30,28 @@ class FenceGateBlock extends TransparentBlock{
 		parent::__construct(FENCE_GATE, $meta, "Fence Gate");
 		$this->isActivable = true;
 	}
-	
+	public function place(BlockAPI $level, Item $item, Player $player, Block $block, Block $target, $face, $fx, $fy, $fz){
+		if($block->inWorld === true){
+			$faces = array(
+				0 => 3,
+				1 => 0,
+				2 => 1,
+				3 => 2,
+			);
+			$this->meta = $faces[$player->entity->getDirection()] & 0x03;
+			$level->setBlock($block, $this->id, $this->meta);
+			return true;
+		}
+		return false;
+	}
+	public function getDrops(Item $item, Player $player){
+		return array(
+			array($this->id, 0, 1),
+		);
+	}
+	public function onActivate(BlockAPI $level, Item $item, Player $player){
+		$this->meta ^= 0x04;
+		$level->setBlock($this, $this->id, $this->meta);
+		return true;
+	}	
 }
