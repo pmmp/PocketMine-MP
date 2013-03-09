a9f7d47c25c3fdebae448ab24435373ea48ed8ad
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-03-09 13:58:22 +0100

    Added all the future tags

--- src/Player.php
+++ src/Player.php
@@ -624,14 +624,38 @@ class Player{
 									$this->sendInventory();
 									$this->entity->setPosition($this->entity->x, $this->entity->y, $this->entity->z, 0, 0);
 									/*
-									0x01 world_inmutable
-									0x02 ?
-									0x04 ?
-									0x08 ?
-									0x10 ?
-									0x20 nametags_visible
-									0x40 ?
-									0x80 ?
+									0x00000001 world_inmutable
+									0x00000002 ?
+									0x00000004 ?
+									0x00000008 ?
+									0x00000010 ?
+									0x00000020 nametags_visible
+									0x00000040 ?
+									0x00000080 ?									
+									0x00000100 ?
+									0x00000200 ?
+									0x00000400 ?
+									0x00000800 ?
+									0x00001000 ?
+									0x00002000 ?
+									0x00004000 ?
+									0x00008000 ?									
+									0x00010000 ?
+									0x00020000 ?
+									0x00040000 ?
+									0x00080000 ?
+									0x00100000 ?
+									0x00200000 ?
+									0x00400000 ?
+									0x00800000 ?									
+									0x01000000 ?
+									0x02000000 ?
+									0x04000000 ?
+									0x08000000 ?
+									0x10000000 ?
+									0x20000000 ?
+									0x40000000 ?
+									0x80000000 ?
 									*/
 									$flags = 0;
 									if($this->gamemode === ADVENTURE){
