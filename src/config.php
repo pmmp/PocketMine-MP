01e31fc82e3f4fd20435320f49b64e07678e3e5b
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-04-08 22:59:34 +0200

    Updated test to reflect changes

--- src/config.php
+++ src/config.php
@@ -41,7 +41,7 @@ set_include_path(get_include_path() . PATH_SEPARATOR . FILE_PATH);
 
 ini_set("memory_limit", "256M"); //Default
 define("LOG", true);
-define("TEST_MD5", "ffe889db5932db1e3371d48773590e59");
+define("TEST_MD5", "1e0d28177b73dfd09f922502fe767bec");
 define("MAJOR_VERSION", "Alpha_1.3dev");
 define("CURRENT_STRUCTURE", 5);
 define("CURRENT_PROTOCOL", 9);
