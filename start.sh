88a4116398ad3fb12cb5290e7663da1430a3ed03
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-04-14 13:10:04 +0200

    Fixed #200 Running in a folder with spaces

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+cd "$DIR"
 if [ -f php5/bin/php ]; then
 ./php5/bin/php -d enable_dl=On PocketMine-MP.php $@
 read -p "Press [Enter] to continue..."
