be7e5cd67ad3a9849f39e316d7f27f678b016321
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-04-22 23:46:34 +0200

    Unknown packets are always discarded even if they have been processed

--- src/PocketMinecraftServer.php
+++ src/PocketMinecraftServer.php
@@ -28,7 +28,7 @@ the Free Software Foundation, either version 3 of the License, or
 class PocketMinecraftServer{
 	public $tCnt;
 	public $version, $invisible, $api, $tickMeasure, $preparedSQL, $seed, $gamemode, $name, $maxClients, $clients, $eidCnt, $custom, $description, $motd, $timePerSecond, $spawn, $entities, $mapDir, $mapName, $map, $levelData, $tileEntities;
-	private $serverip, $database, $interface, $evCnt, $handCnt, $events, $eventsID, $handlers, $serverType, $lastTick;
+	private $port, $serverip, $database, $interface, $evCnt, $handCnt, $events, $eventsID, $handlers, $serverType, $lastTick;
 	
 	private function load(){
 		$this->version = new VersionString();
