bbbc54f606c9607ace3ab03efd503cb3ae1433dc
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-04-27 12:45:30 +0200

    New API version

--- src/config.php
+++ src/config.php
@@ -47,7 +47,7 @@ define("MAJOR_VERSION", "Alpha_1.3dev");
 define("CURRENT_STRUCTURE", 5);
 define("CURRENT_PROTOCOL", 9);
 define("CURRENT_MINECRAFT_VERSION", "0.6.1 alpha");
-define("CURRENT_API_VERSION", 6);
+define("CURRENT_API_VERSION", 7);
 define("CURRENT_PHP_VERSION", "5.5");
 $gitsha1 = false;
 if(file_exists(FILE_PATH.".git/refs/heads/master")){ //Found Git information!
