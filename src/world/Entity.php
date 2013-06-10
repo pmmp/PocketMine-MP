1e1448544441730447147a2e05d69b22085ce19e
Shoghi Cervantes <shoghicp@gmail.com>
2013-06-10 15:47:53 +0200

    Increased support Y radius

--- src/world/Entity.php
+++ src/world/Entity.php
@@ -776,7 +776,7 @@ class Entity extends Position{
 	public function isSupport(Vector3 $pos, $radius = 1){
 		$me = new Vector3($this->x - 0.5, $pos->y, $this->z - 0.5);
 		$diff = $this->y - $pos->y;
-		if($me->distance($pos) < $radius and $diff > 0 and $diff < 1.2){
+		if($me->distance($pos) < $radius and $diff > 0 and $diff < 1.6){
 			return true;
 		}
 		return false;
