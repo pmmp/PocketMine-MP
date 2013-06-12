9619ebc2af3ddddfdfbff243c94e12fab0d6565f
Shoghi Cervantes <shoghicp@gmail.com>
2013-06-12 12:13:51 +0200

    Fixed lag issues when breaking blocks

--- src/API/BlockAPI.php
+++ src/API/BlockAPI.php
@@ -260,7 +260,7 @@ class BlockAPI{
 			return $this->cancelAction($target, $player, false);
 		}
 		
-		if((!$target->isBreakable($item, $player) and $this->server->api->dhandle("player.block.break.invalid", array("player" => $player, "target" => $target, "item" => $item)) !== true) or ($player->gamemode & 0x02) === 0x02 or (($player->lastBreak - 0.05) + $target->getBreakTime($item, $player)) >= microtime(true)){
+		if((!$target->isBreakable($item, $player) and $this->server->api->dhandle("player.block.break.invalid", array("player" => $player, "target" => $target, "item" => $item)) !== true) or ($player->gamemode & 0x02) === 0x02 or (($player->lastBreak - $player->getLag() / 1000) + $target->getBreakTime($item, $player) - 0.1) >= microtime(true)){
 			return $this->cancelAction($target, $player, false);
 		}
 		$player->lastBreak = microtime(true);
