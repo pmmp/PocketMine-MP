03b4e40bf43c3d6e969cbad63b86aae9cd073372
Shoghi Cervantes <shoghicp@gmail.com>
2013-07-03 18:12:22 +0200

    Ooops!

--- src/network/Query.php
+++ src/network/Query.php
@@ -77,8 +77,8 @@ class Query{
 			"map" => $this->server->api->level->getDefault()->getName(),
 			"numplayers" => count($this->server->clients),
 			"maxplayers" => $this->server->maxClients,
-			"whitelist" => $this->server->api->getProperty("server-port"),
-			"hostport" => $this->server->api->getProperty("white-list") === true ? "on":"off",
+			"whitelist" => $this->server->api->getProperty("white-list") === true ? "on":"off",
+			"hostport" => $this->server->api->getProperty("server-port"),
 			//"hostip" => $this->server->api->getProperty("server-ip", "0.0.0.0")
 		);
 		foreach($KVdata as $key => $value){
