08000257fb0cf8ba777349cc51ed0249846d88cc
Michael Yoo <sekjun9878@gmail.com>
2013-07-15 12:50:03 +0930

    Update Deprecation.php

--- src/Deprecation.php
+++ src/Deprecation.php
@@ -35,6 +35,7 @@ class Deprecation{
 		"api.op.check" => "op.check",
 		"api.player.offline.get" => "player.offline.get",
 		"api.player.offline.save" => "player.offline.save",
+		"console.check" => "command.check",
 	);
 
 
