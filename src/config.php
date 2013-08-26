f14c5ec90c95d4ec77ed56f9c7b1ffb5f565cb68
Shoghi Cervantes <shoghicp@gmail.com>
2013-08-26 18:36:58 +0200

    Updated version to 0.7.4

--- src/config.php
+++ src/config.php
@@ -66,7 +66,7 @@ ini_set("memory_limit", "128M"); //Default
 define("LOG", true);
 define("START_TIME", microtime(true));
 define("MAJOR_VERSION", "Alpha_1.3.7dev");
-define("CURRENT_MINECRAFT_VERSION", "0.7.3 alpha");
+define("CURRENT_MINECRAFT_VERSION", "0.7.4 alpha");
 define("CURRENT_API_VERSION", 9);
 define("CURRENT_PHP_VERSION", "5.5");
 $gitsha1 = false;
