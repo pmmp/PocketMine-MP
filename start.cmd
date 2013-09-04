a965103a03eb35ce293910bfb3ef3312cb689090
Shoghi Cervantes <shoghicp@gmail.com>
2013-09-04 19:59:12 +0200

    Windows installer fix

--- start.cmd
+++ start.cmd
@@ -7,11 +7,11 @@ echo [ERROR] Couldn't find PHP binary in PATH.
 echo [INFO] Please use the Windows installer from the homepage
 pause
 ) else (
-	if exist php.cmd (
+	if exist bin\php\php.exe (
 		if exist bin\mintty.exe (
-			start "" bin\mintty.exe -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max php.cmd -d enable_dl=On PocketMine-MP.php --enable-ansi %*
+			start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o CursorType=0 -o CursorBlinks=1 -h never -t "PocketMine-MP" -i bin/pocketmine.ico -w max bin\php\php.exe -d enable_dl=On PocketMine-MP.php --enable-ansi %*
 		) else (
-			php.cmd -d enable_dl=On PocketMine-MP.php %*
+			bin\php\php.exe -d enable_dl=On PocketMine-MP.php %*
 		)
 	) else (
 		if exist bin\mintty.exe (
