9db1b741a76add80c4d558bb43e892f9e436eccf
Shoghi Cervantes <shoghicp@gmail.com>
2013-09-04 20:34:48 +0200

    Another Typo ¬¬

--- start.cmd
+++ start.cmd
@@ -8,19 +8,20 @@ if not "%PHPOUTPUT%"=="1" (
 		echo [ERROR] Couldn't find PHP binary in PATH.
 		echo [INFO] Please use the Windows installer from the homepage
 		pause
-	)
-) else (
-	if exist bin\php\php.exe (
-		if exist bin\mintty.exe (
-			start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o CursorType=0 -o CursorBlinks=1 -h never -t "PocketMine-MP" -i bin/pocketmine.ico -w max bin\php\php.exe -d enable_dl=On PocketMine-MP.php --enable-ansi %*
-		) else (
-			bin\php\php.exe -d enable_dl=On PocketMine-MP.php %*
-		)
-	) else (
-		if exist bin\mintty.exe (
-			start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o CursorType=0 -o CursorBlinks=1 -h never -t "PocketMine-MP" -i bin/pocketmine.ico -w max php -d enable_dl=On PocketMine-MP.php --enable-ansi %*
-		) else (
-			php -d enable_dl=On PocketMine-MP.php %*
-		)
+		exit
 	)
 )
+if exist bin\php\php.exe (
+	if exist bin\mintty.exe (
+		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o CursorType=0 -o CursorBlinks=1 -h never -t "PocketMine-MP" -i bin/pocketmine.ico -w max bin\php\php.exe -d enable_dl=On PocketMine-MP.php --enable-ansi %*
+	) else (
+		bin\php\php.exe -d enable_dl=On PocketMine-MP.php %*
+	)
+) else (
+	if exist bin\mintty.exe (
+		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o CursorType=0 -o CursorBlinks=1 -h never -t "PocketMine-MP" -i bin/pocketmine.ico -w max php -d enable_dl=On PocketMine-MP.php --enable-ansi %*
+	) else (
+		php -d enable_dl=On PocketMine-MP.php %*
+	)
+)
+
