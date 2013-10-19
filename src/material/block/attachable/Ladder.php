386b2cb0ef34fd5789f910442884c6767603e6f7
Michael Yoo <sekjun9878@gmail.com>
2013-10-19 11:20:33 +1030

    Fix 805

--- src/material/block/attachable/Ladder.php
+++ src/material/block/attachable/Ladder.php
@@ -42,6 +42,18 @@ class LadderBlock extends TransparentBlock{
 		}
 		return false;
 	}
+
+	public function onUpdate($type){
+		if($type === BLOCK_UPDATE_NORMAL){
+			if($this->getSide(0)->getID() === AIR){ //Replace with common break method
+				ServerAPI::request()->api->entity->drop($this, BlockAPI::getItem(LADDER, 0, 1));
+				$this->level->setBlock($this, new AirBlock(), true, true, true);
+				return BLOCK_UPDATE_NORMAL;
+			}
+		}
+		return false;
+	}
+
 	public function getDrops(Item $item, Player $player){
 		return array(
 			array($this->id, 0, 1),
