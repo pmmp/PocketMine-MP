cb47bf82c994c6866a08e69c4f620b9165348aa0
Michael Yoo <sekjun9878@gmail.com>
2013-11-09 20:44:09 +1030

    Reset player's armors after death UNTESTED. Fixes #896.

--- src/world/Entity.php
+++ src/world/Entity.php
@@ -169,7 +169,7 @@ class Entity extends Position{
 			}
 			for($re = 0; $re < 3; $re++)
 			{
-				//TODO: Test this.
+				//TODO: Test this
 				$slot = $this->player->getArmor($re);
 				$this->player->setArmor($re, BlockAPI::getItem(AIR, 0, 0));
 				if($slot->getID() !== AIR and $slot->count > 0){
