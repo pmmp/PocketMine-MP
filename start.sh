a8055eb59ab4793860035c2bcb3db6b24ece2fab
Shoghi Cervantes <shoghicp@gmail.com>
2013-11-24 20:09:04 +0100

    Warn about not using the bin/ PHP binary

--- start.sh
+++ start.sh
@@ -2,12 +2,13 @@
 DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$DIR"
 if [ -f ./php5/bin/php ]; then
-mkdir -m 0777 bin/
-mv ./php5/bin/php ./bin/php
-rm -r -f ./php5/
+	mkdir -m 0777 bin/
+	mv ./php5/bin/php ./bin/php
+	rm -r -f ./php5/
 fi
 if [ -f ./bin/php ]; then
-./bin/php -d enable_dl=On PocketMine-MP.php $@
+	./bin/php -d enable_dl=On PocketMine-MP.php $@
 else
-php -d enable_dl=On PocketMine-MP.php $@
+	echo "[WARNING] You are not using the standalone PocketMine-MP PHP binary."
+	php -d enable_dl=On PocketMine-MP.php $@
 fi
