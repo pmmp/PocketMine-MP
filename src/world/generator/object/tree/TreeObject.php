3968f3b298e40a5e2bd7f67d4959f8e2c9b43df3
Shoghi Cervantes <shoghicp@gmail.com>
2013-11-24 15:12:31 +0100

    Jungle Tree generation placeholder

--- src/world/generator/object/tree/TreeObject.php
+++ src/world/generator/object/tree/TreeObject.php
@@ -41,6 +41,10 @@ class TreeObject{
 				$tree = new SmallTreeObject();
 				$tree->type = SaplingBlock::BIRCH;
 				break;
+			case SaplingBlock::JUNGLE:
+				$tree = new SmallTreeObject();
+				$tree->type = SaplingBlock::JUNGLE;
+				break;
 			default:
 			case SaplingBlock::OAK:
 				/*if($random->nextRange(0, 9) === 0){
