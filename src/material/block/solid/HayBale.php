c25607588bcc6fa1721975bf5e592758e768defc
Shoghi Cervantes <shoghicp@gmail.com>
2013-11-24 13:32:21 +0100

    Added sideway Hay Bales

--- src/material/block/solid/HayBale.php
+++ src/material/block/solid/HayBale.php
@@ -20,9 +20,30 @@
 */
 
 class HayBaleBlock extends SolidBlock{
-	public function __construct(){
-		parent::__construct(HAY_BALE, "Hay Bale");
+	public function __construct($meta = 0){
+		parent::__construct(HAY_BALE, $meta, "Hay Bale");
 		$this->hardness = 10;
+	}
+	
+	public function place(Item $item, Player $player, Block $block, Block $target, $face, $fx, $fy, $fz){
+		$faces = array(
+			0 => 0,
+			1 => 0,
+			2 => 0b1000,
+			3 => 0b1000,
+			4 => 0b0100,
+			5 => 0b0100,
+		);
+
+		$this->meta = ($this->meta & 0x03) | $faces[$face];
+		$this->level->setBlock($block, $this, true, false, true);
+		return true;
+	}
+
+	public function getDrops(Item $item, Player $player){
+		return array(
+			array($this->id, 0, 1),
+		);
 	}
 	
 }
