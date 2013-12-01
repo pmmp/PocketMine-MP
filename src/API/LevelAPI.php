7d0ff5e073c7a63c537f6f7cb1ac708f8c23e912
Shoghi Cervantes <shoghicp@gmail.com>
2013-12-01 14:16:38 +0100

    Formatting OCD
    Please refer to CONTRIBUTING.md

--- src/API/LevelAPI.php
+++ src/API/LevelAPI.php
@@ -2,11 +2,11 @@
 
 /**
  *
- *  ____            _        _   __  __ _                  __  __ ____  
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+ *  ____			_		_   __  __ _				  __  __ ____  
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \ 
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_| 
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -33,17 +33,13 @@ class LevelAPI{
 		return false;
 	}
 
-    public function isLoaded($name)
-    {
-        if(isset($this->levels[$name]))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+	public function isLoaded($name){
+		if(isset($this->levels[$name])){
+			return true;
+		}else{
+			return false;
+		}
+	}
 	
 	public function getDefault(){
 		return $this->levels[$this->default];
