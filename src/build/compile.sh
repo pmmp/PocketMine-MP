78d72201ce9d131bc62ce40b4c6c48269bb3f8d1
Shoghi Cervantes <shoghicp@gmail.com>
2013-12-17 22:30:45 +0100

    (ノಠ益ಠ)ノ彡┻━┻

--- src/build/compile.sh
+++ src/build/compile.sh
@@ -281,4 +281,4 @@ rm -r -f php5/ >> "$DIR/install.log" 2>&1
 date >> "$DIR/install.log" 2>&1
 echo " done!"
 echo "[PocketMine] You should start the server now using \"./start.sh\""
-echo "[PocketMine] If it doesn't works, please send the \"install.log\" file to the Bug Tracker"
+echo "[PocketMine] If it doesn't work, please send the \"install.log\" file to the Bug Tracker"
