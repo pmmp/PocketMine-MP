cd52cb9ae655aa8a005d2dc27fd507667de91e59
Shoghi Cervantes <shoghicp@gmail.com>
2014-01-29 20:02:16 +0100

    Added exec to start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$DIR"
 if [ -f ./bin/php5/bin/php ]; then
-	./bin/php5/bin/php -d enable_dl=On PocketMine-MP.php $@
+	exec ./bin/php5/bin/php -d enable_dl=On PocketMine-MP.php $@
 else
 	echo "[WARNING] You are not using the standalone PocketMine-MP PHP binary."
-	php -d enable_dl=On PocketMine-MP.php $@
+	exec php -d enable_dl=On PocketMine-MP.php $@
 fi
