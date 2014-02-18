08c1dbe3a7d8a0aad0c5a78fcf74bcd4af382539
Shoghi Cervantes <shoghicp@gmail.com>
2014-02-18 18:52:27 +0100

    fix fix*

--- src/build/installer.sh
+++ src/build/installer.sh
@@ -81,10 +81,10 @@ else
 	echo " detecting if build is available..."
 	if [ "$forcecompile" == "off" ] && [ "$(uname -s)" == "Darwin" ]; then
 		set +e
-		UNAME_M=$(uname -m)
-		IS_IOS=$(expr match $UNAME_M 'iP[a-zA-Z0-9,]*')
+		UNAME_M=`uname -m`
+		IS_IOS=`expr match $UNAME_M 'iP[a-zA-Z0-9,]*'`
 		set -e
-		if [ $IS_IOS -gt 0 2>> /dev/null ]; then
+		if [ $IS_IOS -gt 0 2> /dev/null ]; then
 			rm -r -f bin/ >> /dev/null 2>&1
 			echo -n "[3/3] iOS PHP build available, downloading $IOS_BUILD.tar.gz..."
 			download_file "http://sourceforge.net/projects/pocketmine/files/builds/$IOS_BUILD.tar.gz" | tar -zx > /dev/null 2>&1
@@ -102,7 +102,7 @@ else
 			fi
 		else
 			rm -r -f bin/ >> /dev/null 2>&1
-			if [ `getconf LONG_BIT` = "64" ]; then
+			if [ `getconf LONG_BIT` == "64" ]; then
 				echo -n "[3/3] MacOS 64-bit PHP build available, downloading $MAC_64_BUILD.tar.gz..."
 				MAC_BUILD=$MAC_64_BUILD
 			else
