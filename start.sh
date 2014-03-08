ababc879b58a5fce0d5eaf8bf860299f7b31ab0e
Shoghi Cervantes <shoghicp@gmail.com>
2014-03-08 03:28:00 +0100

    Removed warning message about not-bundled PHP

--- start.sh
+++ start.sh
@@ -4,6 +4,5 @@ cd "$DIR"
 if [ -f ./bin/php5/bin/php ]; then
 	exec ./bin/php5/bin/php -d enable_dl=On PocketMine-MP.php $@
 else
-	echo "[WARNING] You are not using the standalone PocketMine-MP PHP binary."
 	exec php -d enable_dl=On PocketMine-MP.php $@
 fi
