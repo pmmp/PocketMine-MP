d83b5c3a60d126c23fbecdbc04d6954896f2b2bb
Shoghi Cervantes <shoghicp@gmail.com>
2014-04-07 03:25:31 +0200

    Oops

--- src/build/compile.sh
+++ src/build/compile.sh
@@ -514,7 +514,7 @@ HAS_POCKETMINE=""
 if [ "$HAS_ZEPHIR" == "yes" ]; then
 	echo -n "[C PocketMine extension] downloading latest..."
 	download_file https://github.com/PocketMine/PocketMine-MP-Zephir/archive/master.tar.gz | tar -zx >> "$DIR/install.log" 2>&1
-	mv PocketMine-MP-Zephir-master/ext "$DIR/install_data/php/ext/pocketmine"
+	mv PocketMine-MP-Zephir-master/pocketmine/ext "$DIR/install_data/php/ext/pocketmine"
 	rm -r PocketMine-MP-Zephir-master/
 	HAS_POCKETMINE="--enable-pocketmine"
 fi
