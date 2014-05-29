c6632da15928c93356dcdd84d52278ce69549f92
Shoghi Cervantes <shoghicp@gmail.com>
2014-05-29 18:47:22 +0200

    Removed debug code from player data saving

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -674,7 +674,6 @@ class Server{
 		$nbt = new NBT(NBT::BIG_ENDIAN);
 		$nbt->setData($nbtTag);
 		file_put_contents($this->getDataPath() . "players/" . strtolower($name) . ".dat", $nbt->writeCompressed());
-		file_put_contents($this->getDataPath() . "players/" . strtolower($name) . ".raw.dat", $nbt->write());
 	}
 
 	/**
