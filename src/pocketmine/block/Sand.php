6722540bdceba513a781c5d63ef53098571a2f6b
Shoghi Cervantes <shoghicp@gmail.com>
2014-06-16 15:25:30 +0200

    Added Red sand

--- src/pocketmine/block/Sand.php
+++ src/pocketmine/block/Sand.php
@@ -23,8 +23,11 @@ namespace pocketmine\block;
 
 
 class Sand extends Fallable{
-	public function __construct(){
-		parent::__construct(self::SAND, 0, "Sand");
+	public function __construct($meta = 0){
+		parent::__construct(self::SAND, $meta & 0x01, "Sand");
+		if($this->meta === 0x01){
+			$this->name = "Red sand";
+		}
 		$this->hardness = 2.5;
 	}
 
