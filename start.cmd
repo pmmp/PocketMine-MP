0503adc08ce677d24efbe813265475fa365c61d3
Shoghi Cervantes <shoghicp@gmail.com>
2014-07-06 02:40:13 +0200

    Fixed PHP installation reading wrong php.ini

--- start.cmd
+++ start.cmd
@@ -25,6 +25,6 @@ if exist PocketMine-MP.phar (
 	if exist bin\mintty.exe (
 		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
 	) else (
-		%PHP_BINARY% %POCKETMINE_FILE% %*
+		%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
 	)
 #)
