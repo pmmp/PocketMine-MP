0bfa9506d1414a57a339838e185f96af534412a6
Shoghi Cervantes <shoghicp@gmail.com>
2014-07-12 02:26:41 +0200

    Updated API version to 1.1.0 (backwards-compatible)

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -74,7 +74,7 @@ namespace pocketmine {
 	use raklib\RakLib;
 
 	const VERSION = "Alpha_1.4dev";
-	const API_VERSION = "1.0.0";
+	const API_VERSION = "1.1.0";
 	const CODENAME = "絶好(Zekkou)ケーキ(Cake)";
 	const MINECRAFT_VERSION = "v0.9.0 alpha";
 	const PHP_VERSION = "5.5";
