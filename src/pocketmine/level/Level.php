ccf34a9db8bc2cc0f21830b84b443c6b90156064
williamtdr <william@cboos.com>
2014-07-21 15:29:40 -0500

    #1745 part 2!

--- src/pocketmine/level/Level.php
+++ src/pocketmine/level/Level.php
@@ -812,8 +812,8 @@ class Level implements ChunkManager, Metadatable{
 		if($level instanceof Level) {
 			$above = $level->getBlock(new Vector3($target->x, $target->y + 1, $target->z));
 			if($above instanceof Block) {
-				if($above->getID() === pocketmine\block::FIRE) {
-					$level->setBlock($above, new pocketmine\block\AIR(), true, false, true);
+				if($above->getID() === \pocketmine\block::FIRE) {
+					$level->setBlock($above, new \pocketmine\block::AIR(), true, false, true);
 				}
 			}
 		}
