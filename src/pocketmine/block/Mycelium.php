532b12c6c0af73ad47a8e8c5c29741da047b9c31
Intyre <Intyre@gmail.com>
2014-08-18 20:06:18 +0200

    Mycelium spreads like grass and MonsterSpawner breaktime added

--- src/pocketmine/block/Mycelium.php
+++ src/pocketmine/block/Mycelium.php
@@ -28,4 +28,26 @@ class Mycelium extends Solid{
 		parent::__construct(self::MYCELIUM, 0, "Mycelium");
 		$this->hardness = 2.5;
 	}
+
+    public function getDrops(Item $item){
+        return [
+            [Item::DIRT, 0, 1],
+        ];
+    }
+
+    public function onUpdate($type){
+        if($type === Level::BLOCK_UPDATE_RANDOM){
+            //TODO: light levels
+            $x = mt_rand($this->x - 1, $this->x + 1);
+            $y = mt_rand($this->y - 2, $this->y + 2);
+            $z = mt_rand($this->z - 1, $this->z + 1);
+            $block = $this->getLevel()->getBlockIdAt($x, $y, $z);
+            if($block === Block::DIRT){
+                $block = $this->getLevel()->getBlock(new Vector3($x, $y, $z));
+                if($block->getSide(1) instanceof Transparent){
+                    $this->getLevel()->setBlock($block, new Mycelium());
+                }
+            }
+        }
+    }
 }
