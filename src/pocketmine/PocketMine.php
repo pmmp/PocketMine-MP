f7de1ede3fa710a5236b146bad29fd2437393881
Shoghi Cervantes <shoghicp@gmail.com>
2014-08-27 21:30:36 +0200

    Made PocketMine loader backwards-compatible

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -20,9 +20,9 @@
 */
 
 namespace {
-	function safe_var_dump(...$params){
+	function safe_var_dump(){
 		static $cnt = 0;
-		foreach($params as $var){
+		foreach(func_get_args() as $var){
 			switch(true){
 				case is_array($var):
 					echo str_repeat("  ", $cnt) . "array(" . count($var) . ") {" . PHP_EOL;
