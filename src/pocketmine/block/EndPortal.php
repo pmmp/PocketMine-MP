cf542ac73aaee90ac24258b00508e02512d04bf3
Shoghi Cervantes <shoghicp@gmail.com>
2014-08-30 23:06:50 +0200

    Added End Portal frame bounding box

--- src/pocketmine/block/EndPortal.php
+++ src/pocketmine/block/EndPortal.php
@@ -21,9 +21,22 @@
 
 namespace pocketmine\block;
 
+use pocketmine\math\AxisAlignedBB;
+
 class EndPortal extends Solid{
 	public function __construct($meta = 0){
 		parent::__construct(self::END_PORTAL, $meta, "End Portal");
 		$this->hardness = 18000000;
+	}
+
+	public function getBoundingBox(){
+		return new AxisAlignedBB(
+			$this->x,
+			$this->y,
+			$this->z,
+			$this->x + 1,
+			$this->y + (($this->getDamage() & 0x04) > 0 ? 1 : 0.8125),
+			$this->z + 1
+		);
 	}
 }
