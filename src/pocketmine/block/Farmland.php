ccea26c978f10dc277310be0224aca9fc226bb8d
Shoghi Cervantes <shoghicp@gmail.com>
2014-08-30 23:07:55 +0200

    Added Farmland bounding box

--- src/pocketmine/block/Farmland.php
+++ src/pocketmine/block/Farmland.php
@@ -22,11 +22,23 @@
 namespace pocketmine\block;
 
 use pocketmine\item\Item;
+use pocketmine\math\AxisAlignedBB;
 
 class Farmland extends Solid{
 	public function __construct($meta = 0){
 		parent::__construct(self::FARMLAND, $meta, "Farmland");
 		$this->hardness = 3;
+	}
+
+	public function getBoundingBox(){
+		return new AxisAlignedBB(
+			$this->x,
+			$this->y,
+			$this->z,
+			$this->x + 1,
+			$this->y + 0.9375,
+			$this->z + 1
+		);
 	}
 
 	public function getDrops(Item $item){
