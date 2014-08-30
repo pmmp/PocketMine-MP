3eb8ca0d13b5f3c25036946e0cc84bd7931f8941
Shoghi Cervantes <shoghicp@gmail.com>
2014-08-30 23:47:39 +0200

    Added Soul Sand bounding box

--- src/pocketmine/block/SoulSand.php
+++ src/pocketmine/block/SoulSand.php
@@ -22,10 +22,23 @@
 namespace pocketmine\block;
 
 
+use pocketmine\math\AxisAlignedBB;
+
 class SoulSand extends Solid{
 	public function __construct(){
 		parent::__construct(self::SOUL_SAND, 0, "Soul Sand");
 		$this->hardness = 2.5;
+	}
+
+	public function getBoundingBox(){
+		return new AxisAlignedBB(
+			$this->x,
+			$this->y,
+			$this->z,
+			$this->x + 1,
+			$this->y + 1 - 0.125,
+			$this->z + 1
+		);
 	}
 
 }
