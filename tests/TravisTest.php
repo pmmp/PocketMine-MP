92b0b4d43c3a41ec6e3d18be050bf4c25952bab7
Shoghi Cervantes <shoghicp@gmail.com>
2014-09-07 19:32:21 +0200

    Implemented basic Travis-CI test

---
+++ tests/TravisTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____  
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ * 
+ *
+*/
+
+$server = proc_open("php src/pocketmine/PocketMine.php --no-wizard", [
+	0 => ["pipe", "w"],
+	1 => ["file", "php://stdout", "w"],
+	2 => ["file", "php://stderr", "w"]
+], $pipes);
+
+fwrite($pipes[0], "version\nmakeserver\nstop\n\n");
+
+fclose($pipes[0]);
+
+echo "\n\nReturn value: ". proc_close($server) ."\n";
+
+if(count(glob("plugins/DevTools/PocketMine-MP*.phar")) === 0){
+	echo "No server Phar created!\n";
+	exit(1);
+}else{
+	exit(0);
+}
