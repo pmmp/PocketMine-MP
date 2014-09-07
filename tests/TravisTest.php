7d387fe6aa1ebf82680ab5ad981134a292824765
Shoghi Cervantes <shoghicp@gmail.com>
2014-09-07 19:54:13 +0200

    Fixed TravisTest output

--- tests/TravisTest.php
+++ tests/TravisTest.php
@@ -19,15 +19,20 @@
  *
 */
 
-$server = proc_open("php src/pocketmine/PocketMine.php --no-wizard --disable-readline", [
+$server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --disable-readline", [
 	0 => ["pipe", "r"],
-	1 => ["file", "php://stdout", "w"],
+	1 => ["pipe", "w"],
 	2 => ["pipe", "w"]
 ], $pipes);
 
 fwrite($pipes[0], "version\nmakeserver\nstop\n\n");
 
+while(!feof($pipes[1])){
+	echo fgets($pipes[1]);
+}
+
 fclose($pipes[0]);
+fclose($pipes[1]);
 fclose($pipes[2]);
 
 echo "\n\nReturn value: ". proc_close($server) ."\n";
