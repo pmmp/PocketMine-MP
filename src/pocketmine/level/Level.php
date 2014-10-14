07dcbdb9b0859f61e4380ed6f4d062d8b7c916bd
Shoghi Cervantes <shoghicp@gmail.com>
2014-10-14 16:03:14 +0200

    Fixed Level->getNearbyEntities(), fixes item drops not being picked up

--- src/pocketmine/level/Level.php
+++ src/pocketmine/level/Level.php
@@ -1294,9 +1294,9 @@ class Level implements ChunkManager, Metadatable{
 		$nearby = [];
 
 		$minX = Math::floorFloat(($bb->minX - 2) / 16);
-		$maxX = Math::floorFloat(($bb->maxX - 2) / 16);
+		$maxX = Math::floorFloat(($bb->maxX + 2) / 16);
 		$minZ = Math::floorFloat(($bb->minZ - 2) / 16);
-		$maxZ = Math::floorFloat(($bb->maxZ - 2) / 16);
+		$maxZ = Math::floorFloat(($bb->maxZ + 2) / 16);
 
 		for($x = $minX; $x <= $maxX; ++$x){
 			for($z = $minZ; $z <= $maxZ; ++$z){
