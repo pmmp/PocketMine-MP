5cb428e5ccd0e6b691c0d6a56f99614e6bea1a01
Shoghi Cervantes <shoghicp@gmail.com>
2014-10-23 17:23:59 +0200

    Updated build number

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -74,7 +74,7 @@ namespace pocketmine {
 	const VERSION = "Alpha_1.4dev";
 	const API_VERSION = "1.6.0";
 	const CODENAME = "絶好(Zekkou)ケーキ(Cake)";
-	const MINECRAFT_VERSION = "v0.10.0 alpha build1";
+	const MINECRAFT_VERSION = "v0.10.0 alpha build2";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
