09a01be7094ccfbfb52b53e40ba230f48d0bfc32
Shoghi Cervantes <shoghicp@gmail.com>
2014-10-30 16:32:57 +0100

    Added ifndef for NBT Binary

--- src/pocketmine/nbt/NBT.php
+++ src/pocketmine/nbt/NBT.php
@@ -38,8 +38,12 @@ use pocketmine\nbt\tag\NamedTAG;
 use pocketmine\nbt\tag\Short;
 use pocketmine\nbt\tag\String;
 use pocketmine\nbt\tag\Tag;
-use pocketmine\utils\Binary;
 use pocketmine\utils\Utils;
+
+#ifndef COMPILE
+use pocketmine\utils\Binary;
+#endif
+
 
 #include <rules/NBT.h>
 
