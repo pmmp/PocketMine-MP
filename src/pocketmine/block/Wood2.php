05ac256cc3ff0dd244ae334f563cf2b6ef441dda
Shoghi Cervantes <shoghicp@gmail.com>
2014-11-14 15:58:31 +0100

    Fixed Acacia / Dark Oak logs crafting recipes

--- src/pocketmine/block/Wood2.php
+++ src/pocketmine/block/Wood2.php
@@ -23,12 +23,16 @@ namespace pocketmine\block;
 
 
 class Wood2 extends Wood{
+	
+	const ACACIA = 0;
+	const DARK_OAK = 1;
 
 	public function __construct($meta = 0){
 		Solid::__construct(self::WOOD2, $meta, "Wood");
 		$names = [
 			0 => "Acacia Wood",
-			1 => "Dark Oak Wood"
+			1 => "Dark Oak Wood",
+			2 => ""
 		];
 		$this->name = $names[$this->meta & 0x03];
 		$this->hardness = 10;
