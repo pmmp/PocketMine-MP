a5294d5ec7e7cca186230ab15835394f28822aaa
Shoghi Cervantes <shoghicp@gmail.com>
2014-12-07 20:54:38 +0100

    Revert "Fixed small possible error in permission"

--- src/pocketmine/command/defaults/DeopCommand.php
+++ src/pocketmine/command/defaults/DeopCommand.php
@@ -34,7 +34,7 @@ class DeopCommand extends VanillaCommand{
 			"Takes the specified player's operator status",
 			"/deop <player>"
 		);
-		$this->setPermission("pocketmine.command.deop");
+		$this->setPermission("pocketmine.command.op.take");
 	}
 
 	public function execute(CommandSender $sender, $currentAlias, array $args){
