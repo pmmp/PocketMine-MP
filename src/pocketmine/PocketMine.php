0117e8dfaee618e31a7b7320d68c6575b0076889
Shoghi Cervantes <shoghicp@gmail.com>
2015-04-09 12:30:07 +0200

    New codename, MCPE version, bumped split packet limit

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -73,8 +73,8 @@ namespace pocketmine {
 
 	const VERSION = "1.5dev";
 	const API_VERSION = "1.12.0";
-	const CODENAME = "絶好(Zekkou)ケーキ(Cake)";
-	const MINECRAFT_VERSION = "v0.10.5 alpha";
+	const CODENAME = "活発(Kappatsu)フグ(Fugu)";
+	const MINECRAFT_VERSION = "v0.11.0 alpha build 1";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
