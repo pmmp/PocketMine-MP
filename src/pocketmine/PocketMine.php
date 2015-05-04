001915fcd67a9d54a70c4cea758e5fb76316c636
Shoghi Cervantes <shoghicp@gmail.com>
2015-05-04 17:58:29 +0200

    beta build 9

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -74,8 +74,8 @@ namespace pocketmine {
 	const VERSION = "1.5dev";
 	const API_VERSION = "1.12.0";
 	const CODENAME = "活発(Kappatsu)フグ(Fugu)";
-	const MINECRAFT_VERSION = "v0.11.0 alpha build 8";
-	const MINECRAFT_VERSION_NETWORK = "0.11.0.8";
+	const MINECRAFT_VERSION = "v0.11.0 alpha build 9";
+	const MINECRAFT_VERSION_NETWORK = "0.11.0.9";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
