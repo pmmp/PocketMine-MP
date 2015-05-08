7e539ec85a08bce40fdabc6f3f47444aaf9511ef
Shoghi Cervantes <shoghicp@gmail.com>
2015-05-08 15:38:51 +0200

    Fixed invalid check

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -313,7 +313,7 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer{
 	 * @param Player $player
 	 */
 	public function spawnTo(Player $player){
-		if($this->spawned === true and $player->spawned === true and $this->isAlive() and $player->isAdventure() and $player->getLevel() === $this->level and $player->canSee($this) and !$this->isSpectator()){
+		if($this->spawned and $player->spawned and $this->isAlive() and $player->isAlive() and $player->getLevel() === $this->level and $player->canSee($this) and !$this->isSpectator()){
 			parent::spawnTo($player);
 		}
 	}
