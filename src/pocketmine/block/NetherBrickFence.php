a3bce67d35bda64e35a32b7d8a363e45b03fcd99
willowmaster66 <willowmaster@gmail.com>
2015-09-18 22:17:24 +0200

    Updating blocks and items
    Added new blocks/items and added constants to existing blocks/items for
    clearer recipe making.

--- src/pocketmine/block/NetherBrickFence.php
+++ src/pocketmine/block/NetherBrickFence.php
@@ -18,50 +18,55 @@
  * 
  *
 */
-
 namespace pocketmine\block;
 
+use pocketmine\block\Block;
+use pocketmine\block\Fence;
 use pocketmine\item\Item;
-use pocketmine\item\Tool;
+//use pocketmine\item\Tool;
 
-class StoneBricks extends Solid{
-	const NORMAL = 0;
-	const MOSSY = 1;
-	const CRACKED = 2;
-	const CHISELED = 3;
-
-	protected $id = self::STONE_BRICKS;
-
-	public function __construct($meta = 0){
-		$this->meta = $meta;
+class NetherBrickFence extends Transparent {
+       
+	public function __construct(){
+		parent::__construct(self::NETHER_BRICK_FENCE);
+	}
+	
+	public function getBreakTime(Item $item){
+		if ($item instanceof Air){
+			//Breaking by hand
+			return 10;
+		}
+		else{
+			// Other breaktimes are equal to woodfences.
+			return parent::getBreakTime($item);
+		}
 	}
 
 	public function getHardness(){
-		return 1.5;
+		return 2;
 	}
-
+        
 	public function getToolType(){
+		//Different then the woodfences
 		return Tool::TYPE_PICKAXE;
 	}
-
+	
 	public function getName(){
-		static $names = [
-			self::NORMAL => "Stone Bricks",
-			self::MOSSY => "Mossy Stone Bricks",
-			self::CRACKED => "Cracked Stone Bricks",
-			self::CHISELED => "Chiseled Stone Bricks",
-		];
-		return $names[$this->meta & 0x03];
+		return "Nether Brick Fence";
 	}
 	
+	public function canConnect(Block $block){
+		//TODO: activate comments when the NetherBrickFenceGate class has been created.
+		return ($block instanceof NetherBrickFence /* or $block instanceof NetherBrickFenceGate */) ? true : $block->isSolid() and !$block->isTransparent();
+	}
+
 	public function getDrops(Item $item){
-		if($item->isPickaxe() >= 1){
+		if($item->isPickaxe()){
 			return [
-				[Item::STONE_BRICKS, $this->meta & 0x03, 1],
+				[Item::FENCE, Fence::FENCE_NETHER_BRICK, 1],
 			];
 		}else{
 			return [];
 		}
-	}
-
+	}        
 }
