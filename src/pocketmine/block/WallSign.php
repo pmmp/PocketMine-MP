f9d7e204c8725ad198083d6bbf800ddb75a07287
Intyre <Intyre@gmail.com>
2015-10-11 01:32:33 +0200

    Block update on WallSign fix

--- src/pocketmine/block/WallSign.php
+++ src/pocketmine/block/WallSign.php
@@ -22,6 +22,8 @@
 namespace pocketmine\block;
 
 
+use pocketmine\level\Level;
+
 class WallSign extends SignPost{
 
 	protected $id = self::WALL_SIGN;
@@ -31,6 +33,20 @@ class WallSign extends SignPost{
 	}
 
 	public function onUpdate($type){
+		$faces = [
+			2 => 3,
+			3 => 2,
+			4 => 5,
+			5 => 4,
+		];
+		if($type === Level::BLOCK_UPDATE_NORMAL){
+			if(isset($faces[$this->meta])) {
+				if ($this->getSide($faces[$this->meta])->getId() === self::AIR) {
+					$this->getLevel()->useBreakOn($this);
+				}
+				return Level::BLOCK_UPDATE_NORMAL;
+			}
+		}
 		return false;
 	}
 }
