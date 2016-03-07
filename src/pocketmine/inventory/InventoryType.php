08e6caad887724bc553cc4f0e7e09c9d7c5be79d
PEMapModder <pemapmodder1970@gmail.com>
2016-03-07 18:38:42 +0800

    Fixed #3563

--- src/pocketmine/inventory/InventoryType.php
+++ src/pocketmine/inventory/InventoryType.php
@@ -58,7 +58,7 @@ class InventoryType{
 
 		static::$default[static::CHEST] = new InventoryType(27, "Chest", 0);
 		static::$default[static::DOUBLE_CHEST] = new InventoryType(27 + 27, "DoubleTag Chest", 0);
-		static::$default[static::PLAYER] = new InventoryType(40, "Player", 0); //27 CONTAINER, 4 ARMOR (9 reference HOTBAR slots)
+		static::$default[static::PLAYER] = new InventoryType(49, "Player", 0); //36 CONTAINER, 4 ARMOR (9 reference HOTBAR slots)
 		static::$default[static::FURNACE] = new InventoryType(3, "Furnace", 2);
 		static::$default[static::CRAFTING] = new InventoryType(5, "Crafting", 1); //4 CRAFTING slots, 1 RESULT
 		static::$default[static::WORKBENCH] = new InventoryType(10, "Crafting", 1); //9 CRAFTING slots, 1 RESULT
