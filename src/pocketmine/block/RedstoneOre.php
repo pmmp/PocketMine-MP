aade3fe3908e329233628bc3c94e3203ccf1b960
PEMapModder <pemapmodder1970@gmail.com>
2016-03-28 02:42:08 +0800

    Closes #3432

--- src/pocketmine/block/RedstoneOre.php
+++ src/pocketmine/block/RedstoneOre.php
@@ -43,7 +43,7 @@ class RedstoneOre extends Solid{
 
 	public function onUpdate($type){
 		if($type === Level::BLOCK_UPDATE_NORMAL or $type === Level::BLOCK_UPDATE_TOUCH){
-			$this->getLevel()->setBlock($this, Block::get(Item::GLOWING_REDSTONE_ORE, $this->meta), false, true);
+			$this->getLevel()->setBlock($this, Block::get(Item::GLOWING_REDSTONE_ORE, $this->meta));
 
 			return Level::BLOCK_UPDATE_WEAK;
 		}
@@ -51,14 +51,12 @@ class RedstoneOre extends Solid{
 		return false;
 	}
 
-
-
 	public function getToolType(){
 		return Tool::TYPE_PICKAXE;
 	}
 
 	public function getDrops(Item $item){
-		if($item->isPickaxe() >= Tool::TIER_GOLD){
+		if($item->isPickaxe() >= Tool::TIER_IRON){
 			return [
 				[Item::REDSTONE_DUST, 0, mt_rand(4, 5)],
 			];
