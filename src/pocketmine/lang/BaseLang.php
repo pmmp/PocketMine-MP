378c3d7994b41849ba4d5608b0a9a5482e84cc25
dktapps <odigiman@gmail.com>
2016-08-10 16:55:10 +0100

    Patch %0 client translation exploit

--- src/pocketmine/lang/BaseLang.php
+++ src/pocketmine/lang/BaseLang.php
@@ -92,7 +92,7 @@ class BaseLang{
 			$baseText = str_replace("{%$i}", $this->parseTranslation((string) $p), $baseText, $onlyPrefix);
 		}
 
-		return $baseText;
+		return str_replace("%0", "", $baseText); //fixes a client bug where %0 in translation will cause freeze
 	}
 
 	public function translate(TextContainer $c){
