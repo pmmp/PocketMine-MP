2fba1073ddf4965eb5d2bbbbbfb4a5c5f9377db3
Dylan K. Taylor <odigiman@gmail.com>
2016-10-12 13:24:00 +0100

    Fix wrong comment

--- src/pocketmine/utils/UUID.php
+++ src/pocketmine/utils/UUID.php
@@ -92,7 +92,7 @@ class UUID{
 	public function toString(){
 		$hex = bin2hex(self::toBinary());
 
-		//xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx 8-4-4-12
+		//xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx 8-4-4-4-12
 		if($this->version !== null){
 			return substr($hex, 0, 8) . "-" . substr($hex, 8, 4) . "-" . hexdec($this->version) . substr($hex, 13, 3) . "-8" . substr($hex, 17, 3) . "-" . substr($hex, 20, 12);
 		}
