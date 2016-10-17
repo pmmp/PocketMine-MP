707847b46e38dea64feec332c45f3fe247994666
Dylan K. Taylor <odigiman@gmail.com>
2016-10-17 10:12:19 +0100

    Shell-based CI with lint scans

---
+++ tests/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -e "version\nmakeserver\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard --disable-ansi
+if ls plugins/DevTools/PocketMine*.phar >/dev/null 2>&1; then
+    echo Server phar created successfully.
+else
+    echo No phar created!
+    exit 1
+fi
