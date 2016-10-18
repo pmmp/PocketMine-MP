e7e476b65e457c3b5d322d05a4789d9303c362c9
Intyre <Intyre@gmail.com>
2016-10-18 18:19:58 +0200

    Wooden and Stone slabs both placable

--- src/pocketmine/level/Level.php
+++ src/pocketmine/level/Level.php
@@ -1678,11 +1678,7 @@ class Level implements ChunkManager, Metadatable{
 			return false;
 		}
 
-		if($face <= 1 and $hand->getId() === Item::WOOD_SLAB and $target->getId() === Item::WOOD_SLAB){
-			$block = $target;
-		}
-
-		if(!($block->canBeReplaced() === true or ($hand->getId() === Item::WOOD_SLAB and $block->getId() === Item::WOOD_SLAB))){
+		if(!($block->canBeReplaced() === true or ($hand->getId() === Item::WOOD_SLAB and $block->getId() === Item::WOOD_SLAB) or ($hand->getId() === Item::SLAB and $block->getId() === Item::SLAB))){
 			return false;
 		}
 
