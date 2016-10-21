902dd4258b62ea4ec0b00d1f1f8b20616bf3bb90
Dylan K. Taylor <odigiman@gmail.com>
2016-10-21 18:32:07 +0100

    Bump version to 0.16.0.5 and protocol to 91

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -30,7 +30,7 @@ interface Info{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	const CURRENT_PROTOCOL = 90;
+	const CURRENT_PROTOCOL = 91;
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
