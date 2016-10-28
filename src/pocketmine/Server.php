7d0a6c9134d1981b85e9b650e5dc679ed6afddde
FunnyCrafteur <nicolas.planchet@gmail.com>
2016-10-28 11:25:02 +0200

    no

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -2341,27 +2341,27 @@ class Server{
 		return $this->memoryManager;
 	}
 
-	// private function titleTick(){
-	// 	if(!Terminal::hasFormattingCodes()){
-	// 		return;
-	// 	}
-	//
-	// 	$d = Utils::getRealMemoryUsage();
-	//
-	// 	$u = Utils::getMemoryUsage(true);
-	// 	$usage = sprintf("%g/%g/%g/%g MB @ %d threads", round(($u[0] / 1024) / 1024, 2), round(($d[0] / 1024) / 1024, 2), round(($u[1] / 1024) / 1024, 2), round(($u[2] / 1024) / 1024, 2), Utils::getThreadCount());
-	//
-	// 	echo "\x1b]0;" . $this->getName() . " " .
-	// 		$this->getPocketMineVersion() .
-	// 		" | Online " . count($this->players) . "/" . $this->getMaxPlayers() .
-	// 		" | Memory " . $usage .
-	// 		" | U " . round($this->network->getUpload() / 1024, 2) .
-	// 		" D " . round($this->network->getDownload() / 1024, 2) .
-	// 		" kB/s | TPS " . $this->getTicksPerSecond() .
-	// 		" | Load " . $this->getTickUsage() . "%\x07";
-	//
-	// 	$this->network->resetStatistics();
-	// }
+	private function titleTick(){
+		if(!Terminal::hasFormattingCodes()){
+			return;
+		}
+
+		$d = Utils::getRealMemoryUsage();
+
+		$u = Utils::getMemoryUsage(true);
+		$usage = sprintf("%g/%g/%g/%g MB @ %d threads", round(($u[0] / 1024) / 1024, 2), round(($d[0] / 1024) / 1024, 2), round(($u[1] / 1024) / 1024, 2), round(($u[2] / 1024) / 1024, 2), Utils::getThreadCount());
+
+		echo "\x1b]0;" . $this->getName() . " " .
+			$this->getPocketMineVersion() .
+			" | Online " . count($this->players) . "/" . $this->getMaxPlayers() .
+			" | Memory " . $usage .
+			" | U " . round($this->network->getUpload() / 1024, 2) .
+			" D " . round($this->network->getDownload() / 1024, 2) .
+			" kB/s | TPS " . $this->getTicksPerSecond() .
+			" | Load " . $this->getTickUsage() . "%\x07";
+
+		$this->network->resetStatistics();
+	}
 
 	/**
 	 * @param string $address
