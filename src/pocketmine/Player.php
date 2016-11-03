208884422d47eedc176a9251082ade14b8091003
Kripth <markwhite1404@gmail.com>
2016-11-03 18:18:53 +0100

    Added whisper

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2989,6 +2989,14 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 		$pk->message = $message;
 		$this->dataPacket($pk);
 	}
+	
+	public function whisper($from, $message){
+		$pk = new TextPacket();
+		$pk->type = TextPacket::TYPE_WHISPER;
+		$pk->source = $from instanceof Player ? $from->getDisplayName() : $from;
+		$pk->message = $message;
+		$this->dataPacket($pk);
+	}
 
 	/**
 	 * Note for plugin developers: use kick() with the isAdmin
