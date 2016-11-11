50b088949b5e5597e74c288524767f9b457ec1fa
SOFe <sofe2038@gmail.com>
2016-11-11 22:53:20 +0800

    Disallow scheduling the same AsyncTask instance more than once

--- src/pocketmine/scheduler/ServerScheduler.php
+++ src/pocketmine/scheduler/ServerScheduler.php
@@ -76,6 +76,9 @@ class ServerScheduler{
 	 * @return void
 	 */
 	public function scheduleAsyncTask(AsyncTask $task){
+		if($task->getTaskId() !== null){
+			throw new \UnexpectedValueException("Attempt to schedule the same AsyncTask instance twice");
+		}
 		$id = $this->nextId();
 		$task->setTaskId($id);
 		$this->asyncPool->submitTask($task);
@@ -90,6 +93,9 @@ class ServerScheduler{
 	 * @return void
 	 */
 	public function scheduleAsyncTaskToWorker(AsyncTask $task, $worker){
+		if($task->getTaskId() !== null){
+			throw new \UnexpectedValueException("Attempt to schedule the same AsyncTask instance twice");
+		}
 		$id = $this->nextId();
 		$task->setTaskId($id);
 		$this->asyncPool->submitTaskToWorker($task, $worker);
