367fa411619ab6a4f54462032cdfc58fd99a8af2
Dylan K. Taylor <odigiman@gmail.com>
2016-11-27 19:29:47 +0000

    Remove GUI remnants from start.cmd

--- start.cmd
+++ start.cmd
@@ -21,12 +21,8 @@ if exist PocketMine-MP.phar (
 	)
 )
 
-REM if exist bin\php\php_wxwidgets.dll (
-REM 	%PHP_BINARY% %POCKETMINE_FILE% --enable-gui %*
-REM ) else (
-	if exist bin\mintty.exe (
-		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
-	) else (
-		%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
-	)
-REM )
+if exist bin\mintty.exe (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+) else (
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+)
