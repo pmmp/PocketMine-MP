166241bdc72f8e2aa887de10c2dcd17ff3b7511c
Dylan K. Taylor <odigiman@gmail.com>
2016-12-01 12:01:42 +0000

    Do not perform scheduled updates on still lava/water

--- src/pocketmine/block/StillWater.php
+++ src/pocketmine/block/StillWater.php
@@ -21,9 +21,18 @@
 
 namespace pocketmine\block;
 
+use pocketmine\level\Level;
+
 class StillWater extends Water{
 
 	protected $id = self::STILL_WATER;
+
+	public function onUpdate($type){
+		if($type !== Level::BLOCK_UPDATE_SCHEDULED){
+			return parent::onUpdate($type);
+		}
+		return false;
+	}
 
 	public function getName(){
 		return "Still Water";
