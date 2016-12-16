0380c789262445603ab3e70f122fa80f1562de3b
Dylan K. Taylor <odigiman@gmail.com>
2016-12-16 16:33:31 +0000

    Remove redundant override in Fallable

--- src/pocketmine/block/Fallable.php
+++ src/pocketmine/block/Fallable.php
@@ -35,12 +35,6 @@ use pocketmine\Player;
 
 abstract class Fallable extends Solid{
 
-	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
-		$ret = $this->getLevel()->setBlock($this, $this, true, true);
-
-		return $ret;
-	}
-
 	public function onUpdate($type){
 		if($type === Level::BLOCK_UPDATE_NORMAL){
 			$down = $this->getSide(Vector3::SIDE_DOWN);
