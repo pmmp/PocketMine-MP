fa358668e91caacbebfbf1d55952186409ed3b10
Oleg Basalyga <basalygaoleg@yandex.ru>
2016-12-25 23:19:52 +0500

    New event packets

--- src/pocketmine/network/protocol/LevelEventPacket.php
+++ src/pocketmine/network/protocol/LevelEventPacket.php
@@ -59,8 +59,15 @@ class LevelEventPacket extends DataPacket{
 	const EVENT_STOP_RAIN = 3003;
 	const EVENT_STOP_THUNDER = 3004;
 
+	const EVENT_SOUND_BUTTON_CLICK = 3500;
+	const EVENT_SOUND_EXPLODE = 3501;
+ 	
+ 	const EVENT_SOUND_SPELL = 3504;
+ 	const EVENT_SOUND_SPLASH = 3506;
+ 	const EVENT_SOUND_GRAY_SPLASH = 3507;
+	
 	const EVENT_SET_DATA = 4000;
-
+	
 	const EVENT_PLAYERS_SLEEPING = 9800;
 
 	const EVENT_ADD_PARTICLE_MASK = 0x4000;
