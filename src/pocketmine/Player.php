522167a131e3642112df43608c14e6ad5fe4719f
xFlare <boughousp19@student.riverdell.org>
2017-01-12 09:09:14 -0500

    Fixed unused variable being created.

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1908,7 +1908,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 					$this->forceMovement = new Vector3($this->x, $this->y, $this->z);
 				}
 
-				if($this->forceMovement instanceof Vector3 and (($dist = $newPos->distanceSquared($this->forceMovement)) > 0.1 or $revert)){
+				if($this->forceMovement instanceof Vector3 and ($newPos->distanceSquared($this->forceMovement) > 0.1 or $revert)){
 					$this->sendPosition($this->forceMovement, $packet->yaw, $packet->pitch);
 				}else{
 					$packet->yaw %= 360;
