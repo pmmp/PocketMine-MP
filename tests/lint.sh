3ab9722a9f10d66f49783a1a428e946afcbee95e
Dylan K. Taylor <odigiman@gmail.com>
2017-01-22 15:26:16 +0000

    Add DevTools as submodule, build DevTools for Travis Test

--- tests/lint.sh
+++ tests/lint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
+PHP_BINARY="php"
+
+while getopts "p:" OPTION 2> /dev/null; do
+	case ${OPTION} in
+		p)
+			PHP_BINARY="$OPTARG"
+			;;
+	esac
+done
+
 echo Running PHP lint scans...
 shopt -s globstar
 for file in src/pocketmine/*.php src/pocketmine/**/*.php; do
-    OUTPUT=`php -l "$file"`
-    [ $? -ne 0 ] && echo -n "$OUTPUT" && exit 1
+	OUTPUT=`"$PHP_BINARY" -l "$file"`
+	[ $? -ne 0 ] && echo -n "$OUTPUT" && exit 1
 done
 echo Lint scan completed successfully.
