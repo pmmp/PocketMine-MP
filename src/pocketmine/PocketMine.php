d823ff18d8f091286149f2a42969046b7c501365
Dylan K. Taylor <odigiman@gmail.com>
2017-03-02 20:41:12 +0000

    Bump API version to 3.0.0-ALPHA5 (not finalized)

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -74,7 +74,7 @@ namespace pocketmine {
 	use raklib\RakLib;
 
 	const VERSION = "1.6.2dev";
-	const API_VERSION = "3.0.0-ALPHA4";
+	const API_VERSION = "3.0.0-ALPHA5";
 	const CODENAME = "Unleashed";
 
 	/*
