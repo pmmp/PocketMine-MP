a653b555503ac1ca780bfe8ee964c0a9df1c39dc
TheDiamondYT1 <luke.wyatt21@gmail.com>
2017-03-05 23:23:40 +0000

    Add fireball item and partial functionality

---
+++ src/pocketmine/item/Fireball.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+namespace pocketmine\item;
+
+use pocketmine\block\Block;
+use pocketmine\block\Fire;
+use pocketmine\block\Solid;
+use pocketmine\level\Level;
+use pocketmine\Player;
+
+class Fireball extends Item{
+    public function __construct($meta = 0, $count = 1){
+        parent::__construct(self::FIREBALL, $meta, $count, "Fire Charge");
+    }
+    
+    public function canBeActivated(){
+        return true;
+    }
+    
+    public function onActivate(Level $level, Player $player, Block $block, Block $target, $face, $fx, $fy, $fz){
+        if($block->getId() === self::AIR and ($target instanceof Solid)){
+            $level->setBlock($block, new Fire(), true);
+            return true;
+        }
+        return false;
+    }
+}
