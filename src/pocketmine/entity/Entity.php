c925845173b3652681cfa45a7ed798d159a4a094
Dylan K. Taylor <odigiman@gmail.com>
2017-03-09 21:33:55 +0000

    Added forceSend for attribute value setting, fixed slowness >= 7 removed client-side when sprinting

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -423,7 +423,7 @@ abstract class Entity extends Location implements Metadatable{
 		if($value !== $this->isSprinting()){
 			$this->setDataFlag(self::DATA_FLAGS, self::DATA_FLAG_SPRINTING, (bool) $value);
 			$attr = $this->attributeMap->getAttribute(Attribute::MOVEMENT_SPEED);
-			$attr->setValue($value ? ($attr->getValue() * 1.3) : ($attr->getValue() / 1.3));
+			$attr->setValue($value ? ($attr->getValue() * 1.3) : ($attr->getValue() / 1.3), false, true);
 		}
 	}
 
