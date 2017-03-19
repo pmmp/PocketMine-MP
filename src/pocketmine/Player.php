4638ccbb68c398b72a6807a81c1b2a3369df07f3
Dylan K. Taylor <odigiman@gmail.com>
2017-03-19 21:58:12 +0000

    Remove this workaround (client bug fixed in 1.0.5 beta)

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2092,10 +2092,6 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 	public function handleMovePlayer(MovePlayerPacket $packet) : bool{
 		$newPos = new Vector3($packet->x, $packet->y - $this->getEyeHeight(), $packet->z);
 
-		if($newPos->distanceSquared($this) == 0 and ($packet->yaw % 360) === $this->yaw and ($packet->pitch % 360) === $this->pitch){ //player hasn't moved, just client spamming packets
-			return true;
-		}
-
 		$revert = false;
 		if(!$this->isAlive() or $this->spawned !== true){
 			$revert = true;
