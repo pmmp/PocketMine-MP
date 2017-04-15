012afcc7b774b60789ac0ff4d21d15bd1e69672d
SOFe <sofe2038@gmail.com>
2017-04-15 12:00:34 +0800

    Created BulkCurlTask

--- src/pocketmine/utils/Utils.php
+++ src/pocketmine/utils/Utils.php
@@ -402,17 +402,17 @@ class Utils{
 	 * General cURL shorthand function.
 	 * NOTE: This is a blocking operation and can take a significant amount of time. It is inadvisable to use this method on the main thread.
 	 *
-	 * @param string    $page
-	 * @param float|int $timeout The maximum connect timeout and timeout in seconds, correct to ms.
-	 * @param string[] $extraHeaders extra headers to send as a plain string array
-	 * @param array    $extraOpts extra CURLOPT_* to set as an [opt => value] map
-	 * @param callable|null $onSuccess function to be called if there is no error. Accepts a resource argument as the cURL handle.
+	 * @param string        $page
+	 * @param float|int     $timeout      The maximum connect timeout and timeout in seconds, correct to ms.
+	 * @param string[]      $extraHeaders extra headers to send as a plain string array
+	 * @param array         $extraOpts    extra CURLOPT_* to set as an [opt => value] map
+	 * @param callable|null $onSuccess    function to be called if there is no error. Accepts a resource argument as the cURL handle.
 	 *
 	 * @return array a plain array of three [result body : string, headers : array[], HTTP response code : int]. Headers are grouped by requests with header name as keys and header value as values
 	 *
 	 * @throws \RuntimeException if a cURL error occurs
 	 */
-	public static function simpleCurl(string $page, $timeout, array $extraHeaders = [], array $extraOpts = [], callable $onSuccess = null){
+	public static function simpleCurl(string $page, $timeout = 10, array $extraHeaders = [], array $extraOpts = [], callable $onSuccess = null){
 		if(Utils::$online === false){
 			throw new \RuntimeException("Server is offline");
 		}
