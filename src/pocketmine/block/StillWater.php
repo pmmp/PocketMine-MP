33cc2075cccaf5203a15b20d69d7b90abab2316f
Dylan K. Taylor <odigiman@gmail.com>
2017-04-18 18:55:01 +0100

    Added melting for ice and snow layers

--- src/pocketmine/block/StillWater.php
+++ src/pocketmine/block/StillWater.php
@@ -28,6 +28,7 @@ class StillWater extends FlowingWater{
 	protected $id = self::STILL_WATER;
 
 	public function onUpdate($type){
+		//TODO: add freezing in cold biomes
 		if($type !== Level::BLOCK_UPDATE_SCHEDULED){
 			return parent::onUpdate($type);
 		}
