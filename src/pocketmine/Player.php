4a76d973c2dd8b5fb834e69b487b68834ae2c3bb
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2017-05-05 13:10:30 +0300

    Fix $ev->getFace() returning incorrect value.

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -3241,7 +3241,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 
 		$tile = $this->level->getTile($this->temporalVector->setComponents($packet->x, $packet->y, $packet->z));
 		if($tile instanceof ItemFrame){
-			$ev = new PlayerInteractEvent($this, $this->inventory->getItemInHand(), $tile->getBlock(), $tile->getBlock()->getDamage(), PlayerInteractEvent::LEFT_CLICK_BLOCK);
+			$ev = new PlayerInteractEvent($this, $this->inventory->getItemInHand(), $tile->getBlock(), 5 - $tile->getBlock()->getDamage(), PlayerInteractEvent::LEFT_CLICK_BLOCK);
 			$this->server->getPluginManager()->callEvent($ev);
 
 			if($this->isSpectator()){
