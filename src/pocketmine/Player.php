11f35d28c2f58e4db4189d9038be5e97e3d5edf3
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2017-05-05 13:18:58 +0300

    Call PlayerInteractEventm when receiving ItemFrameDropItemPacket to allow plugins to prevent item frame item removal (#887)

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -3240,7 +3240,14 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 
 		$tile = $this->level->getTile($this->temporalVector->setComponents($packet->x, $packet->y, $packet->z));
 		if($tile instanceof ItemFrame){
+			$ev = new PlayerInteractEvent($this, $this->inventory->getItemInHand(), $tile->getBlock(), 5 - $tile->getBlock()->getDamage(), PlayerInteractEvent::LEFT_CLICK_BLOCK);
+			$this->server->getPluginManager()->callEvent($ev);
+
 			if($this->isSpectator()){
+				$ev->setCancelled();
+			}
+
+			if($ev->isCancelled()){
 				$tile->spawnTo($this);
 				return true;
 			}
