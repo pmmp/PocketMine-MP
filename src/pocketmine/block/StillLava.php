210bdc2436269ca4d634aa791b1054e594769baa
Dylan K. Taylor <odigiman@gmail.com>
2017-05-07 11:15:52 +0100

    Fixed liquid CPU leaks and some flow issues Liquid still needs a total rewrite, but at least it won't constantly waste CPU anymore.

--- src/pocketmine/block/StillLava.php
+++ src/pocketmine/block/StillLava.php
@@ -29,13 +29,6 @@ class StillLava extends Lava{
 
 	protected $id = self::STILL_LAVA;
 
-	public function onUpdate($type){
-		if($type !== Level::BLOCK_UPDATE_SCHEDULED){
-			return parent::onUpdate($type);
-		}
-		return false;
-	}
-
 	public function getName(){
 		return "Still Lava";
 	}
--- src/pocketmine/block/StillWater.php
+++ src/pocketmine/block/StillWater.php
@@ -29,14 +29,6 @@ class StillWater extends Water{
 
 	protected $id = self::STILL_WATER;
 
-	public function onUpdate($type){
-		//TODO: add freezing in cold biomes
-		if($type !== Level::BLOCK_UPDATE_SCHEDULED){
-			return parent::onUpdate($type);
-		}
-		return false;
-	}
-
 	public function getName(){
 		return "Still Water";
 	}
