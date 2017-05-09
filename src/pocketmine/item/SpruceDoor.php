66bff4ce330cf369e62a0a44c2cacceb24368c54
Gerald Ebberink <gerald@ebberink.nl>
2017-05-09 22:56:23 +0200

    Changed the naming of the spruce door item

--- src/pocketmine/item/SpruceDoor.php
+++ src/pocketmine/item/SpruceDoor.php
@@ -26,7 +26,7 @@ use pocketmine\block\Block;
 class SpruceDoor extends Item{
 	public function __construct($meta = 0, $count = 1){
 		$this->block = Block::get(Item::SPRUCE_DOOR_BLOCK);
-		parent::__construct(self::SPRUCE_DOOR, 0, $count, "Wooden Door");
+		parent::__construct(self::SPRUCE_DOOR, 0, $count, "Spruce Door");
 	}
 
 	public function getMaxStackSize(){
