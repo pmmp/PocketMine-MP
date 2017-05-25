4062888628b533f7274190f55d9e9b93bbb1b93a
Dylan K. Taylor <odigiman@gmail.com>
2017-05-25 10:41:55 +0100

    Fixed splash potions

--- src/pocketmine/entity/projectile/SplashPotion.php
+++ src/pocketmine/entity/projectile/SplashPotion.php
@@ -196,7 +196,7 @@ class SplashPotion extends Projectile{
 	public function spawnTo(Player $player){
 		$pk = new AddEntityPacket();
 		$pk->type = SplashPotion::NETWORK_ID;
-		$pk->eid = $this->getId();
+		$pk->entityRuntimeId = $this->getId();
 		$pk->x = $this->x;
 		$pk->y = $this->y;
 		$pk->z = $this->z;
