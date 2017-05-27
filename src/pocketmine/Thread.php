9f4f60d0bccbde1715db2acceadc717d4fd2ab80
Dylan K. Taylor <odigiman@gmail.com>
2017-05-26 20:21:50 -0400

    updates for php 7.1

--- src/pocketmine/Thread.php
+++ src/pocketmine/Thread.php
@@ -60,7 +60,7 @@ abstract class Thread extends \Thread{
 		}
 	}
 
-	public function start(int $options = PTHREADS_INHERIT_ALL){
+	public function start(?int $options = \PTHREADS_INHERIT_ALL){
 		ThreadManager::getInstance()->add($this);
 
 		if(!$this->isRunning() and !$this->isJoined() and !$this->isTerminated()){
