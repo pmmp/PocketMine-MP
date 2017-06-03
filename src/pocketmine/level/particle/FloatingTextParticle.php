4ba3a3af3d129948d49e3426720776900b383f52
Dylan K. Taylor <odigiman@gmail.com>
2017-06-03 20:51:48 +0100

    fix FloatingTextParticle

--- src/pocketmine/level/particle/FloatingTextParticle.php
+++ src/pocketmine/level/particle/FloatingTextParticle.php
@@ -66,7 +66,7 @@ class FloatingTextParticle extends Particle{
 		$p = [];
 
 		if($this->entityId === null){
-			$this->entityId = bcadd("1095216660480", mt_rand(0, 0x7fffffff)); //No conflict with other things
+			$this->entityId = (int) bcadd("1095216660480", mt_rand(0, 0x7fffffff)); //No conflict with other things
 		}else{
 			$pk0 = new RemoveEntityPacket();
 			$pk0->eid = $this->entityId;
