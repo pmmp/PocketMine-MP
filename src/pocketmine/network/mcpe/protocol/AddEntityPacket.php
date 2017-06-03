5baf59ca5687d4f27e35e1d06e4aa33fa49afbcb
Dylan K. Taylor <odigiman@gmail.com>
2017-06-03 20:25:24 +0100

    fix #990

--- src/pocketmine/network/mcpe/protocol/AddEntityPacket.php
+++ src/pocketmine/network/mcpe/protocol/AddEntityPacket.php
@@ -35,9 +35,9 @@ class AddEntityPacket extends DataPacket{
 	public $x;
 	public $y;
 	public $z;
-	public $speedX;
-	public $speedY;
-	public $speedZ;
+	public $speedX = 0.0;
+	public $speedY = 0.0;
+	public $speedZ = 0.0;
 	public $yaw = 0.0;
 	public $pitch = 0.0;
 	/** @var Attribute[] */
