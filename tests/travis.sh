092e3b1545635e58bffa8e3c7c10bec2beaaf86a
Apple <ytapplegamer@gmail.com>
2017-06-06 02:17:12 -0400

    oops

--- tests/travis.sh
+++ tests/travis.sh
@@ -26,7 +26,7 @@ cp -r tests/plugins/PocketMine-DevTools ./plugins
 rm -rf ./plugins/PocketMine-DevTools
 
 echo -e "version\nmakeserver\nstop\n" | "$PHP_BINARY" src/pocketmine/PocketMine.php --no-wizard --disable-ansi --disable-readline --debug.level=2
-if ls plugins/DevTools/PocketMine*.phar >/dev/null 2>&1; then
+if ls plugins/DevTools/TesseractPM*.phar >/dev/null 2>&1; then
     echo Server phar created successfully.
 else
     echo No phar created!
