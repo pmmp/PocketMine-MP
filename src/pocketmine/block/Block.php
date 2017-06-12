8fc6e53978a1559008547cf7ef0741522597b7ca
Dylan K. Taylor <odigiman@gmail.com>
2017-06-12 16:12:18 +0100

    Added concrete

--- src/pocketmine/block/Block.php
+++ src/pocketmine/block/Block.php
@@ -273,6 +273,7 @@ class Block extends Position implements BlockIds, Metadatable{
 			self::registerBlock((new GlazedTerracotta(Block::GREEN_GLAZED_TERRACOTTA))->setName("Green Glazed Terracotta"));
 			self::registerBlock((new GlazedTerracotta(Block::RED_GLAZED_TERRACOTTA))->setName("Red Glazed Terracotta"));
 			self::registerBlock((new GlazedTerracotta(Block::BLACK_GLAZED_TERRACOTTA))->setName("Black Glazed Terracotta"));
+			self::registerBlock((new Block(Block::CONCRETE))->setName("Concrete")->setToolType(Tool::TYPE_PICKAXE)->setRequiredHarvestLevel(TieredTool::TIER_WOODEN)->setHardness(1.8));
 
 			self::registerBlock(new Podzol());
 			self::registerBlock(new Beetroot());
