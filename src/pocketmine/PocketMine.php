7f99d9019ac30dcd41aecdf8a30c7378ddb1dfc6
Dylan K. Taylor <odigiman@gmail.com>
2017-07-05 19:18:49 +0100

    API bump and submodule updates

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -79,7 +79,7 @@ namespace pocketmine {
 	use raklib\RakLib;
 
 	const VERSION = "1.6.2dev";
-	const API_VERSION = "3.0.0-ALPHA6";
+	const API_VERSION = "3.0.0-ALPHA7";
 	const CODENAME = "Unleashed";
 
 	/*
