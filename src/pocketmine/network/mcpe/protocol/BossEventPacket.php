6bcc8cea8e5724dacb8295fcffbdbc3c02cc7a90
Dylan K. Taylor <odigiman@gmail.com>
2017-07-11 15:32:46 +0100

    Fixed BossEventPacket breaks, now shut up and go away

--- src/pocketmine/network/mcpe/protocol/BossEventPacket.php
+++ src/pocketmine/network/mcpe/protocol/BossEventPacket.php
@@ -72,9 +72,11 @@ class BossEventPacket extends DataPacket{
 			case self::TYPE_UNREGISTER_PLAYER:
 				$this->playerEid = $this->getEntityUniqueId();
 				break;
+			/** @noinspection PhpMissingBreakStatementInspection */
 			case self::TYPE_SHOW:
 				$this->title = $this->getString();
 				$this->healthPercent = $this->getLFloat();
+			/** @noinspection PhpMissingBreakStatementInspection */
 			case self::TYPE_UNKNOWN_6:
 				$this->unknownShort = $this->getLShort();
 			case self::TYPE_TEXTURE:
@@ -86,6 +88,7 @@ class BossEventPacket extends DataPacket{
 				break;
 			case self::TYPE_TITLE:
 				$this->title = $this->getString();
+				break;
 			default:
 				break;
 		}
@@ -99,9 +102,11 @@ class BossEventPacket extends DataPacket{
 			case self::TYPE_UNREGISTER_PLAYER:
 				$this->putEntityUniqueId($this->playerEid);
 				break;
+			/** @noinspection PhpMissingBreakStatementInspection */
 			case self::TYPE_SHOW:
 				$this->putString($this->title);
 				$this->putLFloat($this->healthPercent);
+			/** @noinspection PhpMissingBreakStatementInspection */
 			case self::TYPE_UNKNOWN_6:
 				$this->putLShort($this->unknownShort);
 			case self::TYPE_TEXTURE:
