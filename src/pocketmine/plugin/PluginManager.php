d95c7d6951590fd3af804656b5cd318f7b883a11
Dylan K. Taylor <odigiman@gmail.com>
2017-07-26 19:19:08 +0100

    Added capability to specify compatible protocol versions in plugin.yml

--- src/pocketmine/plugin/PluginManager.php
+++ src/pocketmine/plugin/PluginManager.php
@@ -32,6 +32,7 @@ use pocketmine\event\HandlerList;
 use pocketmine\event\Listener;
 use pocketmine\event\Timings;
 use pocketmine\event\TimingsHandler;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\permission\Permissible;
 use pocketmine\permission\Permission;
 use pocketmine\Server;
@@ -256,6 +257,24 @@ class PluginManager{
 							if($compatible === false){
 								$this->server->getLogger()->error($this->server->getLanguage()->translateString("pocketmine.plugin.loadError", [$name, "%pocketmine.plugin.incompatibleAPI"]));
 								continue;
+							}
+
+							if(count($pluginMcpeProtocols = $description->getCompatibleMcpeProtocols()) > 0){
+								$serverMcpeProtocols = [ProtocolInfo::CURRENT_PROTOCOL];
+								if(count(array_intersect($pluginMcpeProtocols, $serverMcpeProtocols)) === 0){
+									$this->server->getLogger()->error($this->server->getLanguage()->translateString(
+										"pocketmine.plugin.loadError",
+										[
+											$name,
+											$this->server->getLanguage()->translateString(
+												"%pocketmine.plugin.incompatibleProtocol",
+												[
+													implode(", ", $pluginMcpeProtocols)
+												])
+										]
+									));
+									continue;
+								}
 							}
 
 							$plugins[$name] = $file;
