8a02dbb15a1a291b83a672e383562c26c3df125f
Dylan K. Taylor <odigiman@gmail.com>
2017-08-03 08:45:56 +0100

    fix crash when var_dumping packets

--- src/pocketmine/network/mcpe/protocol/DataPacket.php
+++ src/pocketmine/network/mcpe/protocol/DataPacket.php
@@ -107,7 +107,7 @@ abstract class DataPacket extends BinaryStream{
 	public function __debugInfo(){
 		$data = [];
 		foreach($this as $k => $v){
-			if($k === "buffer"){
+			if($k === "buffer" and is_string($v)){
 				$data[$k] = bin2hex($v);
 			}elseif(is_string($v) or (is_object($v) and method_exists($v, "__toString"))){
 				$data[$k] = Utils::printable((string) $v);
