e84d4cf97cfce42b80f963c737474e68b748970b
Dylan K. Taylor <odigiman@gmail.com>
2017-10-04 13:02:45 +0100

    fix field names in BookEditPacket

--- src/pocketmine/network/mcpe/protocol/BookEditPacket.php
+++ src/pocketmine/network/mcpe/protocol/BookEditPacket.php
@@ -90,8 +90,8 @@ class BookEditPacket extends DataPacket{
 			case self::TYPE_REPLACE_PAGE:
 			case self::TYPE_ADD_PAGE:
 				$this->putByte($this->pageNumber);
-				$this->putString($this->content1);
-				$this->putString($this->content2);
+				$this->putString($this->text);
+				$this->putString($this->photoName);
 				break;
 			case self::TYPE_DELETE_PAGE:
 				$this->putByte($this->pageNumber);
