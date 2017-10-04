0d69d745ffce491b34534b325184ae66ab028850
Dylan K. Taylor <odigiman@gmail.com>
2017-10-04 13:04:39 +0100

    remove unused import

--- src/pocketmine/item/WritableBook.php
+++ src/pocketmine/item/WritableBook.php
@@ -24,7 +24,6 @@ declare(strict_types=1);
 namespace pocketmine\item;
 
 use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\IntTag;
 use pocketmine\nbt\tag\ListTag;
 use pocketmine\nbt\tag\StringTag;
 
