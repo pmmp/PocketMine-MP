a82efc44aadd24c8c427a7700bcbadf3890e3455
Dylan K. Taylor <odigiman@gmail.com>
2017-10-24 13:29:02 +0100

    wtf

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2626,7 +2626,7 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 
 				$target = $this->level->getBlock($pos);
 				$ev = new PlayerInteractEvent($this, $this->inventory->getItemInHand(), $target, $packet->face, $target->getId() === 0 ? PlayerInteractEvent::LEFT_CLICK_AIR : PlayerInteractEvent::LEFT_CLICK_BLOCK);
-				if(!$this->level->checkSpawnProtection($this, $target)){
+				if($this->level->checkSpawnProtection($this, $target)){
 					$ev->setCancelled();
 				}
 
