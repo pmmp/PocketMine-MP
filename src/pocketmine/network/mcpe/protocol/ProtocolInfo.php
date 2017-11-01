dea2942062495f531ff1f8b4cd353141f3b1d620
Dylan K. Taylor <odigiman@gmail.com>
2017-11-01 16:14:12 +0000

    bump to 141

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -39,15 +39,15 @@ interface ProtocolInfo{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	const CURRENT_PROTOCOL = 140;
+	const CURRENT_PROTOCOL = 141;
 	/**
 	 * Current Minecraft PE version reported by the server. This is usually the earliest currently supported version.
 	 */
-	const MINECRAFT_VERSION = 'v1.2.5.12 beta';
+	const MINECRAFT_VERSION = 'v1.2.5.15 beta';
 	/**
 	 * Version number sent to clients in ping responses.
 	 */
-	const MINECRAFT_VERSION_NETWORK = '1.2.5.12';
+	const MINECRAFT_VERSION_NETWORK = '1.2.5.15';
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
