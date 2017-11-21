88ad43971ad51b87d566cbed090d4e769028ce95
Dylan K. Taylor <odigiman@gmail.com>
2017-11-21 16:50:27 +0000

    constant visibility

--- src/pocketmine/network/mcpe/protocol/types/CommandOriginData.php
+++ src/pocketmine/network/mcpe/protocol/types/CommandOriginData.php
@@ -26,18 +26,18 @@ namespace pocketmine\network\mcpe\protocol\types;
 use pocketmine\utils\UUID;
 
 class CommandOriginData{
-	const ORIGIN_PLAYER = 0;
-	const ORIGIN_BLOCK = 1;
-	const ORIGIN_MINECART_BLOCK = 2;
-	const ORIGIN_DEV_CONSOLE = 3;
-	const ORIGIN_TEST = 4;
-	const ORIGIN_AUTOMATION_PLAYER = 5;
-	const ORIGIN_CLIENT_AUTOMATION = 6;
-	const ORIGIN_DEDICATED_SERVER = 7;
-	const ORIGIN_ENTITY = 8;
-	const ORIGIN_VIRTUAL = 9;
-	const ORIGIN_GAME_ARGUMENT = 10;
-	const ORIGIN_ENTITY_SERVER = 11; //???
+	public const ORIGIN_PLAYER = 0;
+	public const ORIGIN_BLOCK = 1;
+	public const ORIGIN_MINECART_BLOCK = 2;
+	public const ORIGIN_DEV_CONSOLE = 3;
+	public const ORIGIN_TEST = 4;
+	public const ORIGIN_AUTOMATION_PLAYER = 5;
+	public const ORIGIN_CLIENT_AUTOMATION = 6;
+	public const ORIGIN_DEDICATED_SERVER = 7;
+	public const ORIGIN_ENTITY = 8;
+	public const ORIGIN_VIRTUAL = 9;
+	public const ORIGIN_GAME_ARGUMENT = 10;
+	public const ORIGIN_ENTITY_SERVER = 11; //???
 
 	/** @var int */
 	public $type;
