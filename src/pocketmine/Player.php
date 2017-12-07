7a916fdf537b68f7f370fd5baa1f05bad98f9e03
Dylan K. Taylor <odigiman@gmail.com>
2017-12-07 09:49:50 +0000

    Fixed returning $this in onSubmit() or onClose()

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -3348,11 +3348,11 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 			$form = array_shift($this->formQueue);
 		}
 
+		$this->sentFormId = null;
+		$this->sentForm = null;
+
 		if($form !== null){
-			$this->sendFormRequestPacket($form);
-		}else{
-			$this->sentFormId = null;
-			$this->sentForm = null;
+			$this->sendForm($form);
 		}
 
 		return true;
