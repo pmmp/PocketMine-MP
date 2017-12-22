a5c3fbdd7a648d4a774e167570d2fc0165c66a80
Dylan K. Taylor <odigiman@gmail.com>
2017-12-22 20:38:39 +0000

    Player: Remove obsolete maxDiff parameter from canInteract()

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1743,11 +1743,10 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 	 *
 	 * @param Vector3 $pos
 	 * @param         $maxDistance
-	 * @param float   $maxDiff default 0.71 (approximately sqrt(2) / 2, half of the diagonal width of a block)
 	 *
 	 * @return bool
 	 */
-	public function canInteract(Vector3 $pos, $maxDistance, float $maxDiff = 0.71) : bool{
+	public function canInteract(Vector3 $pos, $maxDistance) : bool{
 		$eyePos = $this->getPosition()->add(0, $this->getEyeHeight(), 0);
 		if($eyePos->distanceSquared($pos) > $maxDistance ** 2){
 			return false;
@@ -1756,7 +1755,7 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 		$dV = $this->getDirectionVector();
 		$eyeDot = $dV->dot($eyePos);
 		$targetDot = $dV->dot($pos);
-		return ($targetDot - $eyeDot) >= -$maxDiff;
+		return ($targetDot - $eyeDot) >= 0;
 	}
 
 	protected function initHumanData(){
