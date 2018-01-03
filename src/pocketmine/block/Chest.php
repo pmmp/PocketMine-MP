6ce728169ee57d9ad4c1984f787b6dcacea3bc31
Dylan K. Taylor <odigiman@gmail.com>
2018-01-03 12:08:35 +0000

    Chest: Remove redundant unpairing code from onBreak() This is already done in the Level.

--- src/pocketmine/block/Chest.php
+++ src/pocketmine/block/Chest.php
@@ -101,16 +101,6 @@ class Chest extends Transparent{
 		return true;
 	}
 
-	public function onBreak(Item $item, Player $player = null) : bool{
-		$t = $this->getLevel()->getTile($this);
-		if($t instanceof TileChest){
-			$t->unpair();
-		}
-		$this->getLevel()->setBlock($this, BlockFactory::get(Block::AIR), true, true);
-
-		return true;
-	}
-
 	public function onActivate(Item $item, Player $player = null) : bool{
 		if($player instanceof Player){
 
