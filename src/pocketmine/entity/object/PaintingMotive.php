c7f8796136f0414927ddca403296b42da8b2c84a
Dylan K. Taylor <odigiman@gmail.com>
2018-03-07 09:03:30 +0000

    Implemented Paintings (#2073)
    This supports vanilla placement of paintings, with overlap and collision checking.
    Paintings are removed when a block is placed inside them or if any of their supporting blocks are removed.
    
    As per vanilla, a random painting is chosen from the largest subset that will fit into the given space.

---
+++ src/pocketmine/entity/object/PaintingMotive.php
@@ -0,0 +1,127 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\entity\object;
+
+class PaintingMotive{
+	/** @var PaintingMotive[] */
+	protected static $motives = [];
+
+	public static function init() : void{
+		foreach([
+			new PaintingMotive(1, 1, "Alban"),
+			new PaintingMotive(1, 1, "Aztec"),
+			new PaintingMotive(1, 1, "Aztec2"),
+			new PaintingMotive(1, 1, "Bomb"),
+			new PaintingMotive(1, 1, "Kebab"),
+			new PaintingMotive(1, 1, "Plant"),
+			new PaintingMotive(1, 1, "Wasteland"),
+			new PaintingMotive(1, 2, "Graham"),
+			new PaintingMotive(1, 2, "Wanderer"),
+			new PaintingMotive(2, 1, "Courbet"),
+			new PaintingMotive(2, 1, "Creebet"),
+			new PaintingMotive(2, 1, "Pool"),
+			new PaintingMotive(2, 1, "Sea"),
+			new PaintingMotive(2, 1, "Sunset"),
+			new PaintingMotive(2, 2, "Bust"),
+			new PaintingMotive(2, 2, "Earth"),
+			new PaintingMotive(2, 2, "Fire"),
+			new PaintingMotive(2, 2, "Match"),
+			new PaintingMotive(2, 2, "SkullAndRoses"),
+			new PaintingMotive(2, 2, "Stage"),
+			new PaintingMotive(2, 2, "Void"),
+			new PaintingMotive(2, 2, "Water"),
+			new PaintingMotive(2, 2, "Wind"),
+			new PaintingMotive(2, 2, "Wither"),
+			new PaintingMotive(4, 2, "Fighters"),
+			new PaintingMotive(4, 3, "DonkeyKong"),
+			new PaintingMotive(4, 3, "Skeleton"),
+			new PaintingMotive(4, 4, "BurningSkull"),
+			new PaintingMotive(4, 4, "Pigscene"),
+			new PaintingMotive(4, 4, "Pointer")
+		] as $motive){
+			self::registerMotive($motive);
+		}
+	}
+
+	/**
+	 * @param PaintingMotive $motive
+	 */
+	public static function registerMotive(PaintingMotive $motive) : void{
+		self::$motives[$motive->getName()] = $motive;
+	}
+
+	/**
+	 * @param string $name
+	 * @return PaintingMotive|null
+	 */
+	public static function getMotiveByName(string $name) : ?PaintingMotive{
+		return self::$motives[$name] ?? null;
+	}
+
+	/**
+	 * @return PaintingMotive[]
+	 */
+	public static function getAll() : array{
+		return self::$motives;
+	}
+
+	/** @var string */
+	protected $name;
+	/** @var int */
+	protected $width;
+	/** @var int */
+	protected $height;
+
+
+	public function __construct(int $width, int $height, string $name){
+		$this->name = $name;
+		$this->width = $width;
+		$this->height = $height;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName() : string{
+		return $this->name;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getWidth() : int{
+		return $this->width;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getHeight() : int{
+		return $this->height;
+	}
+
+	public function __toString() : string{
+		return "PaintingMotive(name: " . $this->getName() . ", height: " . $this->getHeight() . ", width: " . $this->getWidth() . ")";
+	}
+}
---
+++ src/pocketmine/item/PaintingItem.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\item;
+
+use pocketmine\block\Block;
+use pocketmine\entity\Entity;
+use pocketmine\entity\object\Painting;
+use pocketmine\entity\object\PaintingMotive;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use pocketmine\Player;
+
+class PaintingItem extends Item{
+	public function __construct(int $meta = 0){
+		parent::__construct(self::PAINTING, $meta, "Painting");
+	}
+
+	public function onActivate(Player $player, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector) : bool{
+		if(!$blockClicked->isTransparent() and $face > 1 and !$blockReplace->isSolid()){
+			/** @var PaintingMotive[] $motives */
+			$motives = [];
+
+			$totalDimension = 0;
+			foreach(PaintingMotive::getAll() as $motive){
+				$currentTotalDimension = $motive->getHeight() + $motive->getWidth();
+				if($currentTotalDimension < $totalDimension){
+					continue;
+				}
+
+				if(Painting::canFit($player->level, $blockReplace, $face, true, $motive)){
+					if($currentTotalDimension > $totalDimension){
+						$totalDimension = $currentTotalDimension;
+						/*
+						 * This drops all motive possibilities smaller than this
+						 * We use the total of height + width to allow equal chance of horizontal/vertical paintings
+						 * when there is an L-shape of space available.
+						 */
+						$motives = [];
+					}
+
+					$motives[] = $motive;
+				}
+			}
+
+			if(empty($motives)){ //No space available
+				return false;
+			}
+
+			/** @var PaintingMotive $motive */
+			$motive = $motives[array_rand($motives)];
+
+			static $directions = [
+				Vector3::SIDE_SOUTH => 0,
+				Vector3::SIDE_WEST => 1,
+				Vector3::SIDE_NORTH => 2,
+				Vector3::SIDE_EAST => 3
+			];
+
+			$direction = $directions[$face] ?? -1;
+			if($direction === -1){
+				return false;
+			}
+
+			$nbt = Entity::createBaseNBT($blockReplace, null, $direction * 90, 0);
+			$nbt->setByte("Direction", $direction);
+			$nbt->setString("Motive", $motive->getName());
+			$nbt->setInt("TileX", $blockClicked->getFloorX());
+			$nbt->setInt("TileY", $blockClicked->getFloorY());
+			$nbt->setInt("TileZ", $blockClicked->getFloorZ());
+
+			$entity = Entity::createEntity("Painting", $blockReplace->getLevel(), $nbt);
+
+			if($entity instanceof Entity){
+				--$this->count;
+				$entity->spawnToAll();
+
+				$player->getLevel()->broadcastLevelEvent($blockReplace->add(0.5, 0.5, 0.5), LevelEventPacket::EVENT_SOUND_ITEMFRAME_PLACE); //item frame and painting have the same sound
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
