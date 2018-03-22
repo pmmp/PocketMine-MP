526f05631ecdc8be777f4f692dc190b4010c4bbc
Dylan K. Taylor <odigiman@gmail.com>
2018-03-22 19:54:01 +0000

    fix metadata properties for new beta there are more things than this, but I don't have time to figure them all out.

--- src/pocketmine/entity/Human.php
+++ src/pocketmine/entity/Human.php
@@ -52,9 +52,9 @@ class Human extends Creature implements ProjectileSource, InventoryHolder{
 	public const DATA_PLAYER_FLAG_SLEEP = 1;
 	public const DATA_PLAYER_FLAG_DEAD = 2; //TODO: CHECK
 
-	public const DATA_PLAYER_FLAGS = 27;
+	public const DATA_PLAYER_FLAGS = 26;
 
-	public const DATA_PLAYER_BED_POSITION = 29;
+	public const DATA_PLAYER_BED_POSITION = 28;
 
 	/** @var PlayerInventory */
 	protected $inventory;
