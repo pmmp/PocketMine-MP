842f54fb3f58d3827b94034c3b76e65a9ae79a0d
Killer549 <ahmhosny2002@gmail.com>
2018-04-14 21:32:18 +0300

    Add files via upload

---
+++ src/pocketmine/command/defaults/XPCommand.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+declare(strict_types=1);
+namespace pocketmine\command\defaults;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\utils\InvalidCommandSyntaxException;
+use pocketmine\entity\utils\ExperienceUtils;
+use pocketmine\entity\object\ExperienceOrb;
+use pocketmine\lang\TranslationContainer;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+
+class XPCommand extends VanillaCommand{
+    public function __construct(string $name){
+	parent::__construct(
+		$name,
+		"%pocketmine.command.xp.description",
+		"%commands.xp.usage"
+		);
+	$this->setPermission("pocketmine.command.xp");
+	}
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+	if(!$this->testPermission($sender)){
+		return true;
+	}
+        if(count($args) < 2){
+	    throw new InvalidCommandSyntaxException();
+	}
+        $player = $sender->getServer()->getPlayer($args[1]);
+        if($player === null){
+	    $sender->sendMessage(new TranslationContainer(TextFormat::RED . "%commands.generic.player.notFound"));
+	    return true;
+        }
+        if(strtolower(preg_replace('/[^a-zA-Z]/', '',$args[0])) == "l"){
+            $xp = intval(preg_replace('/[^0-9]+/', '', $args[0]), 10);
+            $player->setXpLevel(intval($player->getXpLevel()) + $xp);
+            $sender->sendMessage(new TranslationContainer("command.xp.success"));
+            return true;
+        }
+        elseif(!is_numeric($args[0])){
+            $sender->sendMessage(TextFormat::RED. "Please write in numbers");
+            return true;
+        } 
+        $player->addXp(intval($args[0]));
+        $sender->sendMessage(new TranslationContainer("command.xp.success"));
+            
+    }
+
+}
+
