dbda0442293fde226d4febf784633396b120a9ba
Dylan K. Taylor <odigiman@gmail.com>
2018-06-19 18:34:19 +0100

    bump version

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -37,7 +37,7 @@ namespace pocketmine {
 	use pocketmine\wizard\SetupWizard;
 
 	const NAME = "PocketMine-MP";
-	const BASE_VERSION = "3.0.0";
+	const BASE_VERSION = "3.0.1";
 	const IS_DEVELOPMENT_BUILD = false;
 	const BUILD_NUMBER = 0;
 
