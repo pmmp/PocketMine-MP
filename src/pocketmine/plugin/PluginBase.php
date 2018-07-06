ad03cc1971d974839185c95990f3adcdd95d8673
Dylan K. Taylor <odigiman@gmail.com>
2018-07-06 13:58:42 +0100

    PluginManager: Automatically create data directories for plugins

--- src/pocketmine/plugin/PluginBase.php
+++ src/pocketmine/plugin/PluginBase.php
@@ -256,9 +256,7 @@ abstract class PluginBase implements Plugin{
 	}
 
 	public function reloadConfig(){
-		if(!$this->saveDefaultConfig()){
-			@mkdir($this->dataFolder);
-		}
+		$this->saveDefaultConfig();
 		$this->config = new Config($this->configFile);
 		if(($configStream = $this->getResource("config.yml")) !== null){
 			$this->config->setDefaults(yaml_parse(Config::fixYAMLIndexes(stream_get_contents($configStream))));
