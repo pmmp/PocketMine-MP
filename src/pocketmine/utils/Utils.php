62cb7963dcd4a8f1adbb2ee99bad565523404b59
Dylan K. Taylor <odigiman@gmail.com>
2018-07-31 14:37:54 +0100

    Remove deprecated functions for 4.0.0 If any moron starts complaining that their plugins broke, tell them to use 3.x... thanks

--- src/pocketmine/utils/Utils.php
+++ src/pocketmine/utils/Utils.php
@@ -121,18 +121,6 @@ class Utils{
 	}
 
 	/**
-	 * @deprecated
-	 * @see Internet::getIP()
-	 *
-	 * @param bool $force default false, force IP check even when cached
-	 *
-	 * @return string|bool
-	 */
-	public static function getIP(bool $force = false){
-		return Internet::getIP($force);
-	}
-
-	/**
 	 * Returns the current Operating System
 	 * Windows => win
 	 * MacOS => mac
@@ -329,59 +317,6 @@ class Utils{
 
 		return array("yaw" => $hAngle, "pitch" => $vAngle);
 	}*/
-
-	/**
-	 * @deprecated
-	 * @see Internet::getURL()
-	 *
-	 * @param string  $page
-	 * @param int     $timeout default 10
-	 * @param array   $extraHeaders
-	 * @param string  &$err    Will be set to the output of curl_error(). Use this to retrieve errors that occured during the operation.
-	 * @param array[] &$headers
-	 * @param int     &$httpCode
-	 *
-	 * @return bool|mixed false if an error occurred, mixed data if successful.
-	 */
-	public static function getURL(string $page, int $timeout = 10, array $extraHeaders = [], &$err = null, &$headers = null, &$httpCode = null){
-		return Internet::getURL($page, $timeout, $extraHeaders, $err, $headers, $httpCode);
-	}
-
-	/**
-	 * @deprecated
-	 * @see Internet::postURL()
-	 *
-	 * @param string       $page
-	 * @param array|string $args
-	 * @param int          $timeout
-	 * @param array        $extraHeaders
-	 * @param string       &$err Will be set to the output of curl_error(). Use this to retrieve errors that occured during the operation.
-	 * @param array[]      &$headers
-	 * @param int          &$httpCode
-	 *
-	 * @return bool|mixed false if an error occurred, mixed data if successful.
-	 */
-	public static function postURL(string $page, $args, int $timeout = 10, array $extraHeaders = [], &$err = null, &$headers = null, &$httpCode = null){
-		return Internet::postURL($page, $args, $timeout, $extraHeaders, $err, $headers, $httpCode);
-	}
-
-	/**
-	 * @deprecated
-	 * @see Internet::simpleCurl()
-	 *
-	 * @param string        $page
-	 * @param float|int     $timeout      The maximum connect timeout and timeout in seconds, correct to ms.
-	 * @param string[]      $extraHeaders extra headers to send as a plain string array
-	 * @param array         $extraOpts    extra CURLOPT_* to set as an [opt => value] map
-	 * @param callable|null $onSuccess    function to be called if there is no error. Accepts a resource argument as the cURL handle.
-	 *
-	 * @return array a plain array of three [result body : string, headers : array[], HTTP response code : int]. Headers are grouped by requests with strtolower(header name) as keys and header value as values
-	 *
-	 * @throws \RuntimeException if a cURL error occurs
-	 */
-	public static function simpleCurl(string $page, $timeout = 10, array $extraHeaders = [], array $extraOpts = [], callable $onSuccess = null){
-		return Internet::simpleCurl($page, $timeout, $extraHeaders, $extraOpts, $onSuccess);
-	}
 
 	public static function javaStringHash(string $string) : int{
 		$hash = 0;
