424c50e1e9e405ebf0aa0733ce80497c22273212
Dylan K. Taylor <odigiman@gmail.com>
2018-08-21 17:36:29 +0100

    Protocol changes for 1.6.0.5, minus Entity->Actor rename

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -39,15 +39,15 @@ interface ProtocolInfo{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	public const CURRENT_PROTOCOL = 280;
+	public const CURRENT_PROTOCOL = 281;
 	/**
 	 * Current Minecraft PE version reported by the server. This is usually the earliest currently supported version.
 	 */
-	public const MINECRAFT_VERSION = 'v1.6.0.1 beta';
+	public const MINECRAFT_VERSION = 'v1.6.0.5 beta';
 	/**
 	 * Version number sent to clients in ping responses.
 	 */
-	public const MINECRAFT_VERSION_NETWORK = '1.6.0.1';
+	public const MINECRAFT_VERSION_NETWORK = '1.6.0.5';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
