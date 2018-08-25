b9b3d13cb1fe0f1e0fe213dee4f9481ea9993550
adeynes <alexisdeynes@outlook.com>
2018-08-25 15:25:15 +0200

    Use regex instead of 100s of strpos in FormattedCommandAlias::buildCommand

--- src/pocketmine/command/FormattedCommandAlias.php
+++ src/pocketmine/command/FormattedCommandAlias.php
@@ -72,86 +72,24 @@ class FormattedCommandAlias extends Command{
 	 * @return string
 	 */
 	private function buildCommand(string $formatString, array $args) : string{
-		$index = strpos($formatString, '$');
-		while($index !== false){
-			$start = $index;
-			if($index > 0 and $formatString{$start - 1} === "\\"){
-				$formatString = substr($formatString, 0, $start - 1) . substr($formatString, $start);
-				$index = strpos($formatString, '$', $index);
-				continue;
-			}
+        $placeholders = [];
+        preg_match_all('/\s(\$\d+)(?!\S)/', $formatString, $placeholders);
 
-			$required = false;
-			if($formatString{$index + 1} == '$'){
-				$required = true;
+        // Remove duplicates since we replace every instance anyways
+        $placeholders = array_unique($placeholders[1]);
 
-				++$index;
-			}
+        foreach($placeholders as $placeholder){
+            $index = substr($placeholder, 1);
+            var_dump($index);
+            if(!isset($args[$index])){
+                throw new \InvalidArgumentException('Not enough arguments provided!');
+            }
 
-			++$index;
+            // Only match those that are words by themselves and add space to replacement
+            // because this will match the space before the $
+            $formatString = preg_replace('/\s\$'.$index.'(?!\S)/', " $args[$index]", $formatString);
+        }
 
-			$argStart = $index;
-
-			while($index < strlen($formatString) and self::inRange(ord($formatString{$index}) - 48, 0, 9)){
-				++$index;
-			}
-
-			if($argStart === $index){
-				throw new \InvalidArgumentException("Invalid replacement token");
-			}
-
-			$position = (int) substr($formatString, $argStart, $index);
-
-			if($position === 0){
-				throw new \InvalidArgumentException("Invalid replacement token");
-			}
-
-			--$position;
-
-			$rest = false;
-
-			if($index < strlen($formatString) and $formatString{$index} === "-"){
-				$rest = true;
-				++$index;
-			}
-
-			$end = $index;
-
-			if($required and $position >= count($args)){
-				throw new \InvalidArgumentException("Missing required argument " . ($position + 1));
-			}
-
-			$replacement = "";
-			if($rest and $position < count($args)){
-				for($i = $position, $c = count($args); $i < $c; ++$i){
-					if($i !== $position){
-						$replacement .= " ";
-					}
-
-					$replacement .= $args[$i];
-				}
-			}elseif($position < count($args)){
-				$replacement .= $args[$position];
-			}
-
-			$formatString = substr($formatString, 0, $start) . $replacement . substr($formatString, $end);
-
-			$index = $start + strlen($replacement);
-
-			$index = strpos($formatString, '$', $index);
-		}
-
-		return $formatString;
-	}
-
-	/**
-	 * @param int $i
-	 * @param int $j
-	 * @param int $k
-	 *
-	 * @return bool
-	 */
-	private static function inRange(int $i, int $j, int $k) : bool{
-		return $i >= $j and $i <= $k;
-	}
+        return $formatString;
+    }
 }
