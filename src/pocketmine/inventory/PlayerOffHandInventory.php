4e86d08d0aff2775b7cf797b2884e11afb36c3a0
EmreTr1 <emre.yavuz.842@gmail.com>
2018-09-04 14:19:31 +0300

    PlayerOffHandInventory: Fix?

--- src/pocketmine/inventory/PlayerOffHandInventory.php
+++ src/pocketmine/inventory/PlayerOffHandInventory.php
@@ -1,30 +1,33 @@
 <?php
 
 /*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *               _ _
+ *         /\   | | |
+ *        /  \  | | |_ __ _ _   _
+ *       / /\ \ | | __/ _` | | | |
+ *      / ____ \| | || (_| | |_| |
+ *     /_/    \_|_|\__\__,_|\__, |
+ *                           __/ |
+ *                          |___/
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
+ * @author TuranicTeam
+ * @link https://github.com/TuranicTeam/Altay
  *
- *
-*/
+ */
 
 declare(strict_types=1);
 
 namespace pocketmine\inventory;
 
+use pocketmine\item\Item;
 use pocketmine\network\mcpe\protocol\MobEquipmentPacket;
-use pocketmine\network\mcpe\protocol\types\ContainerIds;
+use pocketmine\network\mcpe\protocol\InventoryContentPacket;
+use pocketmine\network\mcpe\protocol\InventorySlotPacket;
 use pocketmine\Player;
 
 class PlayerOffHandInventory extends BaseInventory{
@@ -44,31 +47,65 @@ class PlayerOffHandInventory extends BaseInventory{
 		return 1;
 	}
 
-	public function setSize(int $size){
-		throw new \BadMethodCallException("Cursor can only carry one item at a time");
-	}
-
-	/**
-	 * This override is here for documentation and code completion purposes only.
-	 * @return Player
-	 */
-	public function getHolder(){
+	public function getHolder() : Player{
 		return $this->holder;
 	}
 
-	public function sendSlot(int $index, $target) : void{
-		$pk = new MobEquipmentPacket();
-		$pk->entityRuntimeId = $this->getHolder()->getId();
-		$pk->item = $this->getItem(0);
-		$pk->inventorySlot = $pk->hotbarSlot = 0;
-		$pk->windowId = ContainerIds::OFFHAND;
+	public function setOffHand(Item $item) : void{
+		$this->setItem(0, $item);
+	}
 
-		if(!is_array($target)){
-			$target->sendDataPacket($pk);
-		}else{
-			$this->getHolder()->getLevel()->getServer()->broadcastPacket($target, $pk);
+	public function setSize(int $size){
+		throw new \BadMethodCallException("OffHand can only carry one item at a time");
+	}
+
+	public function sendSlot(int $index, $target) : void{
+		if($target instanceof Player){
+			$target = [$target];
 		}
 
-		parent::sendSlot($index, $target);
+		/** @var Player[] $target */
+
+		if(($k = array_search($this->holder, $target, true)) !== false){
+			$pk = new InventorySlotPacket();
+			$pk->windowId = $target[$k]->getWindowId($this);
+			$pk->inventorySlot = $index;
+			$pk->item = $this->getItem($index);
+			$target[$k]->sendDataPacket($pk);
+			unset($target[$k]);
+		}
+		if(!empty($target)){
+			$pk = new MobEquipmentPacket();
+			$pk->entityRuntimeId = $this->getHolder()->getId();
+			$pk->inventorySlot = $pk->hotbarSlot = $this->getItem($index);
+			$this->holder->getLevel()->getServer()->broadcastPacket($target, $pk);
+		}
+	}
+
+	public function sendContents($target) : void{
+		if($target instanceof Player){
+			$target = [$target];
+		}
+
+		if(($k = array_search($this->holder, $target, true)) !== false){
+			$pk = new InventoryContentPacket();
+			$pk->windowId = $target[$k]->getWindowId($this);
+			$pk->items = $this->getContents(true);
+			$target[$k]->sendDataPacket($pk);
+			unset($target[$k]);
+		}
+		if(!empty($target)){
+			$pk = new MobEquipmentPacket();
+			$pk->entityRuntimeId = $this->getHolder()->getId();
+			$pk->inventorySlot = $pk->hotbarSlot = $this->getItem(0);
+			$this->holder->getLevel()->getServer()->broadcastPacket($target, $pk);
+		}
+	}
+
+	/**
+	 * @return Player[]
+	 */
+	public function getViewers() : array{
+		return array_merge(parent::getViewers(), $this->holder->getViewers());
 	}
 }
