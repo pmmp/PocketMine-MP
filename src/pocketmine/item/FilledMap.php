07e1dbf06ae4f210ef1fb78eecca25bebbdb568b
EmreTr1 <emre.yavuz.842@gmail.com>
2018-09-16 12:03:52 +0300

    FilledMap: fix for old save format

--- src/pocketmine/item/FilledMap.php
+++ src/pocketmine/item/FilledMap.php
@@ -87,27 +87,33 @@ class FilledMap extends Item{
 		if($zoom > 4){
 			$zoom = 4;
 		}
-		$this->setNamedTagEntry(new ByteTag(self::TAG_ZOOM, $zoom));
+		$nbt = $this->getNamedTag();
+		$nbt->setByte(self::TAG_ZOOM, $zoom, true);
+
+		$this->setNamedTag($nbt);
 	}
 
 	/**
 	 * @return int
 	 */
 	public function getZoom() : int{
-		return $this->getNamedTag()->getByte(self::TAG_ZOOM, 0);
+		return $this->getNamedTag()->getByte(self::TAG_ZOOM, 0, true);
 	}
 
 	/**
 	 * @param int $mapId
 	 */
 	public function setMapId(int $mapId) : void{
-		$this->setNamedTagEntry(new LongTag(self::TAG_MAP_UUID, $mapId));
+		$nbt = $this->getNamedTag();
+		$nbt->setLong(self::TAG_MAP_UUID, $mapId, true);
+
+		$this->setNamedTag($nbt);
 	}
 
 	/**
 	 * @return int
 	 */
 	public function getMapId() : int{
-		return $this->getNamedTag()->getLong(self::TAG_MAP_UUID, 0, false);
+		return $this->getNamedTag()->getLong(self::TAG_MAP_UUID, 0, true);
 	}
 }
