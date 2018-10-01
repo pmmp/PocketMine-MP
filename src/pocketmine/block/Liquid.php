77fd57e11a936f1afbad8805aaf2139bec1af979
Dylan K. Taylor <odigiman@gmail.com>
2018-10-01 16:20:35 -0400

    Removed wtf from Liquid

--- src/pocketmine/block/Liquid.php
+++ src/pocketmine/block/Liquid.php
@@ -246,7 +246,7 @@ abstract class Liquid extends Transparent{
 				}
 			}
 
-*/			if($falling !== $this->falling or (!$falling and $newDecay !== $this->decay)){
+			if($falling !== $this->falling or (!$falling and $newDecay !== $this->decay)){
 				if(!$falling and $newDecay < 0){
 					$this->level->setBlock($this, BlockFactory::get(Block::AIR));
 					return;
