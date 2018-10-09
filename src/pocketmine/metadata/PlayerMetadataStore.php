1c363f801017a9bae088bd4f31967535267ef099
Dylan K. Taylor <odigiman@gmail.com>
2018-10-09 15:36:59 +0100

    Revamp MetadataStore API This would be a lot less messy if we had generics, but no tango.

--- src/pocketmine/metadata/PlayerMetadataStore.php
+++ src/pocketmine/metadata/PlayerMetadataStore.php
@@ -24,14 +24,27 @@ declare(strict_types=1);
 namespace pocketmine\metadata;
 
 use pocketmine\IPlayer;
+use pocketmine\plugin\Plugin;
 
 class PlayerMetadataStore extends MetadataStore{
 
-	public function disambiguate(Metadatable $player, string $metadataKey) : string{
-		if(!($player instanceof IPlayer)){
-			throw new \InvalidArgumentException("Argument must be an IPlayer instance");
-		}
-
+	private function disambiguate(IPlayer $player, string $metadataKey) : string{
 		return strtolower($player->getName()) . ":" . $metadataKey;
+	}
+
+	public function getMetadata(IPlayer $subject, string $metadataKey){
+		return $this->getMetadataInternal($this->disambiguate($subject, $metadataKey));
+	}
+
+	public function hasMetadata(IPlayer $subject, string $metadataKey) : bool{
+		return $this->hasMetadataInternal($this->disambiguate($subject, $metadataKey));
+	}
+
+	public function removeMetadata(IPlayer $subject, string $metadataKey, Plugin $owningPlugin){
+		$this->removeMetadataInternal($this->disambiguate($subject, $metadataKey), $owningPlugin);
+	}
+
+	public function setMetadata(IPlayer $subject, string $metadataKey, MetadataValue $newMetadataValue){
+		$this->setMetadataInternal($this->disambiguate($subject, $metadataKey), $newMetadataValue);
 	}
 }
