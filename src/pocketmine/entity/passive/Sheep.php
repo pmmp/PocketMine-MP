eb5df52518c328b3531f6c0f83d9f60faef24ba2
EmreTr1 <emre.yavuz.842@gmail.com>
2018-10-10 20:34:00 +0300

    these are not tamable animal

--- src/pocketmine/entity/passive/Sheep.php
+++ src/pocketmine/entity/passive/Sheep.php
@@ -24,6 +24,7 @@ declare(strict_types=1);
 
 namespace pocketmine\entity\passive;
 
+use pocketmine\entity\Animal;
 use pocketmine\entity\behavior\EatBlockBehavior;
 use pocketmine\entity\behavior\FloatBehavior;
 use pocketmine\entity\behavior\FollowParentBehavior;
@@ -33,7 +34,6 @@ use pocketmine\entity\behavior\PanicBehavior;
 use pocketmine\entity\behavior\RandomLookAroundBehavior;
 use pocketmine\entity\behavior\TemptedBehavior;
 use pocketmine\entity\behavior\WanderBehavior;
-use pocketmine\entity\Tamable;
 use pocketmine\item\Dye;
 use pocketmine\item\Shears;
 use pocketmine\item\Item;
@@ -42,7 +42,7 @@ use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\Player;
 
-class Sheep extends Tamable{
+class Sheep extends Animal{
 
     public const NETWORK_ID = self::SHEEP;
 
