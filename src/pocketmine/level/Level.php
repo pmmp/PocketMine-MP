pmmp/PocketMine-MP

commit 48a5eeb3a459cb90b302990d3f243a34b0bb7da0

BlockFactory: remove some useless static arrays

src/pocketmine/level/Level.php
@@ -1463,8 +1463,8 @@ class Level implements ChunkManager, Metadatable{
 	public function updateBlockLight(int $x, int $y, int $z){
 		$this->timings->doBlockLightUpdates->startTiming();
 
-		$id = $this->getBlockIdAt($x, $y, $z);
-		$newLevel = max(BlockFactory::$light[$id], $this->getHighestAdjacentBlockLight($x, $y, $z) - BlockFactory::$lightFilter[$id]);
+		$block = $this->getBlockAt($x, $y, $z);
+		$newLevel = max($block->getLightLevel(), $this->getHighestAdjacentBlockLight($x, $y, $z) - $block->getLightFilter());
 
 		if($this->blockLightUpdate === null){
 			$this->blockLightUpdate = new BlockLightUpdate($this);

src/pocketmine/block/BlockFactory.php
@@ -35,12 +35,6 @@ class BlockFactory{
 
 	/** @var \SplFixedArray<bool> */
 	public static $solid = null;
-	/** @var \SplFixedArray<bool> */
-	public static $transparent = null;
-	/** @var \SplFixedArray<float> */
-	public static $hardness = null;
-	/** @var \SplFixedArray<int> */
-	public static $light = null;
 	/** @var \SplFixedArray<int> */
 	public static $lightFilter = null;
 	/** @var \SplFixedArray<bool> */
@@ -64,11 +58,8 @@ class BlockFactory{
 	public static function init() : void{
 		self::$fullList = new \SplFixedArray(4096);
 
-		self::$light = new \SplFixedArray(256);
 		self::$lightFilter = new \SplFixedArray(256);
 		self::$solid = new \SplFixedArray(256);
-		self::$hardness = new \SplFixedArray(256);
-		self::$transparent = new \SplFixedArray(256);
 		self::$diffusesSkyLight = new \SplFixedArray(256);
 		self::$blastResistance = new \SplFixedArray(256);
 
@@ -357,9 +348,6 @@ class BlockFactory{
 		}
 
 		self::$solid[$id] = $block->isSolid();
-		self::$transparent[$id] = $block->isTransparent();
-		self::$hardness[$id] = $block->getHardness();
-		self::$light[$id] = $block->getLightLevel();
 		self::$lightFilter[$id] = min(15, $block->getLightFilter() + 1); //opacity plus 1 standard light filter
 		self::$diffusesSkyLight[$id] = $block->diffusesSkyLight();
 		self::$blastResistance[$id] = $block->getBlastResistance();

commit 93b8a6c44a8a38d26cd48d98c3ceb83644dfbfa2

Level: fix light removal bugs caused by 48a5eeb3a459cb90b302990d3f243a34b0bb7da0 and 19e68f98a707536866c33bb1e9fad9f7c280447d BlockFactory::\$lightFilter and Block->getLightFilter() are not equivalent.

src/pocketmine/level/Level.php
@@ -1418,7 +1418,7 @@ class Level implements ChunkManager, Metadatable{
 				$this->skyLightUpdate->setAndUpdateLight($x, $i, $z, 15);
 			}
 		}else{ //No heightmap change, block changed "underground"
-			$this->skyLightUpdate->setAndUpdateLight($x, $y, $z, max(0, $this->getHighestAdjacentBlockSkyLight($x, $y, $z) - $source->getLightFilter()));
+			$this->skyLightUpdate->setAndUpdateLight($x, $y, $z, max(0, $this->getHighestAdjacentBlockSkyLight($x, $y, $z) - BlockFactory::$lightFilter[($source->getId() << 4) | $source->getDamage()]));
 		}
 
 		$this->timings->doBlockSkyLightUpdates->stopTiming();
@@ -1448,7 +1448,7 @@ class Level implements ChunkManager, Metadatable{
 		$this->timings->doBlockLightUpdates->startTiming();
 
 		$block = $this->getBlockAt($x, $y, $z);
-		$newLevel = max($block->getLightLevel(), $this->getHighestAdjacentBlockLight($x, $y, $z) - $block->getLightFilter());
+		$newLevel = max($block->getLightLevel(), $this->getHighestAdjacentBlockLight($x, $y, $z) - BlockFactory::$lightFilter[($block->getId() << 4) | $block->getDamage()]);
 
 		if($this->blockLightUpdate === null){
 			$this->blockLightUpdate = new BlockLightUpdate($this);