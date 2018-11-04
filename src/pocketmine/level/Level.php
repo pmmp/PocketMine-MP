38d419fb29783a7c73dd746e612f6674a425b3dc
Dylan K. Taylor <odigiman@gmail.com>
2018-11-04 12:59:34 +0000

    Level: use isInWorld() where appropriate

--- src/pocketmine/level/Level.php
+++ src/pocketmine/level/Level.php
@@ -1782,7 +1782,7 @@ class Level implements ChunkManager, Metadatable{
 			$clickVector = new Vector3(0.0, 0.0, 0.0);
 		}
 
-		if($blockReplace->y >= $this->worldHeight or $blockReplace->y < 0){
+		if(!$this->isInWorld($blockReplace->x, $blockReplace->y, $blockReplace->z)){
 			//TODO: build height limit messages for custom world heights and mcregion cap
 			return false;
 		}
