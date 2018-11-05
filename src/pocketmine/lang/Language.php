c201a0e909d9e8212657c7b072d6c620895e3013
Dylan K. Taylor <odigiman@gmail.com>
2018-11-05 09:49:21 +0000

    Move language submodule to resources/ again, this isn't source code.

--- src/pocketmine/lang/Language.php
+++ src/pocketmine/lang/Language.php
@@ -35,7 +35,7 @@ class Language{
 	 */
 	public static function getLanguageList(string $path = "") : array{
 		if($path === ""){
-			$path = \pocketmine\PATH . "src/pocketmine/lang/locale/";
+			$path = \pocketmine\RESOURCE_PATH . "locale/";
 		}
 
 		if(is_dir($path)){
@@ -82,7 +82,7 @@ class Language{
 		$this->langName = strtolower($lang);
 
 		if($path === null){
-			$path = \pocketmine\PATH . "src/pocketmine/lang/locale/";
+			$path = \pocketmine\RESOURCE_PATH . "locale/";
 		}
 
 		$this->lang = self::loadLang($path, $this->langName);
