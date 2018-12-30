77b9feb3c01c3d504a19a609aa9647e7f741f18b
Dylan K. Taylor <odigiman@gmail.com>
2018-12-30 18:40:36 +0000

    Player: don't waste CPU time ordering chunks for non-moving players

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1073,8 +1073,6 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 
 		Timings::$playerChunkOrderTimer->startTiming();
 
-		$this->nextChunkOrderRun = 200;
-
 		$radius = $this->server->getAllowedViewDistance($this->viewDistance);
 		$radiusSquared = $radius ** 2;
 
@@ -1772,7 +1770,8 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 			return;
 		}
 
-		if($this->nextChunkOrderRun-- <= 0){
+		if($this->nextChunkOrderRun !== PHP_INT_MAX and $this->nextChunkOrderRun-- <= 0){
+			$this->nextChunkOrderRun = PHP_INT_MAX;
 			$this->orderChunks();
 		}
 
