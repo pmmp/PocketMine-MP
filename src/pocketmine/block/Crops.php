80d48161d3acf8cfdbdee6cfeb3d5d57b7631e4b
Dylan K. Taylor <odigiman@gmail.com>
2019-01-29 13:40:29 +0000

    Crops: fixed consuming bonemeal on fully-grown crop, closes #2704

--- src/pocketmine/block/Crops.php
+++ src/pocketmine/block/Crops.php
@@ -43,7 +43,7 @@ abstract class Crops extends Flowable{
 
 
 	public function onActivate(Item $item, Player $player = null) : bool{
-		if($item->getId() === Item::DYE and $item->getDamage() === 0x0F){ //Bonemeal
+		if($this->meta < 7 and $item->getId() === Item::DYE and $item->getDamage() === 0x0F){ //Bonemeal
 			$block = clone $this;
 			$block->meta += mt_rand(2, 5);
 			if($block->meta > 7){
