c6a5829a9222d0a48542a57e44284c8429fa9223
Dylan K. Taylor <odigiman@gmail.com>
2019-02-03 14:23:55 +0000

    Entity: remove dead function

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -1305,10 +1305,6 @@ abstract class Entity extends Location implements Metadatable, EntityIds{
 		);
 	}
 
-	public function canTriggerWalking() : bool{
-		return true;
-	}
-
 	public function resetFallDistance() : void{
 		$this->fallDistance = 0.0;
 	}
