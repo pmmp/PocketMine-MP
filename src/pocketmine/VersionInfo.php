59334d652f57fa7d0982277bfba3abb7271f60eb
Dylan K. Taylor <odigiman@gmail.com>
2019-05-02 17:13:50 +0100

    3.8.3 is next

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -22,6 +22,6 @@
 namespace pocketmine;
 
 const NAME = "PocketMine-MP";
-const BASE_VERSION = "3.8.2";
-const IS_DEVELOPMENT_BUILD = false;
+const BASE_VERSION = "3.8.3";
+const IS_DEVELOPMENT_BUILD = true;
 const BUILD_NUMBER = 0;
