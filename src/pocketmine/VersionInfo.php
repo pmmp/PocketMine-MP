158f4a62801ec892ed1bf4f684a47f6f4e9d8ac6
오르카 <44334220+TrueToneTeam@users.noreply.github.com>
2019-05-07 22:39:24 +0900

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -21,8 +21,8 @@
 
 namespace pocketmine;
 
-const NAME = "Cloud-MP";
+const NAME = "Z3 Nano";
 const BASE_VERSION = "3.8.0";
 const IS_DEVELOPMENT_BUILD = false;
-const BUILD_NUMBER = 50;
-const MP_VERSION = "1.0.3";
+const BUILD_NUMBER = 104;
+const MP_VERSION = "1.3.0";
