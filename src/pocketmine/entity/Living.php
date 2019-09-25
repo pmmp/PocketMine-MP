dc218121777f1049047b44f2a91d1ea203baa6cf
EmreTr1 <emre.yavuz.842@gmail.com>
2019-09-25 19:15:14 +0300

    Living: removed non-vanilla attack behavior Close #182

--- src/pocketmine/entity/Living.php
+++ src/pocketmine/entity/Living.php
@@ -719,10 +719,7 @@ abstract class Living extends Entity implements Damageable{
 			}
 
 			if($e !== null){
-				if((
-					$source->getCause() === EntityDamageEvent::CAUSE_PROJECTILE or
-					$source->getCause() === EntityDamageEvent::CAUSE_ENTITY_ATTACK
-				) and $e->isOnFire()){
+				if($source->getCause() === EntityDamageEvent::CAUSE_PROJECTILE and $e->isOnFire()){
 					$this->setOnFire(2 * $this->level->getDifficulty());
 				}
 
