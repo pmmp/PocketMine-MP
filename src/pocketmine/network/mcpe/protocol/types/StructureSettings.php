aeeee5eb5321c604f751721af19d634c4a5b564e
Ivan <tikhonov.03@mail.ru>
2019-10-14 17:14:42 +0700

    Added encode/decode for StructureTemplateDataExport(Request|Response)Packet (#3145)

---
+++ src/pocketmine/network/mcpe/protocol/types/StructureSettings.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\network\mcpe\protocol\types;
+
+class StructureSettings{
+	/** @var string */
+	public $paletteName;
+	/** @var bool */
+	public $ignoreEntities;
+	/** @var bool */
+	public $ignoreBlocks;
+	/** @var int */
+	public $structureSizeX;
+	/** @var int */
+	public $structureSizeY;
+	/** @var int */
+	public $structureSizeZ;
+	/** @var int */
+	public $structureOffsetX;
+	/** @var int */
+	public $structureOffsetY;
+	/** @var int */
+	public $structureOffsetZ;
+	/** @var int */
+	public $lastTouchedByPlayerID;
+	/** @var int */
+	public $rotation;
+	/** @var int */
+	public $mirror;
+	/** @var float */
+	public $integrityValue;
+	/** @var int */
+	public $integritySeed;
+}
