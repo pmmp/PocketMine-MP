311efe3bbd6a4b16458fa3753c43931192b9fb9d
Minejong <admin@lhub.su>
2019-10-16 20:50:51 +0300

    Founded some constants

--- src/pocketmine/network/mcpe/protocol/types/StructureEditorData.php
+++ src/pocketmine/network/mcpe/protocol/types/StructureEditorData.php
@@ -24,6 +24,13 @@ declare(strict_types=1);
 namespace pocketmine\network\mcpe\protocol\types;
 
 class StructureEditorData{
+	public const TYPE_DATA = 0;
+	public const TYPE_SAVE = 1;
+	public const TYPE_LOAD = 2;
+	public const TYPE_CORNER = 3;
+	public const TYPE_INVALID = 4;
+	public const TYPE_EXPORT = 5;
+
 	/** @var string */
 	public $structureName;
 	/** @var string */
