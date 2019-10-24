acdcb99566402251b46b4122fbbbd5ed374689f5
Dylan K. Taylor <odigiman@gmail.com>
2019-10-24 11:46:47 +0100

    fixed 2cac7331f5f4922fba3eeb1c98ce1fbe45541cec merge error

--- src/PocketMine.php
+++ src/PocketMine.php
@@ -201,9 +201,9 @@ namespace pocketmine {
 		$gitHash = str_repeat("00", 20);
 
 		if(\Phar::running(true) === ""){
-			if(Utils::execute("git rev-parse HEAD", $out) === 0 and $out !== false and strlen($out = trim($out)) === 40){
+			if(Process::execute("git rev-parse HEAD", $out) === 0 and $out !== false and strlen($out = trim($out)) === 40){
 				$gitHash = trim($out);
-				if(Utils::execute("git diff --quiet") === 1 or Utils::execute("git diff --cached --quiet") === 1){ //Locally-modified
+				if(Process::execute("git diff --quiet") === 1 or Process::execute("git diff --cached --quiet") === 1){ //Locally-modified
 					$gitHash .= "-dirty";
 				}
 			}
