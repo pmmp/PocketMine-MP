3c9af56e066948b747a089199544103c4db3eeeb
Dylan K. Taylor <odigiman@gmail.com>
2019-12-11 20:20:26 +0000

    3.11.2 is next

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -30,6 +30,6 @@ const _VERSION_INFO_INCLUDED = true;
 
 
 const NAME = "PocketMine-MP";
-const BASE_VERSION = "3.11.1";
-const IS_DEVELOPMENT_BUILD = false;
+const BASE_VERSION = "3.11.2";
+const IS_DEVELOPMENT_BUILD = true;
 const BUILD_NUMBER = 0;
