70d99a8fb6b2570d37eae36be08b99a0d1c729b2
Dylan K. Taylor <odigiman@gmail.com>
2019-12-14 10:04:51 +0000

    Enchantment: don't throw exceptions on out of range IDs this function is used for data deserialization, and data may have bad enchantment IDs in it.

--- src/pocketmine/item/enchantment/Enchantment.php
+++ src/pocketmine/item/enchantment/Enchantment.php
@@ -155,6 +155,9 @@ class Enchantment{
 	 * @return Enchantment|null
 	 */
 	public static function getEnchantment(int $id) : ?Enchantment{
+		if($id < 0 or $id >= self::$enchantments->getSize()){
+			return null;
+		}
 		return self::$enchantments[$id] ?? null;
 	}
 
