a37d7401112fe6cbafcb5aa58b13d382fb9aeee4
Dylan K. Taylor <odigiman@gmail.com>
2019-12-29 13:11:30 +0000

    Release 3.11.2

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -31,5 +31,5 @@ const _VERSION_INFO_INCLUDED = true;
 
 const NAME = "PocketMine-MP";
 const BASE_VERSION = "3.11.2";
-const IS_DEVELOPMENT_BUILD = true;
+const IS_DEVELOPMENT_BUILD = false;
 const BUILD_NUMBER = 0;
