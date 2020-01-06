733263f7d50d6c1617303978b848cef7489a727f
Ege Gian <52561965+GianC-Dev@users.noreply.github.com>
2020-01-06 22:25:55 +0300

    It Was Supported Source Code

--- start.sh
+++ start.sh
@@ -34,6 +34,8 @@ fi
 if [ "$POCKETMINE_FILE" == "" ]; then
 	if [ -f ./PocketMine-MP.phar ]; then
 		POCKETMINE_FILE="./PocketMine-MP.phar"
+	elif [ -f ./src/pocketmine/PocketMine.php ]; then
+		POCKETMINE_FILE="./src/pocketmine/PocketMine.php"
 	else
 		echo "PocketMine-MP.phar not found"
 		echo "Downloads can be found at https://github.com/pmmp/PocketMine-MP/releases"
