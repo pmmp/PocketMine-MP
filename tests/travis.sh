f28544385cbbfa644f6f374d853392446b3cff21
EmreTr1 <emre.yavuz.842@gmail.com>
2020-01-15 12:56:09 +0300

    phpstan level 2

--- tests/travis.sh
+++ tests/travis.sh
@@ -14,9 +14,9 @@ while getopts "p:t:" OPTION 2> /dev/null; do
 	esac
 done
 
-#[ ! -f phpstan.phar ] && echo "Downloading PHPStan..." && curl -sSLO https://github.com/phpstan/phpstan/releases/download/0.12.3/phpstan.phar
-#"$PHP_BINARY" phpstan.phar analyze --no-progress --memory-limit=2G || exit 1
-#echo "PHPStan scan succeeded"
+[ ! -f phpstan.phar ] && echo "Downloading PHPStan..." && curl -sSLO https://github.com/phpstan/phpstan/releases/download/0.12.3/phpstan.phar
+"$PHP_BINARY" phpstan.phar analyze --no-progress --memory-limit=2G || exit 1
+echo "PHPStan scan succeeded"
 
 [ ! -f phpunit.phar ] && echo "Downloading PHPUnit..." && curl https://phar.phpunit.de/phpunit-7.phar --silent --location -o phpunit.phar
 "$PHP_BINARY" phpunit.phar --bootstrap vendor/autoload.php --fail-on-warning tests/phpunit || exit 1
