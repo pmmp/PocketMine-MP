d418dd7a090c408ce1b80eebf1eeb516c3b9c783
Dylan K. Taylor <odigiman@gmail.com>
2020-02-03 11:17:16 +0000

    build/make-release: fix formatting

--- build/make-release.php
+++ build/make-release.php
@@ -58,7 +58,7 @@ function replaceVersion(string $versionInfoPath, string $newVersion, bool $isDev
 	);
 	$versionInfo = preg_replace(
 		'/^const IS_DEVELOPMENT_BUILD = (?:true|false);$/m',
-		'const IS_DEVELOPMENT_BUILD = ' . ($isDev ? 'true' : 'false'). ';',
+		'const IS_DEVELOPMENT_BUILD = ' . ($isDev ? 'true' : 'false') . ';',
 		$versionInfo
 	);
 	file_put_contents($versionInfoPath, $versionInfo);
