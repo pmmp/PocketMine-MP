262728b0911b5a0b9ac0129d1a8fbbde5200ec2a
Dylan K. Taylor <odigiman@gmail.com>
2020-02-05 18:31:51 +0000

    PopulationTask: clean up overcomplicated garbage

--- src/pocketmine/level/generator/PopulationTask.php
+++ src/pocketmine/level/generator/PopulationTask.php
@@ -102,48 +102,26 @@ class PopulationTask extends AsyncTask{
 		}
 
 		foreach($chunks as $c){
-			if($c !== null){
-				$manager->setChunk($c->getX(), $c->getZ(), $c);
-				if(!$c->isGenerated()){
-					$generator->generateChunk($c->getX(), $c->getZ());
-					$c = $manager->getChunk($c->getX(), $c->getZ());
-					$c->setGenerated();
-				}
+			$manager->setChunk($c->getX(), $c->getZ(), $c);
+			if(!$c->isGenerated()){
+				$generator->generateChunk($c->getX(), $c->getZ());
+				$c->setGenerated();
 			}
 		}
 
 		$generator->populateChunk($chunk->getX(), $chunk->getZ());
 
-		$chunk = $manager->getChunk($chunk->getX(), $chunk->getZ());
 		$chunk->recalculateHeightMap();
 		$chunk->populateSkyLight();
 		$chunk->setLightPopulated();
 		$chunk->setPopulated();
 		$this->chunk = $chunk->fastSerialize();
 
-		$manager->setChunk($chunk->getX(), $chunk->getZ(), null);
-
 		foreach($chunks as $i => $c){
-			if($c !== null){
-				$c = $chunks[$i] = $manager->getChunk($c->getX(), $c->getZ());
-				if(!$c->hasChanged()){
-					$chunks[$i] = null;
-				}
-			}else{
-				//This way non-changed chunks are not set
-				$chunks[$i] = null;
-			}
+			$this->{"chunk$i"} = $c->hasChanged() ? $c->fastSerialize() : null;
 		}
 
 		$manager->cleanChunks();
-
-		for($i = 0; $i < 9; ++$i){
-			if($i === 4){
-				continue;
-			}
-
-			$this->{"chunk$i"} = $chunks[$i] !== null ? $chunks[$i]->fastSerialize() : null;
-		}
 	}
 
 	public function onCompletion(Server $server){
