pmmp/PocketMine-MP

commit 66a1134ca1d55b8d03811ea57f316859d27ce7dd

Player: avoid another empty() usage

src/player/Player.php
@@ -1958,8 +1958,8 @@ class Player extends Human implements CommandSender, ChunkLoader, ChunkListener,
 
 		$ev = new PlayerQuitEvent($this, $quitMessage ?? $this->getLeaveMessage(), $reason);
 		$ev->call();
-		if(!empty($ev->getQuitMessage())){
-			$this->server->broadcastMessage($ev->getQuitMessage());
+		if($quitMessage != ""){
+			$this->server->broadcastMessage($quitMessage);
 		}
 		$this->save();
 

commit 17fc49db8a83a5941a549773353c5aadd3c79f33

Player: fix regression in disconnect caused by 66a1134ca1d55b8d03811ea57f316859d27ce7dd

src/player/Player.php
@@ -1958,7 +1958,7 @@ class Player extends Human implements CommandSender, ChunkLoader, ChunkListener,
 
 		$ev = new PlayerQuitEvent($this, $quitMessage ?? $this->getLeaveMessage(), $reason);
 		$ev->call();
-		if($quitMessage != ""){
+		if(($quitMessage = $ev->getQuitMessage()) != ""){
 			$this->server->broadcastMessage($quitMessage);
 		}
 		$this->save();