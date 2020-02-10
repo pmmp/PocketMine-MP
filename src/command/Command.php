pmmp/PocketMine-MP

commit 4c51f1dda348d9d1c53c89185ceeb909a1cad21a

Scrap TextContainer, make TranslationContainer immutable TextContainer provided zero real value as a base of TranslationContainer, given that it required its own logic to be handled wherever accepted. As such, it's no better than a simple string. Removing it also allows fixing an ambiguity when embedding translations inside other translations, allowing it to be made immutable.

src/command/Command.php
@@ -27,7 +27,6 @@ declare(strict_types=1);
 namespace pocketmine\command;
 
 use pocketmine\command\utils\CommandException;
-use pocketmine\lang\TextContainer;
 use pocketmine\lang\TranslationContainer;
 use pocketmine\permission\PermissionManager;
 use pocketmine\Server;
@@ -224,20 +223,15 @@ abstract class Command{
 	}
 
 	/**
-	 * @param TextContainer|string $message
+	 * @param TranslationContainer|string $message
 	 */
 	public static function broadcastCommandMessage(CommandSender $source, $message, bool $sendToSource = true) : void{
 		$users = PermissionManager::getInstance()->getPermissionSubscriptions(Server::BROADCAST_CHANNEL_ADMINISTRATIVE);
-		if($message instanceof TextContainer){
-			$m = clone $message;
-			$result = "[" . $source->getName() . ": " . ($source->getServer()->getLanguage()->get($m->getText()) !== $m->getText() ? "%" : "") . $m->getText() . "]";
+		if($message instanceof TranslationContainer){
+			$formatted = "[" . $source->getName() . ": %" . $message->getText() . "]";
 
-			$colored = TextFormat::GRAY . TextFormat::ITALIC . $result;
-
-			$m->setText($result);
-			$result = clone $m;
-			$m->setText($colored);
-			$colored = clone $m;
+			$result = new TranslationContainer($formatted, $message->getParameters());
+			$colored = new TranslationContainer(TextFormat::GRAY . TextFormat::ITALIC . $formatted, $message->getParameters());
 		}else{
 			$result = new TranslationContainer("chat.type.admin", [$source->getName(), $message]);
 			$colored = new TranslationContainer(TextFormat::GRAY . TextFormat::ITALIC . "%chat.type.admin", [$source->getName(), $message]);

src/Server.php
@@ -45,7 +45,7 @@ use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\ItemFactory;
 use pocketmine\lang\Language;
 use pocketmine\lang\LanguageNotFoundException;
-use pocketmine\lang\TextContainer;
+use pocketmine\lang\TranslationContainer;
 use pocketmine\nbt\BigEndianNbtSerializer;
 use pocketmine\nbt\NbtDataException;
 use pocketmine\nbt\tag\CompoundTag;
@@ -1138,8 +1138,8 @@ class Server{
 	}
 
 	/**
-	 * @param TextContainer|string $message
-	 * @param CommandSender[]|null $recipients
+	 * @param TranslationContainer|string $message
+	 * @param CommandSender[]|null        $recipients
 	 */
 	public function broadcastMessage($message, ?array $recipients = null) : int{
 		if(!is_array($recipients)){
@@ -1210,7 +1210,7 @@ class Server{
 	}
 
 	/**
-	 * @param TextContainer|string $message
+	 * @param TranslationContainer|string $message
 	 */
 	public function broadcast($message, string $permissions) : int{
 		/** @var CommandSender[] $recipients */

src/command/CommandSender.php
@@ -23,14 +23,14 @@ declare(strict_types=1);
 
 namespace pocketmine\command;
 
-use pocketmine\lang\TextContainer;
+use pocketmine\lang\TranslationContainer;
 use pocketmine\permission\Permissible;
 use pocketmine\Server;
 
 interface CommandSender extends Permissible{
 
 	/**
-	 * @param TextContainer|string $message
+	 * @param TranslationContainer|string $message
 	 */
 	public function sendMessage($message) : void;
 

src/command/ConsoleCommandSender.php
@@ -23,7 +23,7 @@ declare(strict_types=1);
 
 namespace pocketmine\command;
 
-use pocketmine\lang\TextContainer;
+use pocketmine\lang\TranslationContainer;
 use pocketmine\permission\PermissibleBase;
 use pocketmine\permission\PermissibleDelegateTrait;
 use pocketmine\Server;
@@ -46,11 +46,11 @@ class ConsoleCommandSender implements CommandSender{
 	}
 
 	/**
-	 * @param TextContainer|string $message
+	 * @param TranslationContainer|string $message
 	 */
 	public function sendMessage($message) : void{
 		$server = $this->getServer();
-		if($message instanceof TextContainer){
+		if($message instanceof TranslationContainer){
 			$message = $server->getLanguage()->translate($message);
 		}else{
 			$message = $server->getLanguage()->translateString($message);

src/event/player/PlayerDeathEvent.php
@@ -30,7 +30,6 @@ use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDeathEvent;
 use pocketmine\item\Item;
-use pocketmine\lang\TextContainer;
 use pocketmine\lang\TranslationContainer;
 use pocketmine\player\Player;
 
@@ -38,14 +37,14 @@ class PlayerDeathEvent extends EntityDeathEvent{
 	/** @var Player */
 	protected $entity;
 
-	/** @var TextContainer|string */
+	/** @var TranslationContainer|string */
 	private $deathMessage;
 	/** @var bool */
 	private $keepInventory = false;
 
 	/**
-	 * @param Item[]                    $drops
-	 * @param string|TextContainer|null $deathMessage Null will cause the default vanilla message to be used
+	 * @param Item[]                           $drops
+	 * @param string|TranslationContainer|null $deathMessage Null will cause the default vanilla message to be used
 	 */
 	public function __construct(Player $entity, array $drops, int $xp, $deathMessage){
 		parent::__construct($entity, $drops, $xp);
@@ -64,14 +63,14 @@ class PlayerDeathEvent extends EntityDeathEvent{
 	}
 
 	/**
-	 * @return TextContainer|string
+	 * @return TranslationContainer|string
 	 */
 	public function getDeathMessage(){
 		return $this->deathMessage;
 	}
 
 	/**
-	 * @param TextContainer|string $deathMessage
+	 * @param TranslationContainer|string $deathMessage
 	 */
 	public function setDeathMessage($deathMessage) : void{
 		$this->deathMessage = $deathMessage;

src/event/player/PlayerJoinEvent.php
@@ -23,7 +23,7 @@ declare(strict_types=1);
 
 namespace pocketmine\event\player;
 
-use pocketmine\lang\TextContainer;
+use pocketmine\lang\TranslationContainer;
 use pocketmine\player\Player;
 
 /**
@@ -34,13 +34,13 @@ use pocketmine\player\Player;
  * @see PlayerLoginEvent
  */
 class PlayerJoinEvent extends PlayerEvent{
-	/** @var string|TextContainer */
+	/** @var string|TranslationContainer */
 	protected $joinMessage;
 
 	/**
 	 * PlayerJoinEvent constructor.
 	 *
-	 * @param TextContainer|string $joinMessage
+	 * @param TranslationContainer|string $joinMessage
 	 */
 	public function __construct(Player $player, $joinMessage){
 		$this->player = $player;
@@ -48,14 +48,14 @@ class PlayerJoinEvent extends PlayerEvent{
 	}
 
 	/**
-	 * @param string|TextContainer $joinMessage
+	 * @param string|TranslationContainer $joinMessage
 	 */
 	public function setJoinMessage($joinMessage) : void{
 		$this->joinMessage = $joinMessage;
 	}
 
 	/**
-	 * @return string|TextContainer
+	 * @return string|TranslationContainer
 	 */
 	public function getJoinMessage(){
 		return $this->joinMessage;

src/event/player/PlayerKickEvent.php
@@ -25,7 +25,7 @@ namespace pocketmine\event\player;
 
 use pocketmine\event\Cancellable;
 use pocketmine\event\CancellableTrait;
-use pocketmine\lang\TextContainer;
+use pocketmine\lang\TranslationContainer;
 use pocketmine\player\Player;
 
 /**
@@ -34,7 +34,7 @@ use pocketmine\player\Player;
 class PlayerKickEvent extends PlayerEvent implements Cancellable{
 	use CancellableTrait;
 
-	/** @var TextContainer|string */
+	/** @var TranslationContainer|string */
 	protected $quitMessage;
 
 	/** @var string */
@@ -43,7 +43,7 @@ class PlayerKickEvent extends PlayerEvent implements Cancellable{
 	/**
 	 * PlayerKickEvent constructor.
 	 *
-	 * @param TextContainer|string $quitMessage
+	 * @param TranslationContainer|string $quitMessage
 	 */
 	public function __construct(Player $player, string $reason, $quitMessage){
 		$this->player = $player;
@@ -60,14 +60,14 @@ class PlayerKickEvent extends PlayerEvent implements Cancellable{
 	}
 
 	/**
-	 * @param TextContainer|string $quitMessage
+	 * @param TranslationContainer|string $quitMessage
 	 */
 	public function setQuitMessage($quitMessage) : void{
 		$this->quitMessage = $quitMessage;
 	}
 
 	/**
-	 * @return TextContainer|string
+	 * @return TranslationContainer|string
 	 */
 	public function getQuitMessage(){
 		return $this->quitMessage;

src/lang/Language.php
@@ -142,16 +142,12 @@ class Language{
 		return $baseText;
 	}
 
-	public function translate(TextContainer $c) : string{
-		if($c instanceof TranslationContainer){
-			$baseText = $this->internalGet($c->getText());
-			$baseText = $this->parseTranslation($baseText ?? $c->getText());
+	public function translate(TranslationContainer $c) : string{
+		$baseText = $this->internalGet($c->getText());
+		$baseText = $this->parseTranslation($baseText ?? $c->getText());
 
-			foreach($c->getParameters() as $i => $p){
-				$baseText = str_replace("{%$i}", $this->parseTranslation($p), $baseText);
-			}
-		}else{
-			$baseText = $this->parseTranslation($c->getText());
+		foreach($c->getParameters() as $i => $p){
+			$baseText = str_replace("{%$i}", $this->parseTranslation($p), $baseText);
 		}
 
 		return $baseText;

commit e8bb6b86250c0e5331955e19728fdc177e6ffa36

Command: fix output translation regression caused by 4c51f1dda348d9d1c53c89185ceeb909a1cad21a

src/command/Command.php
@@ -228,7 +228,7 @@ abstract class Command{
 	public static function broadcastCommandMessage(CommandSender $source, $message, bool $sendToSource = true) : void{
 		$users = PermissionManager::getInstance()->getPermissionSubscriptions(Server::BROADCAST_CHANNEL_ADMINISTRATIVE);
 		if($message instanceof TranslationContainer){
-			$formatted = "[" . $source->getName() . ": %" . $message->getText() . "]";
+			$formatted = "[" . $source->getName() . ": " . ($source->getServer()->getLanguage()->get($message->getText()) !== $message->getText() ? "%" : "") . $message->getText() . "]";
 
 			$result = new TranslationContainer($formatted, $message->getParameters());
 			$colored = new TranslationContainer(TextFormat::GRAY . TextFormat::ITALIC . $formatted, $message->getParameters());