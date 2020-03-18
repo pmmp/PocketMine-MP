pmmp/PocketMine-MP

commit 89ea7f0a769014a12cfaffcb85b668a96730f7a3

Player: Stop setting held item post entity attack if changed (#3348)
closes #3340

src/pocketmine/Player.php
@@ -2590,7 +2590,7 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 						if($this->isAlive()){
 							//reactive damage like thorns might cause us to be killed by attacking another mob, which
 							//would mean we'd already have dropped the inventory by the time we reached here
-							if($heldItem->onAttackEntity($target) and $this->isSurvival()){ //always fire the hook, even if we are survival
+							if($heldItem->onAttackEntity($target) and $this->isSurvival() and $heldItem->equalsExact($this->inventory->getItemInHand())){ //always fire the hook, even if we are survival
 								$this->inventory->setItemInHand($heldItem);
 							}
 

commit 517f9a3c3acfa20b5a68dc7e67edb02d80f6e74f

Player: fix weapons not taking damage from attacks (bug introduced by #3348)

src/pocketmine/Player.php
@@ -2535,6 +2535,7 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 						$cancelled = false;
 
 						$heldItem = $this->inventory->getItemInHand();
+						$oldItem = clone $heldItem;
 
 						if(!$this->canInteract($target, 8)){
 							$cancelled = true;
@@ -2594,7 +2595,7 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 						if($this->isAlive()){
 							//reactive damage like thorns might cause us to be killed by attacking another mob, which
 							//would mean we'd already have dropped the inventory by the time we reached here
-							if($heldItem->onAttackEntity($target) and $this->isSurvival() and $heldItem->equalsExact($this->inventory->getItemInHand())){ //always fire the hook, even if we are survival
+							if($heldItem->onAttackEntity($target) and $this->isSurvival() and $oldItem->equalsExact($this->inventory->getItemInHand())){ //always fire the hook, even if we are survival
 								$this->inventory->setItemInHand($heldItem);
 							}
 