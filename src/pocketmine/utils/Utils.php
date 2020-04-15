ae76e8f08fb20953bff44808a38d1dfa0a5a7f9c
Dylan K. Taylor <odigiman@gmail.com>
2020-04-15 12:09:10 +0100

    Utils: fix some implicit casts to boolean on result of preg_match_all()

--- src/pocketmine/utils/Utils.php
+++ src/pocketmine/utils/Utils.php
@@ -188,7 +188,7 @@ class Utils{
 		if($os === "win"){
 			@exec("ipconfig /ALL", $mac);
 			$mac = implode("\n", $mac);
-			if(preg_match_all("#Physical Address[. ]{1,}: ([0-9A-F\\-]{17})#", $mac, $matches)){
+			if(preg_match_all("#Physical Address[. ]{1,}: ([0-9A-F\\-]{17})#", $mac, $matches) > 0){
 				foreach($matches[1] as $i => $v){
 					if($v == "00-00-00-00-00-00"){
 						unset($matches[1][$i]);
@@ -202,7 +202,7 @@ class Utils{
 			}else{
 				@exec("ifconfig 2>/dev/null", $mac);
 				$mac = implode("\n", $mac);
-				if(preg_match_all("#HWaddr[ \t]{1,}([0-9a-f:]{17})#", $mac, $matches)){
+				if(preg_match_all("#HWaddr[ \t]{1,}([0-9a-f:]{17})#", $mac, $matches) > 0){
 					foreach($matches[1] as $i => $v){
 						if($v == "00:00:00:00:00:00"){
 							unset($matches[1][$i]);
