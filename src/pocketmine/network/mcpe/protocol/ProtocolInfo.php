d37bd6de6e1f3b7c34c2129337152c84c8464554
EmreTr1 <emre.yavuz.842@gmail.com>
2020-04-16 18:30:41 +0300

    Revert "Update ProtocolInfo.php"
    This reverts commit abb1d3c6

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -37,11 +37,11 @@ interface ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 390;
+	public const CURRENT_PROTOCOL = 389;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
-	public const MINECRAFT_VERSION = 'v1.14.60';
+	public const MINECRAFT_VERSION = 'v1.14.0';
 	/** Version number sent to clients in ping responses. */
-	public const MINECRAFT_VERSION_NETWORK = '1.14.60';
+	public const MINECRAFT_VERSION_NETWORK = '1.14.0';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
