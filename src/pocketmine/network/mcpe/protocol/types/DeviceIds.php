5b7b3a5dd72282f29dbebfc1b88b4e15cdef3555
Wumpotamus <s@wumpotamus.xyz>
2020-04-29 19:57:47 -0400

    Added Hololens and TVOS constants.

--- src/pocketmine/network/mcpe/protocol/types/DeviceIds.php
+++ src/pocketmine/network/mcpe/protocol/types/DeviceIds.php
@@ -31,9 +31,11 @@ interface DeviceIds{
 	public const OSX = 3;
 	public const AMAZON = 4;
 	public const GEAR_VR = 5;
+	public const HOLOLENS = 6;
 	public const WINDOWS_10 = 7;
 	public const WIN32 = 8;
 	public const DEDICATED = 9;
+	public const TVOS = 10;
 	public const PLAYSTATION = 11;
 	public const NINTENDO = 12;
 	public const XBOX = 13;
