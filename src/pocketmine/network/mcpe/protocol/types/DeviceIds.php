4cdd0fc4a7836d15ea2abfcee828ec4a46ca2c19
Wumpotamus <s@wumpotamus.xyz>
2020-04-29 19:48:00 -0400

    Fixed formatting.

--- src/pocketmine/network/mcpe/protocol/types/DeviceIds.php
+++ src/pocketmine/network/mcpe/protocol/types/DeviceIds.php
@@ -25,18 +25,18 @@ namespace pocketmine\network\mcpe\protocol\types;
 
 interface DeviceIds{
 
-  public const UNKNOWN = -1;
-  public const ANDROID = 1;
-  public const IOS = 2;
-  public const OSX = 3;
-  public const AMAZON = 4;
-  public const GEAR_VR = 5;
-  public const WINDOWS_10 = 7;
-  public const WIN32 = 8;
-  public const DEDICATED = 9;
-  public const PLAYSTATION = 11;
-  public const NINTENDO = 12;
-  public const XBOX = 13;
-  public const WINDOWS_PHONE = 14;
+	public const UNKNOWN = -1;
+	public const ANDROID = 1;
+	public const IOS = 2;
+	public const OSX = 3;
+	public const AMAZON = 4;
+	public const GEAR_VR = 5;
+	public const WINDOWS_10 = 7;
+	public const WIN32 = 8;
+	public const DEDICATED = 9;
+	public const PLAYSTATION = 11;
+	public const NINTENDO = 12;
+	public const XBOX = 13;
+	public const WINDOWS_PHONE = 14;
 
 }
