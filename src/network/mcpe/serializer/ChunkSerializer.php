c2857a91bd99b2a45b5a8dd72850d2f8bd0a1074
Dylan K. Taylor <odigiman@gmail.com>
2020-05-04 12:14:27 +0100

    [Network]ChunkSerializer: allow injecting RuntimeBlockMapping this will allow the same serializer to be reused with different mapping tables (will be needed for multi version).

--- src/network/mcpe/serializer/ChunkSerializer.php
+++ src/network/mcpe/serializer/ChunkSerializer.php
@@ -51,10 +51,9 @@ final class ChunkSerializer{
 		return 0;
 	}
 
-	public static function serialize(Chunk $chunk, ?string $tiles = null) : string{
+	public static function serialize(Chunk $chunk, RuntimeBlockMapping $blockMapper, ?string $tiles = null) : string{
 		$stream = new NetworkBinaryStream();
 		$subChunkCount = self::getSubChunkCount($chunk);
-		$blockMapper = RuntimeBlockMapping::getInstance();
 		for($y = 0; $y < $subChunkCount; ++$y){
 			$layers = $chunk->getSubChunk($y)->getBlockLayers();
 			$stream->putByte(8); //version
