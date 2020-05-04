ba6fb872800264582a492840996640b4a57da359
Dylan K. Taylor <odigiman@gmail.com>
2020-05-04 13:27:31 +0100

    remove unused imports

--- src/world/sound/EntityAttackSound.php
+++ src/world/sound/EntityAttackSound.php
@@ -24,7 +24,6 @@ declare(strict_types=1);
 namespace pocketmine\world\sound;
 
 use pocketmine\math\Vector3;
-use pocketmine\network\mcpe\protocol\ClientboundPacket;
 use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 
 /**
