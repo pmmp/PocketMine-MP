4199c3796f4c6d8da0eccba64fe4cd9812acadeb
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2020-05-24 18:00:19 +0500

    Water: Remove duplicate call to Entity->resetFallDistance() (#3524)
    It's not clear what the intended goal of this code was, but the duplicate call is obviously useless.

--- src/pocketmine/block/Water.php
+++ src/pocketmine/block/Water.php
@@ -70,8 +70,6 @@ class Water extends Liquid{
 		if($entity->isOnFire()){
 			$entity->extinguish();
 		}
-
-		$entity->resetFallDistance();
 	}
 
 	public function place(Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, Player $player = null) : bool{
