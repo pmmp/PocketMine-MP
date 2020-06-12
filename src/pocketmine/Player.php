8436a4478bd70de626347fc655c150cebffa0d0b
DylanMarkon <66834111+DylanMarkon@users.noreply.github.com>
2020-06-12 22:05:39 +0800

    letattack like vanilla

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2620,6 +2620,7 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 							}
 						}
 
+						$this->setSprinting(false);
 						$ev = new EntityDamageByEntityEvent($this, $target, EntityDamageEvent::CAUSE_ENTITY_ATTACK, $heldItem->getAttackPoints());
 
 						$meleeEnchantmentDamage = 0;
