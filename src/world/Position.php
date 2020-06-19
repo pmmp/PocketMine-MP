fb4a99a0efacd121146d5494cbfded399d8999db
Dylan K. Taylor <odigiman@gmail.com>
2020-06-19 11:21:47 +0100

    Position: x,y,z parameters are now mandatory

--- src/world/Position.php
+++ src/world/Position.php
@@ -37,7 +37,7 @@ class Position extends Vector3{
 	 * @param float|int $y
 	 * @param float|int $z
 	 */
-	public function __construct($x = 0, $y = 0, $z = 0, ?World $world = null){
+	public function __construct($x, $y, $z, ?World $world = null){
 		parent::__construct($x, $y, $z);
 		if($world !== null and $world->isClosed()){
 			throw new \InvalidArgumentException("Specified world has been unloaded and cannot be used");
