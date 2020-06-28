680c0002e2af80084f391649f16f5d16e074b652
Dylan K. Taylor <odigiman@gmail.com>
2020-06-28 17:41:06 +0100

    added FoodSourceItem interface

--- src/item/FoodSourceItem.php
+++ src/item/FoodSourceItem.php
@@ -24,22 +24,7 @@ declare(strict_types=1);
 namespace pocketmine\item;
 
 use pocketmine\entity\FoodSource;
-use pocketmine\entity\Living;
 
-abstract class Food extends Item implements FoodSourceItem{
-	public function requiresHunger() : bool{
-		return true;
-	}
+interface FoodSourceItem extends ConsumableItem, FoodSource{
 
-	public function getResidue() : Item{
-		return ItemFactory::air();
-	}
-
-	public function getAdditionalEffects() : array{
-		return [];
-	}
-
-	public function onConsume(Living $consumer) : void{
-
-	}
 }
