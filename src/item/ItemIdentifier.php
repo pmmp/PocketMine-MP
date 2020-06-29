0b05fd198733ac5d4eebf4cfa720c63a9efa070b
Dylan K. Taylor <odigiman@gmail.com>
2020-06-29 18:54:47 +0100

    added missing file

---
+++ src/item/ItemIdentifier.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\item;
+
+final class ItemIdentifier{
+
+	/** @var int */
+	private $id;
+	/** @var int */
+	private $meta;
+
+	public function __construct(int $id, int $meta){
+		if($id < -0x8000 or $id > 0x7fff){ //signed short range
+			throw new \InvalidArgumentException("ID must be in range " . -0x8000 . " - " . 0x7fff);
+		}
+		$this->id = $id;
+		$this->meta = $meta !== -1 ? $meta & 0x7FFF : -1;
+	}
+
+	public function getId() : int{
+		return $this->id;
+	}
+
+	public function getMeta() : int{
+		return $this->meta;
+	}
+}
