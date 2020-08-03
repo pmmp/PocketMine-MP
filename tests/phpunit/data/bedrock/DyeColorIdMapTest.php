164c1552d1782a9a0e34c0eb0412940b85188946
Dylan K. Taylor <odigiman@gmail.com>
2020-08-03 16:48:34 +0100

    Add test to make sure DyeColorIdMap recognizes all colours this will be useful for integrity testing if more colours get added.

---
+++ tests/phpunit/data/bedrock/DyeColorIdMapTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\data\bedrock;
+
+use PHPUnit\Framework\TestCase;
+use pocketmine\block\utils\DyeColor;
+
+class DyeColorIdMapTest extends TestCase{
+
+	public function testAllColorsMapped() : void{
+		foreach(DyeColor::getAll() as $color){
+			$id = DyeColorIdMap::getInstance()->toId($color);
+			$color2 = DyeColorIdMap::getInstance()->fromId($id);
+			self::assertTrue($color->equals($color2));
+		}
+	}
+}
