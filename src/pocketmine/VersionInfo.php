cfc253bb7835f2915fbced1e8f565418aa24266f
brokiem <61258338+brokiem@users.noreply.github.com>
2020-09-10 19:34:56 +0800

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -32,7 +32,7 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 }
 const _VERSION_INFO_INCLUDED = true;
 
-const NAME = "PocketMine-MP";
+const NAME = "Zafkiel";
 const BASE_VERSION = "3.15.2";
 const IS_DEVELOPMENT_BUILD = true;
 const BUILD_NUMBER = 0;
