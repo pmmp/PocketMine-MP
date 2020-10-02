a77970448e13af2b5123ee2dc7a8bc63232a300d
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-02 04:07:49 -0700

    Emerald ore in Mountains biome (#3750)

--- src/world/biome/MountainsBiome.php
+++ src/world/biome/MountainsBiome.php
@@ -23,6 +23,9 @@ declare(strict_types=1);
 
 namespace pocketmine\world\biome;
 
+use pocketmine\block\VanillaBlocks;
+use pocketmine\world\generator\object\OreType;
+use pocketmine\world\generator\populator\Ore;
 use pocketmine\world\generator\populator\TallGrass;
 use pocketmine\world\generator\populator\Tree;
 
@@ -40,7 +43,12 @@ class MountainsBiome extends GrassyBiome{
 
 		$this->addPopulator($tallGrass);
 
-		//TODO: add emerald
+		$ores = new Ore();
+		$ores->setOreTypes([
+			new OreType(VanillaBlocks::EMERALD_ORE(), 11, 1, 0, 32)
+		]);
+
+		$this->addPopulator($ores);
 
 		$this->setElevation(63, 127);
 
