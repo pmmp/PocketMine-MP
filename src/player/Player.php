2d3da6c23a43a8d35b11730a4cb0ff52a232be41
alvin0319 <alvin7810@naver.com>
2020-10-12 22:30:45 +0900

    Player: sync command data when player join/quit

--- src/player/Player.php
+++ src/player/Player.php
@@ -832,6 +832,10 @@ class Player extends Human implements CommandSender, ChunkListener, IPlayer{
 		if($this->getHealth() <= 0){
 			$this->respawn();
 		}
+
+		foreach($this->server->getOnlinePlayers() as $player){
+			$player->getNetworkSession()->syncAvailableCommands();
+		}
 	}
 
 	protected function orderChunks() : void{
@@ -1972,6 +1976,10 @@ class Player extends Human implements CommandSender, ChunkListener, IPlayer{
 			$this->server->broadcastMessage($quitMessage);
 		}
 		$this->save();
+
+		foreach($this->server->getOnlinePlayers() as $player){
+			$player->getNetworkSession()->syncAvailableCommands();
+		}
 
 		$this->spawned = false;
 
