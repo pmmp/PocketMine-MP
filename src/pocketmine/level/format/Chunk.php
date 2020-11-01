pmmp/PocketMine-MP

commit 2bb497b7162719507184e0e3a36027cedd7bbe29

Chunk: flag hasChanged when changing generated/populated/light-populated flags

src/pocketmine/level/format/Chunk.php
@@ -548,6 +548,7 @@ class Chunk{
 	 */
 	public function setLightPopulated(bool $value = true){
 		$this->lightPopulated = $value;
+		$this->hasChanged = true;
 	}
 
 	public function isPopulated() : bool{
@@ -559,6 +560,7 @@ class Chunk{
 	 */
 	public function setPopulated(bool $value = true){
 		$this->terrainPopulated = $value;
+		$this->hasChanged = true;
 	}
 
 	public function isGenerated() : bool{
@@ -570,6 +572,7 @@ class Chunk{
 	 */
 	public function setGenerated(bool $value = true){
 		$this->terrainGenerated = $value;
+		$this->hasChanged = true;
 	}
 
 	/**

commit 6cff08cd65a9badcd0ae20f0d443035f17bcd3cf

Chunk: fixed hasChanged being set on fastDeserialize() chunks (caused by 2bb497b7162719507184e0e3a36027cedd7bbe29) this caused some performance issues and silent bugs with the generator, notably that the generator would always think all chunks had been changed, causing them to be re-set back into the world 9 times.

src/pocketmine/level/format/Chunk.php
@@ -929,6 +929,7 @@ class Chunk{
 		$chunk->setGenerated($terrainGenerated);
 		$chunk->setPopulated($terrainPopulated);
 		$chunk->setLightPopulated($lightPopulated);
+		$chunk->setChanged(false);
 
 		return $chunk;
 	}