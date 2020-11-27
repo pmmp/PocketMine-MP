ab18332572468dba15bf38e82d0a9ac58e9fdd6c
Dylan K. Taylor <odigiman@gmail.com>
2020-11-27 19:16:07 +0000

    PermissionManager: fixed nonsensical PHPDoc type

--- src/pocketmine/permission/PermissionManager.php
+++ src/pocketmine/permission/PermissionManager.php
@@ -157,7 +157,7 @@ class PermissionManager{
 	}
 
 	/**
-	 * @return array|Permissible[]
+	 * @return Permissible[]
 	 */
 	public function getPermissionSubscriptions(string $permission) : array{
 		return $this->permSubs[$permission] ?? [];
