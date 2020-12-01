bac986d0b2b358f4580c5c1f614653392fe86b17
DerDevHD <59358798+DerDevHD@users.noreply.github.com>
2020-12-01 16:47:55 +0100

    Fixed crash when executing command /version with multiple authors declared in 'author' (#3940)
    closes #3902

--- src/pocketmine/plugin/PluginDescription.php
+++ src/pocketmine/plugin/PluginDescription.php
@@ -156,7 +156,11 @@ class PluginDescription{
 		}
 		$this->authors = [];
 		if(isset($plugin["author"])){
-			$this->authors[] = $plugin["author"];
+			if(is_array($plugin["author"])){
+				$this->authors = $plugin["author"];
+			}else{
+				$this->authors[] = $plugin["author"];
+			}
 		}
 		if(isset($plugin["authors"])){
 			foreach($plugin["authors"] as $author){
