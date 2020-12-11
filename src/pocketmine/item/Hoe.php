61aa97e2d03d86649ee8233f9d8d998d4bb914f6
Mohamed <mohamedelyousfi87@gmail.com>
2020-12-11 20:55:50 +0100

    When a hoe is used, damage is applied

--- src/pocketmine/item/Hoe.php
+++ src/pocketmine/item/Hoe.php
@@ -23,11 +23,19 @@ declare(strict_types=1);
 
 namespace pocketmine\item;
 
+use pocketmine\block\Block;
 use pocketmine\entity\Entity;
 
 class Hoe extends TieredTool{
 
 	public function onAttackEntity(Entity $victim) : bool{
 		return $this->applyDamage(1);
+	}
+
+	public function onDestroyBlock(Block $block) : bool{
+		if($block->getHardness() > 0){
+			return $this->applyDamage(1);
+		}
+		return false;
 	}
 }
