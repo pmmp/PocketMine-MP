e156fb47e878dff09cbf233062b2e30444658860
Dylan T <odigiman@gmail.com>
2021-01-15 21:09:45 +0000

    Disable xdebug on Actions

--- tests/gh-actions/build.sh
+++ tests/gh-actions/build.sh
@@ -22,3 +22,4 @@ cd php-build
 ./install-dependencies.sh
 echo '"pthreads",,"https://github.com/pmmp/pthreads.git",,,"extension",' >> share/php-build/extension/definition
 PHP_BUILD_INSTALL_EXTENSION='pthreads=@2bcd8b8c10395d58b8a9bc013e3a5328080c867f yaml=2.2.0' PHP_BUILD_ZTS_ENABLE=on ./bin/php-build "$VERSION" "$INSTALL_DIR"
+rm "$INSTALL_DIR/etc/conf.d/xdebug.ini" || true
