d4290837f33655fd0de629bbac1228cedd680526
Dylan K. Taylor <dktapps@pmmp.io>
2021-02-02 13:55:56 +0000

    fix build failure

--- tests/phpunit/world/format/io/region/RegionLoaderTest.php
+++ tests/phpunit/world/format/io/region/RegionLoaderTest.php
@@ -121,12 +121,12 @@ class RegionLoaderTest extends TestCase{
 	 */
 	public function testRegionHeaderCachedFilesizeRegression() : void{
 		$this->region->close();
-		$region = new RegionLoader($this->regionPath, 0, 0); //now we have a region, so the header will be verified, triggering two filesize() calls
+		$region = new RegionLoader($this->regionPath); //now we have a region, so the header will be verified, triggering two filesize() calls
 		$region->open();
 		$data = str_repeat("hello", 2000);
 		$region->writeChunk(0, 0, $data); //add some data to the end of the file, to make the cached filesize invalid
 		$region->close();
-		$region = new RegionLoader($this->regionPath, 0, 0);
+		$region = new RegionLoader($this->regionPath);
 		$region->open();
 		self::assertSame($data, $region->readChunk(0, 0));
 	}
