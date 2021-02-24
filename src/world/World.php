a49ee0d6b01fa34989e04ff7fffc0c3f0d603b42
Dylan K. Taylor <dktapps@pmmp.io>
2021-02-24 23:42:09 +0000

    World: mark addTile() and removeTile() as @internal

--- src/world/World.php
+++ src/world/World.php
@@ -2244,6 +2244,7 @@ class World implements ChunkManager{
 	}
 
 	/**
+	 * @internal Tiles are now bound with blocks, and their creation is automatic. They should not be directly added.
 	 * @throws \InvalidArgumentException
 	 */
 	public function addTile(Tile $tile) : void{
@@ -2269,6 +2270,7 @@ class World implements ChunkManager{
 	}
 
 	/**
+	 * @internal Tiles are now bound with blocks, and their removal is automatic. They should not be directly removed.
 	 * @throws \InvalidArgumentException
 	 */
 	public function removeTile(Tile $tile) : void{
