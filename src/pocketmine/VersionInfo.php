f95fcecb5b061f6f0bd7d80d06830e26a501f64c
Dylan K. Taylor <dktapps@pmmp.io>
2021-03-10 20:49:10 +0000

    3.17.7 is next

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -33,6 +33,6 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 const _VERSION_INFO_INCLUDED = true;
 
 const NAME = "PocketMine-MP";
-const BASE_VERSION = "3.17.6";
-const IS_DEVELOPMENT_BUILD = false;
+const BASE_VERSION = "3.17.7";
+const IS_DEVELOPMENT_BUILD = true;
 const BUILD_NUMBER = 0;
