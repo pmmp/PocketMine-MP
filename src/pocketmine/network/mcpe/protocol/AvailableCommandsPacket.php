ee868bcccca69c02a3ea724667c2d9c4cd0b114c
Nick <nick@npbe.net>
2021-03-15 21:22:01 +0300

    update argument type constants (#4082)

--- src/pocketmine/network/mcpe/protocol/AvailableCommandsPacket.php
+++ src/pocketmine/network/mcpe/protocol/AvailableCommandsPacket.php
@@ -49,25 +49,26 @@ class AvailableCommandsPacket extends DataPacket{
 	 * ARG_FLAG_VALID | (type const)
 	 */
 	public const ARG_TYPE_INT             = 0x01;
-	public const ARG_TYPE_FLOAT           = 0x02;
-	public const ARG_TYPE_VALUE           = 0x03;
-	public const ARG_TYPE_WILDCARD_INT    = 0x04;
-	public const ARG_TYPE_OPERATOR        = 0x05;
-	public const ARG_TYPE_TARGET          = 0x06;
+	public const ARG_TYPE_FLOAT           = 0x03;
+	public const ARG_TYPE_VALUE           = 0x04;
+	public const ARG_TYPE_WILDCARD_INT    = 0x05;
+	public const ARG_TYPE_OPERATOR        = 0x06;
+	public const ARG_TYPE_TARGET          = 0x07;
+	public const ARG_TYPE_WILDCARD_TARGET = 0x08;
 
-	public const ARG_TYPE_FILEPATH = 0x0e;
+	public const ARG_TYPE_FILEPATH = 0x10;
 
-	public const ARG_TYPE_STRING   = 0x1d;
+	public const ARG_TYPE_STRING   = 0x20;
 
-	public const ARG_TYPE_POSITION = 0x25;
+	public const ARG_TYPE_POSITION = 0x28;
 
-	public const ARG_TYPE_MESSAGE  = 0x29;
+	public const ARG_TYPE_MESSAGE  = 0x2c;
 
-	public const ARG_TYPE_RAWTEXT  = 0x2b;
+	public const ARG_TYPE_RAWTEXT  = 0x2e;
 
-	public const ARG_TYPE_JSON     = 0x2f;
+	public const ARG_TYPE_JSON     = 0x32;
 
-	public const ARG_TYPE_COMMAND  = 0x36;
+	public const ARG_TYPE_COMMAND  = 0x3f;
 
 	/**
 	 * Enums are a little different: they are composed as follows:
