ab9615fb9c3ad1111ae7bd19c8738e2a3378337b
Dylan K. Taylor <dktapps@pmmp.io>
2021-04-15 21:19:58 +0100

    World: fixed documentation for getHighestAdjacentPotentialBlockSkyLight() [ci skip]

--- src/world/World.php
+++ src/world/World.php
@@ -1318,7 +1318,7 @@ class World implements ChunkManager{
 	}
 
 	/**
-	 * Returns the highest block light level available in the positions adjacent to the specified block coordinates.
+	 * Returns the highest potential level of sky light in the positions adjacent to the specified block coordinates.
 	 */
 	public function getHighestAdjacentPotentialBlockSkyLight(int $x, int $y, int $z) : int{
 		return $this->getHighestAdjacentLight($x, $y, $z, \Closure::fromCallable([$this, 'getPotentialBlockSkyLightAt']));
