0402e7e6977989608b4bdd0a843a571cb99a9284
Dylan K. Taylor <dktapps@pmmp.io>
2021-05-29 23:41:11 +0100

    Separate CommandReader impl from CommandReaderThread

--- src/command/CommandReader.php
+++ src/command/CommandReader.php
@@ -23,149 +23,58 @@ declare(strict_types=1);
 
 namespace pocketmine\command;
 
-use pocketmine\snooze\SleeperNotifier;
-use pocketmine\thread\Thread;
-use pocketmine\thread\ThreadException;
 use function fclose;
 use function fgets;
 use function fopen;
-use function fstat;
 use function is_resource;
-use function microtime;
-use function preg_replace;
-use function readline;
-use function stream_isatty;
 use function stream_select;
 use function trim;
 use function usleep;
 
-class CommandReader extends Thread{
-
-	public const TYPE_READLINE = 0;
-	public const TYPE_STREAM = 1;
-	public const TYPE_PIPED = 2;
-
+final class CommandReader{
 	/** @var resource */
-	private static $stdin;
+	private $stdin;
 
-	/** @var \Threaded */
-	protected $buffer;
-	/** @var bool */
-	private $shutdown = false;
-	/** @var int */
-	private $type = self::TYPE_STREAM;
-
-	/** @var SleeperNotifier|null */
-	private $notifier;
-
-	public function __construct(?SleeperNotifier $notifier = null){
-		$this->buffer = new \Threaded;
-		$this->notifier = $notifier;
-	}
-
-	public function shutdown() : void{
-		$this->shutdown = true;
-	}
-
-	public function quit() : void{
-		$wait = microtime(true) + 0.5;
-		while(microtime(true) < $wait){
-			if($this->isRunning()){
-				usleep(100000);
-			}else{
-				parent::quit();
-				return;
-			}
-		}
-
-		$message = "Thread blocked for unknown reason";
-		if($this->type === self::TYPE_PIPED){
-			$message = "STDIN is being piped from another location and the pipe is blocked, cannot stop safely";
-		}
-
-		throw new ThreadException($message);
+	public function __construct(){
+		$this->initStdin();
 	}
 
 	private function initStdin() : void{
-		if(is_resource(self::$stdin)){
-			fclose(self::$stdin);
+		if(is_resource($this->stdin)){
+			fclose($this->stdin);
 		}
 
-		self::$stdin = fopen("php://stdin", "r");
-		if($this->isPipe(self::$stdin)){
-			$this->type = self::TYPE_PIPED;
-		}else{
-			$this->type = self::TYPE_STREAM;
-		}
-	}
-
-	/**
-	 * Checks if the specified stream is a FIFO pipe.
-	 *
-	 * @param resource $stream
-	 */
-	private function isPipe($stream) : bool{
-		return is_resource($stream) and (!stream_isatty($stream) or ((fstat($stream)["mode"] & 0170000) === 0010000));
+		$this->stdin = fopen("php://stdin", "r");
 	}
 
 	/**
 	 * Reads a line from the console and adds it to the buffer. This method may block the thread.
-	 *
-	 * @return bool if the main execution should continue reading lines
 	 */
-	private function readLine() : bool{
-		if(!is_resource(self::$stdin)){
+	public function readLine() : ?string{
+		if(!is_resource($this->stdin)){
 			$this->initStdin();
 		}
 
-		$r = [self::$stdin];
+		$r = [$this->stdin];
 		$w = $e = null;
 		if(($count = stream_select($r, $w, $e, 0, 200000)) === 0){ //nothing changed in 200000 microseconds
-			return true;
+			return null;
 		}elseif($count === false){ //stream error
 			$this->initStdin();
 		}
 
-		if(($raw = fgets(self::$stdin)) === false){ //broken pipe or EOF
+		if(($raw = fgets($this->stdin)) === false){ //broken pipe or EOF
 			$this->initStdin();
-			$this->synchronized(function() : void{
-				$this->wait(200000);
-			}); //prevent CPU waste if it's end of pipe
-			return true; //loop back round
+			usleep(200000); //prevent CPU waste if it's end of pipe
+			return null; //loop back round
 		}
 
 		$line = trim($raw);
 
-		if($line !== ""){
-			$this->buffer[] = preg_replace("#\\x1b\\x5b([^\\x1b]*\\x7e|[\\x40-\\x50])#", "", $line);
-			if($this->notifier !== null){
-				$this->notifier->wakeupSleeper();
-			}
-		}
-
-		return true;
+		return $line !== "" ? $line : null;
 	}
 
-	/**
-	 * Reads a line from console, if available. Returns null if not available
-	 */
-	public function getLine() : ?string{
-		if($this->buffer->count() !== 0){
-			return (string) $this->buffer->shift();
-		}
-
-		return null;
-	}
-
-	protected function onRun() : void{
-		$this->initStdin();
-
-		while(!$this->shutdown and $this->readLine());
-
-		fclose(self::$stdin);
-	}
-
-	public function getThreadName() : string{
-		return "Console";
+	public function __destruct(){
+		fclose($this->stdin);
 	}
 }
