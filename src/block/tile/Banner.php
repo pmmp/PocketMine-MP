5c7f5242ba74a45b36b442cdeb0f89c0a8cf6189
jasonwynn10 <jasonwynn10@me.com>
2021-06-05 09:36:38 -0400

    Copy item data to the banner tile

--- src/block/tile/Banner.php
+++ src/block/tile/Banner.php
@@ -27,6 +27,7 @@ use pocketmine\block\utils\BannerPatternLayer;
 use pocketmine\block\utils\DyeColor;
 use pocketmine\data\bedrock\BannerPatternTypeIdMap;
 use pocketmine\data\bedrock\DyeColorIdMap;
+use pocketmine\item\Item;
 use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\IntTag;
@@ -118,6 +119,12 @@ class Banner extends Spawnable{
 		}
 		$nbt->setTag(self::TAG_PATTERNS, $patterns);
 		$nbt->setInt(self::TAG_TYPE, $this->illagerPattern ? 1 : 0);
+	}
+
+	public function copyDataFromItem(Item $item) : void{
+		parent::copyDataFromItem($item);
+		/** @var \pocketmine\item\Banner $item */
+		$this->illagerPattern = $item->isIllagerPattern();
 	}
 
 	/**
