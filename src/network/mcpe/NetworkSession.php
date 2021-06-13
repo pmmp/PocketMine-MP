3d0b21f30c3a20d7b1d4abcba3ed24c62521c919
Dylan K. Taylor <dktapps@pmmp.io>
2021-06-13 22:27:23 +0100

    Denoise NetworkSession with typed properties

--- src/network/mcpe/NetworkSession.php
+++ src/network/mcpe/NetworkSession.php
@@ -125,72 +125,50 @@ use function time;
 use function ucfirst;
 
 class NetworkSession{
-	/** @var \PrefixedLogger */
-	private $logger;
-	/** @var Server */
-	private $server;
-	/** @var Player|null */
-	private $player = null;
-	/** @var NetworkSessionManager */
-	private $manager;
-	/** @var string */
-	private $ip;
-	/** @var int */
-	private $port;
-	/** @var PlayerInfo|null */
-	private $info = null;
-	/** @var int|null */
-	private $ping = null;
+	private \PrefixedLogger $logger;
+	private Server $server;
+	private ?Player $player = null;
+	private NetworkSessionManager $manager;
+	private string $ip;
+	private int $port;
+	private ?PlayerInfo $info = null;
+	private ?int $ping = null;
 
-	/** @var PacketHandler|null */
-	private $handler = null;
+	private ?PacketHandler $handler = null;
 
-	/** @var bool */
-	private $connected = true;
-	/** @var bool */
-	private $disconnectGuard = false;
-	/** @var bool */
-	private $loggedIn = false;
-	/** @var bool */
-	private $authenticated = false;
-	/** @var int */
-	private $connectTime;
-	/** @var CompoundTag|null */
-	private $cachedOfflinePlayerData = null;
+	private bool $connected = true;
+	private bool $disconnectGuard = false;
+	private bool $loggedIn = false;
+	private bool $authenticated = false;
+	private int $connectTime;
+	private ?CompoundTag $cachedOfflinePlayerData = null;
 
-	/** @var EncryptionContext|null */
-	private $cipher = null;
+	private ?EncryptionContext $cipher = null;
 
 	/** @var Packet[] */
-	private $sendBuffer = [];
+	private array $sendBuffer = [];
 
 	/**
 	 * @var \SplQueue|CompressBatchPromise[]
 	 * @phpstan-var \SplQueue<CompressBatchPromise>
 	 */
-	private $compressedQueue;
-	/** @var Compressor */
-	private $compressor;
-	/** @var bool */
-	private $forceAsyncCompression = true;
+	private \SplQueue $compressedQueue;
+	private Compressor $compressor;
+	private bool $forceAsyncCompression = true;
 
-	/** @var PacketPool */
-	private $packetPool;
+	private PacketPool $packetPool;
 
-	/** @var InventoryManager|null */
-	private $invManager = null;
+	private ?InventoryManager $invManager = null;
 
-	/** @var PacketSender */
-	private $sender;
+	private PacketSender $sender;
 
-	/** @var PacketBroadcaster */
-	private $broadcaster;
+	private PacketBroadcaster $broadcaster;
 
 	/**
 	 * @var \Closure[]|ObjectSet
 	 * @phpstan-var ObjectSet<\Closure() : void>
 	 */
-	private $disposeHooks;
+	private ObjectSet $disposeHooks;
 
 	public function __construct(Server $server, NetworkSessionManager $manager, PacketPool $packetPool, PacketSender $sender, PacketBroadcaster $broadcaster, Compressor $compressor, string $ip, int $port){
 		$this->server = $server;
