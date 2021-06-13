6fb364b16f6f295635c4333fb6d68c399584a2a4
Jason <jasonwynn10@me.com>
2021-06-13 09:37:09 -0400

    Store ender chest viewer count on the tile, instead of relying on the inventory's viewer count (#4238)
    Fixes #4021

--- src/block/tile/EnderChest.php
+++ src/block/tile/EnderChest.php
@@ -27,6 +27,19 @@ use pocketmine\nbt\tag\CompoundTag;
 
 class EnderChest extends Spawnable{
 
+	protected int $viewerCount = 0;
+
+	public function getViewerCount() : int{
+		return $this->viewerCount;
+	}
+
+	public function setViewerCount(int $viewerCount) : void{
+		if($viewerCount < 0){
+			throw new \InvalidArgumentException('Viewer count cannot be negative');
+		}
+		$this->viewerCount = $viewerCount;
+	}
+
 	public function readSaveData(CompoundTag $nbt) : void{
 
 	}
