c07f3f5e12ec202d6450621d16557db03de88aff
Dylan K. Taylor <dktapps@pmmp.io>
2021-06-19 19:48:30 +0100

    fix CS

--- src/entity/object/ExperienceOrb.php
+++ src/entity/object/ExperienceOrb.php
@@ -28,8 +28,6 @@ use pocketmine\entity\EntitySizeInfo;
 use pocketmine\entity\Human;
 use pocketmine\entity\Location;
 use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\IntTag;
-use pocketmine\nbt\tag\ShortTag;
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataCollection;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
