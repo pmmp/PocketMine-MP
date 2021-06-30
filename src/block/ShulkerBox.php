bceb481f234e9e1320b7060111191044cd9b75e1
Dylan K. Taylor <dktapps@pmmp.io>
2021-06-30 19:46:14 +0100

    Shulker boxes have a max stack size of 1

--- src/block/ShulkerBox.php
+++ src/block/ShulkerBox.php
@@ -49,6 +49,10 @@ class ShulkerBox extends Opaque{
 		}
 	}
 
+	public function getMaxStackSize() : int{
+		return 1;
+	}
+
 	public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
 		$this->facing = $face;
 
