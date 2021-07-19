9660aecd45f679073c4d0e210fa9783be46f5c7f
Dylan K. Taylor <dktapps@pmmp.io>
2021-07-19 19:02:21 +0100

    more CS fixes

--- src/item/SweetBerries.php
+++ src/item/SweetBerries.php
@@ -26,17 +26,17 @@ namespace pocketmine\item;
 use pocketmine\block\Block;
 use pocketmine\block\VanillaBlocks;
 
-class SweetBerries extends Food {
+class SweetBerries extends Food{
 
-	public function getFoodRestore(): int{
+	public function getFoodRestore() : int{
 		return 2;
 	}
 
-	public function getSaturationRestore(): float{
+	public function getSaturationRestore() : float{
 		return 1.2;
 	}
 
-	public function getBlock(?int $clickedFace = null): Block{
+	public function getBlock(?int $clickedFace = null) : Block{
 		return VanillaBlocks::SWEET_BERRY_BUSH();
 	}
 }
