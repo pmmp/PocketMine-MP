2cc0fa83362afd8f984cfc5dc0e92edbde014557
Dylan T <odigiman@gmail.com>
2021-07-22 14:21:05 +0100

    Knockback enchantment should be subjected to the same limit

--- src/item/enchantment/KnockbackEnchantment.php
+++ src/item/enchantment/KnockbackEnchantment.php
@@ -39,7 +39,7 @@ class KnockbackEnchantment extends MeleeWeaponEnchantment{
 	public function onPostAttack(Entity $attacker, Entity $victim, int $enchantmentLevel) : void{
 		if($victim instanceof Living){
 			$diff = $victim->getPosition()->subtractVector($attacker->getPosition());
-			$victim->knockBack($diff->x, $diff->z, $enchantmentLevel * 0.5);
+			$victim->knockBack($diff->x, $diff->z, $enchantmentLevel * 0.5, 0.4);
 		}
 	}
 }
