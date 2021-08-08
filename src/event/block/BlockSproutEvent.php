1202fd7048a62d7724d42e3bbb2d49104b4f4f03
ColinHDev <colinheidfeld@gmail.com>
2021-08-08 02:07:34 +0200

    added BlockSproutEvent class

---
+++ src/event/block/BlockSproutEvent.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace pocketmine\event\block;
+
+use pocketmine\block\Block;
+use pocketmine\world\BlockTransaction;
+
+/**
+ * Called when Saplings or Bamboo grow.
+ * These types of plants tend to change multiple blocks at once upon growing, that's why this event returns the BlockTransaction.
+ */
+class BlockSproutEvent extends BaseBlockChangeEvent {
+
+	private BlockTransaction $transaction;
+
+	public function __construct(Block $block, Block $newState, BlockTransaction $transaction) {
+		parent::__construct($block, $newState);
+		$this->transaction = $transaction;
+	}
+
+	public function getTransaction() : BlockTransaction {
+		return $this->transaction;
+	}
+}
