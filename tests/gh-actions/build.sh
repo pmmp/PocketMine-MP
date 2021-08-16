5a970541f964f4b45890e8e08a065ee5fdb7e875
Dylan K. Taylor <dktapps@pmmp.io>
2021-08-16 23:15:47 +0100

    actions: use newer pthreads version

--- tests/gh-actions/build.sh
+++ tests/gh-actions/build.sh
@@ -21,5 +21,5 @@ git clone https://github.com/php-build/php-build.git
 cd php-build
 ./install-dependencies.sh
 echo '"pthreads",,"https://github.com/pmmp/pthreads.git",,,"extension",' >> share/php-build/extension/definition
-PHP_BUILD_INSTALL_EXTENSION='pthreads=@acc6e52b2144c61c434b62a3cb680d537e06828e yaml=2.2.1' PHP_BUILD_ZTS_ENABLE=on ./bin/php-build "$VERSION" "$INSTALL_DIR" || exit 1
+PHP_BUILD_INSTALL_EXTENSION='pthreads=@a6afc0434f91c1e9541444aef6ac7a1f16c595be yaml=2.2.1' PHP_BUILD_ZTS_ENABLE=on ./bin/php-build "$VERSION" "$INSTALL_DIR" || exit 1
 rm "$INSTALL_DIR/etc/conf.d/xdebug.ini" || true
