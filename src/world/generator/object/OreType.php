e0d6357eb7bdeae086816f6269a66e5b133cdecd
Dylan K. Taylor <dktapps@pmmp.io>
2021-08-30 00:18:34 +0100

    OreType: use promoted constructo properties

--- src/world/generator/object/OreType.php
+++ src/world/generator/object/OreType.php
@@ -26,25 +26,12 @@ namespace pocketmine\world\generator\object;
 use pocketmine\block\Block;
 
 class OreType{
-	/** @var Block */
-	public $material;
-	/** @var Block */
-	public $replaces;
-	/** @var int */
-	public $clusterCount;
-	/** @var int */
-	public $clusterSize;
-	/** @var int */
-	public $maxHeight;
-	/** @var int */
-	public $minHeight;
-
-	public function __construct(Block $material, Block $replaces, int $clusterCount, int $clusterSize, int $minHeight, int $maxHeight){
-		$this->material = $material;
-		$this->replaces = $replaces;
-		$this->clusterCount = $clusterCount;
-		$this->clusterSize = $clusterSize;
-		$this->maxHeight = $maxHeight;
-		$this->minHeight = $minHeight;
-	}
+	public function __construct(
+		public Block $material,
+		public Block $replaces,
+		public int $clusterCount,
+		public int $clusterSize,
+		public int $minHeight,
+		public int $maxHeight
+	){}
 }
