b65e3c69b110102e1741e3108b911221454680e4
Dylan K. Taylor <dktapps@pmmp.io>
2021-09-10 17:18:32 +0100

    4.0.0-BETA3 is next

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -29,10 +29,10 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "PocketMine-MP";
-	public const BASE_VERSION = "4.0.0-BETA2";
-	public const IS_DEVELOPMENT_BUILD = false;
+	public const BASE_VERSION = "4.0.0-BETA3";
+	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_NUMBER = 0;
-	public const BUILD_CHANNEL = "beta";
+	public const BUILD_CHANNEL = "";
 
 	private function __construct(){
 		//NOOP
