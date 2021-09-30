67719ed00ba5ba137e5b54aa30f30f01ec3f9171
Dylan K. Taylor <dktapps@pmmp.io>
2021-09-30 18:00:43 +0100

    Remove unnecessary nullsafe operator

--- src/block/tile/BrewingStand.php
+++ src/block/tile/BrewingStand.php
@@ -205,7 +205,7 @@ class BrewingStand extends Spawnable implements Container, Nameable{
 							continue;
 						}
 
-						$output = $recipe?->getOutputFor($input);
+						$output = $recipe->getOutputFor($input);
 						if($output === null){
 							continue;
 						}
