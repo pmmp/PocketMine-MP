b54854529f8a1366e156f8ec32e02635ec670e95
Dylan K. Taylor <dktapps@pmmp.io>
2021-10-09 20:20:37 +0100

    Release 3.24.0

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -33,7 +33,7 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 const _VERSION_INFO_INCLUDED = true;
 
 const NAME = "PocketMine-MP";
-const BASE_VERSION = "3.23.2";
-const IS_DEVELOPMENT_BUILD = true;
+const BASE_VERSION = "3.24.0";
+const IS_DEVELOPMENT_BUILD = false;
 const BUILD_NUMBER = 0;
-const BUILD_CHANNEL = "";
+const BUILD_CHANNEL = "stable";
