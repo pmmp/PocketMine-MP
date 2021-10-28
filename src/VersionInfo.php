7effa03ba45083ad8364b38a1c6399a8d9d3c74e
Dylan K. Taylor <dktapps@pmmp.io>
2021-10-28 17:28:48 +0100

    Release 4.0.0-BETA7

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -30,7 +30,7 @@ use function str_repeat;
 final class VersionInfo{
 	public const NAME = "PocketMine-MP";
 	public const BASE_VERSION = "4.0.0-BETA7";
-	public const IS_DEVELOPMENT_BUILD = true;
+	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_NUMBER = 0;
 	public const BUILD_CHANNEL = "beta";
 
