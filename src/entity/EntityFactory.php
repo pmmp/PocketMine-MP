a125ef3a77af046c3007bec633d067465d397bf4
IvanCraft623 <57236932+IvanCraft623@users.noreply.github.com>
2021-11-04 15:03:17 -0500

    Register trident entity

--- src/entity/EntityFactory.php
+++ src/entity/EntityFactory.php
@@ -42,6 +42,7 @@ use pocketmine\entity\projectile\EnderPearl;
 use pocketmine\entity\projectile\ExperienceBottle;
 use pocketmine\entity\projectile\Snowball;
 use pocketmine\entity\projectile\SplashPotion;
+use pocketmine\entity\projectile\Trident;
 use pocketmine\item\Item;
 use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
@@ -155,6 +156,19 @@ final class EntityFactory{
 			}
 			return new SplashPotion(EntityDataHelper::parseLocation($nbt, $world), null, $potionType, $nbt);
 		}, ['ThrownPotion', 'minecraft:potion', 'thrownpotion'], EntityLegacyIds::SPLASH_POTION);
+
+		$this->register(Trident::class, function(World $world, CompoundTag $nbt) : Trident{
+			$itemTag = $nbt->getCompoundTag("item");
+			if($itemTag === null){
+				throw new \UnexpectedValueException("Expected \"item\" NBT tag not found");
+			}
+
+			$item = Item::nbtDeserialize($itemTag);
+			if($item->isNull()){
+				throw new \UnexpectedValueException("Item is invalid");
+			}
+			return new Trident(EntityDataHelper::parseLocation($nbt, $world), $item, null, $nbt);
+		}, ['Trident', 'ThrownTrident', 'minecraft:trident'], EntityLegacyIds::TRIDENT);
 
 		$this->register(Squid::class, function(World $world, CompoundTag $nbt) : Squid{
 			return new Squid(EntityDataHelper::parseLocation($nbt, $world), $nbt);
