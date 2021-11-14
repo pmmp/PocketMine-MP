399824c31c0986459bac3cbb828c04a7a75a5519
Covered123 <58715544+JavierLeon9966@users.noreply.github.com>
2021-11-14 11:15:36 -0300

    Add correct drop for Podzol (#4573)

--- src/block/Podzol.php
+++ src/block/Podzol.php
@@ -23,6 +23,13 @@ declare(strict_types=1);
 
 namespace pocketmine\block;
 
+use pocketmine\item\Item;
+
 class Podzol extends Opaque{
 
+	public function getDropsForCompatibleTool(Item $item) : array{
+		return [
+			VanillaBlocks::DIRT()->asItem()
+		];
+	}
 }
