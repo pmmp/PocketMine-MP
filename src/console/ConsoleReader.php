6f8f460a6c84f057013c74db8c47782ae4923b42
Dylan K. Taylor <dktapps@pmmp.io>
2021-11-30 00:27:52 +0000

    Partially revert "ConsoleReaderChildProcess: Commit suicide in more cases"
    This reverts commit cbe0f44c4f7bc3715acbf148f981bd93111c4c8f.
    
    This achieves the same result as the reverted commit wrt. process in the
    same manner (writing a keepalive into the socket and checking if it
    failed to send). However, it does _not_ allow the process to die on
    reaching pipe EOF, since this can cause many spams of subprocesses when
    stdin is actually not a tty (e.g. in a Docker container).

--- src/console/ConsoleReader.php
+++ src/console/ConsoleReader.php
@@ -23,38 +23,49 @@ declare(strict_types=1);
 
 namespace pocketmine\console;
 
-use pocketmine\utils\AssumptionFailedError;
 use function fclose;
 use function fgets;
 use function fopen;
+use function is_resource;
 use function stream_select;
 use function trim;
+use function usleep;
 
 final class ConsoleReader{
 	/** @var resource */
 	private $stdin;
 
 	public function __construct(){
-		$stdin = fopen("php://stdin", "r");
-		if($stdin === false) throw new AssumptionFailedError("Opening stdin should never fail");
-		$this->stdin = $stdin;
+		$this->initStdin();
+	}
+
+	private function initStdin() : void{
+		if(is_resource($this->stdin)){
+			fclose($this->stdin);
+		}
+
+		$this->stdin = fopen("php://stdin", "r");
 	}
 
 	/**
 	 * Reads a line from the console and adds it to the buffer. This method may block the thread.
-	 * @throws ConsoleReaderException
 	 */
 	public function readLine() : ?string{
+		if(!is_resource($this->stdin)){
+			$this->initStdin();
+		}
+
 		$r = [$this->stdin];
 		$w = $e = null;
 		if(($count = stream_select($r, $w, $e, 0, 200000)) === 0){ //nothing changed in 200000 microseconds
 			return null;
 		}elseif($count === false){ //stream error
-			throw new ConsoleReaderException("Unexpected EOF on select()");
+			return null;
 		}
 
 		if(($raw = fgets($this->stdin)) === false){ //broken pipe or EOF
-			throw new ConsoleReaderException("Unexpected EOF on fgets()");
+			usleep(200000); //prevent CPU waste if it's end of pipe
+			return null; //loop back round
 		}
 
 		$line = trim($raw);
