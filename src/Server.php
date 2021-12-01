98c0dd6ea89292fd37f08d5ea65eb782417bf2e2
marshall <CortexPE@users.noreply.github.com>
2021-12-01 22:25:42 +0800

    Reverted pocketmine.server.query.running change

--- src/Server.php
+++ src/Server.php
@@ -1151,8 +1151,8 @@ class Server{
 				//RakLib would normally handle the transport for Query packets
 				//if it's not registered we need to make sure Query still works
 				$this->network->registerInterface(new DedicatedQueryNetworkInterface($ip, $port, $ipV6, new \PrefixedLogger($this->logger, "Dedicated Query Interface")));
-				$this->logger->info($this->getLanguage()->translate(KnownTranslationFactory::pocketmine_server_query_running($prettyIp, (string) $port)));
 			}
+			$this->logger->info($this->getLanguage()->translate(KnownTranslationFactory::pocketmine_server_query_running($prettyIp, (string) $port)));
 		}
 		return true;
 	}
