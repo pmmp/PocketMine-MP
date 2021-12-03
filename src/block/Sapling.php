eb79390a1edb3c0f964bef56732a51cf17cc0df8
ねらひかだ <hkshirorom@yahoo.co.jp>
2021-12-03 19:57:37 +0900

    It can now be placed on the mycelium

--- src/block/Sapling.php
+++ src/block/Sapling.php
@@ -71,7 +71,8 @@ class Sapling extends Flowable{
 			$block instanceof Dirt ||
 			$block instanceof Grass ||
 			$block instanceof Podzol ||
-			$block instanceof Farmland;
+			$block instanceof Farmland ||
+			$block instanceof Mycelium;
 	}
 
 	public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
