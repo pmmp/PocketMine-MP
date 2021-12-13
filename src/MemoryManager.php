3be8472ae237bfb5d0d6f4ce1a32a2fa688ba89c
Dylan K. Taylor <dktapps@pmmp.io>
2021-12-13 12:11:49 +0000

    MemoryManager: fixed dumping of uninitialized properties closes #4643

--- src/MemoryManager.php
+++ src/MemoryManager.php
@@ -325,6 +325,9 @@ class MemoryManager{
 				if(!$property->isPublic()){
 					$property->setAccessible(true);
 				}
+				if(!$property->isInitialized()){
+					continue;
+				}
 
 				$staticCount++;
 				$staticProperties[$className][$property->getName()] = self::continueDump($property->getValue(), $objects, $refCounts, 0, $maxNesting, $maxStringSize);
@@ -447,6 +450,9 @@ class MemoryManager{
 							}
 							if(!$property->isPublic()){
 								$property->setAccessible(true);
+							}
+							if(!$property->isInitialized($object)){
+								continue;
 							}
 
 							$info["properties"][$name] = self::continueDump($property->getValue($object), $objects, $refCounts, 0, $maxNesting, $maxStringSize);
