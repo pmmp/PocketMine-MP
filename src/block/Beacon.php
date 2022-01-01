a61aace436fa0f8d66f5fb2165b213d2bfaa0f9b
ShockedPlot7560 <66992287+ShockedPlot7560@users.noreply.github.com>
2022-01-01 17:44:48 +0100

    implement beacon

--- src/block/Beacon.php
+++ src/block/Beacon.php
@@ -23,11 +23,100 @@ declare(strict_types=1);
 
 namespace pocketmine\block;
 
+use InvalidArgumentException;
+use pocketmine\block\inventory\BeaconInventory;
+use pocketmine\block\tile\Beacon as TileBeacon;
+use pocketmine\data\bedrock\EffectIdMap;
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+use function in_array;
+
 final class Beacon extends Transparent{
 
 	public function getLightLevel() : int{
 		return 15;
 	}
 
-	//TODO
+	public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
+		if($player instanceof Player){
+			$player->setCurrentWindow(new BeaconInventory($this->position));
+		}
+
+		return true;
+	}
+
+	public function onScheduledUpdate() : void{
+		$tile = $this->position->getWorld()->getTile($this->position);
+		if(!$tile instanceof TileBeacon){
+			return;
+		}
+		$primaryE = $tile->getPrimaryEffect();
+		$secondaryE = $tile->getSecondaryEffect();
+
+		if($primaryE === 0 && $secondaryE === 0 || !$this->viewSky()){
+			return;
+		}
+
+		$beaconLevel = 0;
+		for ($i = 1; $i <= 4; $i++) {
+			if(!$this->isBeaconLevelValid($i)){
+				break;
+			}
+			$beaconLevel++;
+		}
+		if($beaconLevel > 0){
+			$radius = (10 * $beaconLevel) + 10;
+			$effectDuration = 9 + (2 * $beaconLevel);
+
+			foreach ($this->position->getWorld()->getPlayers() as $player) {
+				if($player->getPosition()->distance($this->position) <= $radius){
+					if($primaryE === $secondaryE){
+						$player->getEffects()->add(new EffectInstance(EffectIdMap::getInstance()->fromId($secondaryE), $effectDuration * 20, 2));
+						break;
+					}
+					foreach ([$primaryE, $secondaryE] as $enchantment) {
+						if($enchantment !== 0){
+							$player->getEffects()->add(new EffectInstance(EffectIdMap::getInstance()->fromId($enchantment), $effectDuration * 20));
+						}
+					}
+				}
+			}
+		}
+		$this->position->getWorld()->scheduleDelayedBlockUpdate($this->position, 20 * 3);
+	}
+
+	public function isBeaconLevelValid(int $level) : bool{
+		if($level < 1 || $level > 4){
+			throw new InvalidArgumentException("Beacon level must be in range 1-4, $level given");
+		}
+
+		for ($x = -$level; $x <= $level; $x++) {
+			for ($z = -$level; $z <= $level; $z++) {
+				$block = $this->position->getWorld()->getBlock($this->position->add($x, 0, $z)->subtract(0, $level, 0));
+				$allowedBlockId = [
+					BlockLegacyIds::IRON_BLOCK,
+					BlockLegacyIds::GOLD_BLOCK,
+					BlockLegacyIds::DIAMOND_BLOCK,
+					BlockLegacyIds::EMERALD_BLOCK
+					//TODO netherite block
+				];
+				if(!in_array($block->getId(), $allowedBlockId, true)){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public function viewSky() : bool{
+		for ($y = 0; $y <= $this->position->getWorld()->getMaxY() - $this->position->getFloorY(); $y++) {
+			$block = $this->position->getWorld()->getBlock($this->position->add(0, $y, 0));
+			if(!$block instanceof Transparent && !$block instanceof Bedrock){
+				return false;
+			}
+		}
+		return true;
+	}
 }
