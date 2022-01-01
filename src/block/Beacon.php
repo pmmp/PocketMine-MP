bcae4093b542da5f8b0886959fd033474190d078
ShockedPlot7560 <66992287+ShockedPlot7560@users.noreply.github.com>
2022-01-01 17:52:23 +0100

    fix styling

--- src/block/Beacon.php
+++ src/block/Beacon.php
@@ -60,7 +60,7 @@ final class Beacon extends Transparent{
 		}
 
 		$beaconLevel = 0;
-		for ($i = 1; $i <= 4; $i++) {
+		for($i = 1; $i <= 4; $i++){
 			if(!$this->isBeaconLevelValid($i)){
 				break;
 			}
@@ -70,13 +70,13 @@ final class Beacon extends Transparent{
 			$radius = (10 * $beaconLevel) + 10;
 			$effectDuration = 9 + (2 * $beaconLevel);
 
-			foreach ($this->position->getWorld()->getPlayers() as $player) {
+			foreach($this->position->getWorld()->getPlayers() as $player){
 				if($player->getPosition()->distance($this->position) <= $radius){
 					if($primaryE === $secondaryE){
 						$player->getEffects()->add(new EffectInstance(EffectIdMap::getInstance()->fromId($secondaryE), $effectDuration * 20, 2));
 						break;
 					}
-					foreach ([$primaryE, $secondaryE] as $enchantment) {
+					foreach([$primaryE, $secondaryE] as $enchantment){
 						if($enchantment !== 0){
 							$player->getEffects()->add(new EffectInstance(EffectIdMap::getInstance()->fromId($enchantment), $effectDuration * 20));
 						}
@@ -92,8 +92,8 @@ final class Beacon extends Transparent{
 			throw new InvalidArgumentException("Beacon level must be in range 1-4, $level given");
 		}
 
-		for ($x = -$level; $x <= $level; $x++) {
-			for ($z = -$level; $z <= $level; $z++) {
+		for($x = -$level; $x <= $level; $x++){
+			for($z = -$level; $z <= $level; $z++){
 				$block = $this->position->getWorld()->getBlock($this->position->add($x, 0, $z)->subtract(0, $level, 0));
 				$allowedBlockId = [
 					BlockLegacyIds::IRON_BLOCK,
@@ -111,7 +111,7 @@ final class Beacon extends Transparent{
 	}
 
 	public function viewSky() : bool{
-		for ($y = 0; $y <= $this->position->getWorld()->getMaxY() - $this->position->getFloorY(); $y++) {
+		for($y = 0; $y <= $this->position->getWorld()->getMaxY() - $this->position->getFloorY(); $y++){
 			$block = $this->position->getWorld()->getBlock($this->position->add(0, $y, 0));
 			if(!$block instanceof Transparent && !$block instanceof Bedrock){
 				return false;
