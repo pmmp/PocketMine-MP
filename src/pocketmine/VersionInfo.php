09f50cfd3ac12b3742f766ea6602257df6f620f7
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-01-17 19:47:14 +0200

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -32,7 +32,7 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 }
 const _VERSION_INFO_INCLUDED = true;
 
-const NAME = "PocketMine-MP";
+const NAME = "DctxGames";
 const BASE_VERSION = "3.26.6";
 const IS_DEVELOPMENT_BUILD = true;
 const BUILD_CHANNEL = "pm3";
