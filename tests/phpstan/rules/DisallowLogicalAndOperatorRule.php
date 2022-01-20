b60dd1e9b4edc8eba5bedb0ad07e1436093e9453
Dylan K. Taylor <dktapps@pmmp.io>
2022-01-20 16:43:58 +0000

    Ban 'and' and 'or' operators via PHPStan

---
+++ tests/phpstan/rules/DisallowLogicalAndOperatorRule.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\phpstan\rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\BinaryOp\LogicalAnd;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/**
+ * @phpstan-implements Rule<LogicalAnd>
+ */
+final class DisallowLogicalAndOperatorRule implements Rule{
+
+	public function getNodeType() : string{
+		return LogicalAnd::class;
+	}
+
+	public function processNode(Node $node, Scope $scope) : array{
+		return [RuleErrorBuilder::message('Use of the "and" operator is discouraged. Use && instead.')->build()];
+	}
+}
