26a68ab4c090d61e9e61b621d3f76a1d5899e4db
Duo Incure <aidenaskwith92@gmail.com>
2022-01-23 02:11:59 +0000

    PHPStan

--- src/block/TallGrass.php
+++ src/block/TallGrass.php
@@ -74,7 +74,7 @@ class TallGrass extends Flowable{
 	public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
 		if($item instanceof Fertilizer){
 			$item->pop();
-			$blockAbove = $this->getPosition()->getWorld()->getBlockAt($this->position->x, $this->position->y + 1, $this->position->z);
+			$blockAbove = $this->getPosition()->getWorld()->getBlockAt((int)$this->position->x, (int)$this->position->y + 1, (int)$this->position->z);
 
 			if($blockAbove->getId() === BlockLegacyIds::AIR){
 				$doubleBlock = match ($this->getMeta()) {
