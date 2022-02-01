25fdf7e442270058f381d17d894867b7ed36fabf
Dylan K. Taylor <dktapps@pmmp.io>
2022-02-01 04:00:49 +0000

    NetherReactor: Remove dead code the state is not exposed anywhere, and we already remap invalid states to default now anyway.

--- src/block/NetherReactor.php
+++ src/block/NetherReactor.php
@@ -29,20 +29,6 @@ use pocketmine\item\VanillaItems;
 
 class NetherReactor extends Opaque{
 
-	protected int $state = BlockLegacyMetadata::NETHER_REACTOR_INACTIVE;
-
-	protected function writeStateToMeta() : int{
-		return $this->state;
-	}
-
-	public function readStateFromData(int $id, int $stateMeta) : void{
-		$this->state = BlockDataSerializer::readBoundedInt("state", $stateMeta, 0, 2);
-	}
-
-	public function getStateBitmask() : int{
-		return 0b11;
-	}
-
 	public function getDropsForCompatibleTool(Item $item) : array{
 		return [
 			VanillaItems::IRON_INGOT()->setCount(6),
