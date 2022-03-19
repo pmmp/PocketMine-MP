256315fb6f00cd45406c6b68eedbbd0557e29232
Rush2929 <76860328+Rush2929@users.noreply.github.com>
2022-03-19 15:23:47 +0900

    Fixed falling damage caused by teleportation in "PlayerToggleSneakEvent" and other events.
    Also, after the "forceMoveSync" flag is set, the position is updated and may get stuck in the teleport ACK.

--- src/network/mcpe/handler/InGamePacketHandler.php
+++ src/network/mcpe/handler/InGamePacketHandler.php
@@ -211,6 +211,13 @@ class InGamePacketHandler extends PacketHandler{
 		// Once we get a movement within a reasonable distance, treat it as a teleport ACK and remove position lock
 		$this->forceMoveSync = false;
 
+		if($packet->hasFlag(PlayerAuthInputFlags::START_JUMPING)){
+			$this->player->jump();
+		}
+
+		//TODO: this packet has WAYYYYY more useful information that we're not using
+		$this->player->handleMovement($newPos);
+
 		$sneaking = $this->resolveOnOffInputFlags($packet, PlayerAuthInputFlags::START_SNEAKING, PlayerAuthInputFlags::STOP_SNEAKING);
 		$sprinting = $this->resolveOnOffInputFlags($packet, PlayerAuthInputFlags::START_SPRINTING, PlayerAuthInputFlags::STOP_SPRINTING);
 		$swimming = $this->resolveOnOffInputFlags($packet, PlayerAuthInputFlags::START_SWIMMING, PlayerAuthInputFlags::STOP_SWIMMING);
@@ -223,13 +230,6 @@ class InGamePacketHandler extends PacketHandler{
 		if((bool) $mismatch){
 			$this->player->sendData([$this->player]);
 		}
-
-		if($packet->hasFlag(PlayerAuthInputFlags::START_JUMPING)){
-			$this->player->jump();
-		}
-
-		//TODO: this packet has WAYYYYY more useful information that we're not using
-		$this->player->handleMovement($newPos);
 
 		$packetHandled = true;
 
