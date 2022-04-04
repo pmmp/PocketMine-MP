90048b86d026dc581dcf195e8ce88c2472db1ced
Sensei Tarzan <32621158+SenseiTarzan@users.noreply.github.com>
2022-04-04 20:15:24 +0200

    test

--- src/player/Player.php
+++ src/player/Player.php
@@ -1224,19 +1224,16 @@ class Player extends Human implements CommandSender, ChunkListener, IPlayer{
 			$this->nextChunkOrderRun = 0;
 		}
 
-
-		if($revert){
-			$this->revertMovement($oldPos);
-			$this->teleport($oldPos);
-			return;
-		}
-
-		if($distanceSquared != 0){
+		if(!$revert && $distanceSquared != 0){
 			$dx = $newPos->x - $this->location->x;
 			$dy = $newPos->y - $this->location->y;
 			$dz = $newPos->z - $this->location->z;
 
 			$this->move($dx, $dy, $dz);
+		}
+		if($revert){
+			$this->revertMovement($oldPos);
+			$this->teleport($oldPos);
 		}
 
 	}
