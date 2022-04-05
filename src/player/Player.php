938a74a94d795d711f765f5a93e72a00f25b6035
Sensei Tarzan <32621158+SenseiTarzan@users.noreply.github.com>
2022-04-06 00:08:24 +0200

    oups!

--- src/player/Player.php
+++ src/player/Player.php
@@ -1197,20 +1197,19 @@ class Player extends Human implements CommandSender, ChunkListener, IPlayer{
 
 		$oldPos = $this->getLocation();
 
-		$calcul = floor(abs(($newPos->y - $oldPos->y)));
 
 		$distanceSquared = $newPos->distanceSquared($oldPos);
 
 		$revert = false;
 
-		var_dump($calcul);
+		$distanceY = floor(abs(($newPos->y - $oldPos->y)));
 		if ($jump) {
 			$this->jump();
 			if ($this->isSurvival(true) && !$this->isOnGround()) {
-				$oldPos->y -= 1;
+				$newPos->y = $oldPos->y;
 				$revert = true;
 			}
-		} else if ($this->isSurvival(true) && $calcul > 0) {
+		} else if ($this->isSurvival(true) && $distanceY > 0) {
 			$newPos->y = $oldPos->y;
 			$revert = true;
 		}
@@ -1234,7 +1233,11 @@ class Player extends Human implements CommandSender, ChunkListener, IPlayer{
 			$this->nextChunkOrderRun = 0;
 		}
 
-		if (!$revert && $distanceSquared != 0) {
+		if ($revert) {
+			$this->revertMovement($oldPos);
+			return;
+		}
+		if ($distanceSquared != 0) {
 			$dx = $newPos->x - $this->location->x;
 			$dy = $newPos->y - $this->location->y;
 			$dz = $newPos->z - $this->location->z;
@@ -1242,9 +1245,6 @@ class Player extends Human implements CommandSender, ChunkListener, IPlayer{
 			$this->move($dx, $dy, $dz);
 		}
 
-		if ($revert) {
-			$this->revertMovement($oldPos);
-		}
 
 	}
 
