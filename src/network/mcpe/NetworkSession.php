4c9d2a989eb1393126a57e12a920b30ab13dd5cf
Dylan K. Taylor <dktapps@pmmp.io>
2022-04-17 19:32:30 +0100

    cs again

--- src/network/mcpe/NetworkSession.php
+++ src/network/mcpe/NetworkSession.php
@@ -125,6 +125,7 @@ use function get_class;
 use function in_array;
 use function json_encode;
 use function ksort;
+use function strcasecmp;
 use function strlen;
 use function strtolower;
 use function substr;
