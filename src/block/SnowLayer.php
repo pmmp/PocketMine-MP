fb618ce92dc7b0fe3205c1650b931b4a16af3788
Dylan K. Taylor <dktapps@pmmp.io>
2022-05-20 12:02:17 +0100

    SnowLayer requires full support

--- src/block/SnowLayer.php
+++ src/block/SnowLayer.php
@@ -89,7 +89,7 @@ class SnowLayer extends Flowable implements Fallable{
 	}
 
 	private function canBeSupportedBy(Block $b) : bool{
-		return $b->getSupportType(Facing::UP)->hasCenterSupport();
+		return $b->getSupportType(Facing::UP)->equals(SupportType::FULL());
 	}
 
 	public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
