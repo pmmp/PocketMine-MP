bf199d3a740a9b8c657a741e528aa73d0ad4270e
Dylan K. Taylor <dktapps@pmmp.io>
2022-05-27 20:40:32 +0100

    duct tape for recipes

--- src/network/mcpe/convert/ItemTranslator.php
+++ src/network/mcpe/convert/ItemTranslator.php
@@ -28,6 +28,7 @@ use pocketmine\data\bedrock\item\ItemSerializer;
 use pocketmine\data\bedrock\item\ItemTypeSerializeException;
 use pocketmine\data\bedrock\item\SavedItemData;
 use pocketmine\item\ItemFactory;
+use pocketmine\item\ItemIds;
 use pocketmine\network\mcpe\protocol\serializer\ItemTypeDictionary;
 use pocketmine\utils\AssumptionFailedError;
 use pocketmine\utils\SingletonTrait;
@@ -86,6 +87,7 @@ final class ItemTranslator{
 	 */
 	public function toNetworkId(int $internalId, int $internalMeta) : array{
 		return $this->toNetworkIdQuiet($internalId, $internalMeta) ??
+			$this->toNetworkIdQuiet(ItemIds::INFO_UPDATE, 0) ?? //TODO: bad duct tape
 			throw new \InvalidArgumentException("Unmapped ID/metadata combination $internalId:$internalMeta");
 	}
 
