4b73bedd57ed73c2986956386e62676ba2cd586f
Dylan K. Taylor <dktapps@pmmp.io>
2022-06-01 19:45:20 +0100

    Bump version to 4.6.0+dev

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,9 +31,9 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "PocketMine-MP";
-	public const BASE_VERSION = "4.4.1";
+	public const BASE_VERSION = "4.6.0";
 	public const IS_DEVELOPMENT_BUILD = true;
-	public const BUILD_CHANNEL = "stable";
+	public const BUILD_CHANNEL = "beta";
 
 	private function __construct(){
 		//NOOP
