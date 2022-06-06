6f158600c06309f5570ee78803a833c2a8ea221b
DriesC-2158300 <coenendries3@gmail.com>
2022-06-06 23:08:11 +0200

    Fix CS

--- src/event/player/SessionDisconnectEvent.php
+++ src/event/player/SessionDisconnectEvent.php
@@ -17,7 +17,7 @@
  * @link http://www.pocketmine.net/
  *
  *
-*/
+ */
 
 declare(strict_types=1);
 
@@ -27,12 +27,8 @@ use pocketmine\event\Event;
 use pocketmine\network\mcpe\NetworkSession;
 
 class SessionDisconnectEvent extends Event{
-	/** @var NetworkSession */
-	private $session;
 
-	public function __construct(NetworkSession $session){
-		$this->session = $session;
-	}
+	public function __construct(private NetworkSession $session){}
 
 	public function getSession() : NetworkSession{
 		return $this->session;
