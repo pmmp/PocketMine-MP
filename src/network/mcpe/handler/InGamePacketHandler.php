c7133bc2e6961d772d9b016837a1742b5d9727ac
Dylan K. Taylor <dktapps@pmmp.io>
2022-07-14 20:36:11 +0100

    InGamePacketHandler: don't kick the player out of inventory windows on actor events this is sent when the player crafts something using an anvil.

--- src/network/mcpe/handler/InGamePacketHandler.php
+++ src/network/mcpe/handler/InGamePacketHandler.php
@@ -269,7 +269,6 @@ class InGamePacketHandler extends PacketHandler{
 			//TODO HACK: EATING_ITEM is sent back to the server when the server sends it for other players (1.14 bug, maybe earlier)
 			return $packet->actorRuntimeId === ActorEvent::EATING_ITEM;
 		}
-		$this->player->removeCurrentWindow();
 
 		switch($packet->eventId){
 			case ActorEvent::EATING_ITEM: //TODO: ignore this and handle it server-side
