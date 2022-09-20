ca7eda61a7fed74f3614315d9b3d2be7b99593ea
xqwtxon <xqwtxon@hotmail.com>
2022-09-20 16:41:35 +0800

    Please do not

--- src/network/NetworkInterface.php
+++ src/network/NetworkInterface.php
@@ -37,10 +37,6 @@ interface NetworkInterface{
 	 */
 	public function start() : void;
 
-	public function setName(string $name) : void;
-
-	public function setLanName(?string $lanName) : void;
-
 	/**
 	 * Called every tick to process events on the interface.
 	 */
