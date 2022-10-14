b3bda788d966603db658950ea442d997403c2720
Dylan K. Taylor <dktapps@pmmp.io>
2022-10-14 21:45:09 +0100

    Server: Deprecated getPlayerByPrefix() this is only used for commands anyway, but we can't get rid of it yet.

--- src/Server.php
+++ src/Server.php
@@ -609,6 +609,10 @@ class Server{
 	}
 
 	/**
+	 * @deprecated This method's results are unpredictable. The string "Steve" will return the player named "SteveJobs",
+	 * until another player named "Steve" joins the server, at which point it will return that player instead. Use
+	 * {@link Server::getPlayersByPrefix()} instead.
+	 *
 	 * Returns an online player whose name begins with or equals the given string (case insensitive).
 	 * The closest match will be returned, or null if there are no online matches.
 	 *
