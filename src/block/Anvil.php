fda285117ab40d3e8a474725af23d57c8f1ca515
IvanCraft623 <ivancraft623@gmail.com>
2022-10-15 22:42:25 -0500

    Fix CS

--- src/block/Anvil.php
+++ src/block/Anvil.php
@@ -37,7 +37,6 @@ use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\world\BlockTransaction;
-use pocketmine\world\sound\AnvilFallSound;
 use function lcg_value;
 use function round;
 
