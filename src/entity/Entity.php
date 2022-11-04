89d198b3aa23271d09fb8995cc9c477293b60464
ipad54 <mcpe_server@mail.ru>
2022-11-04 23:07:43 +0300

    Remove dead comments

--- src/entity/Entity.php
+++ src/entity/Entity.php
@@ -988,9 +988,7 @@ abstract class Entity{
 
 		$this->timings->stopTiming();
 
-		//if($this->isStatic())
 		return ($hasUpdate || $this->hasMovementUpdate());
-		//return !($this instanceof Player);
 	}
 
 	final public function scheduleUpdate() : void{
