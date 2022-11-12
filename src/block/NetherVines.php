16bea68d1a3f04cde908ecf5e4676913459cc828
ipad54 <mcpe_server@mail.ru>
2022-11-12 16:23:51 +0300

    Better ToolType check

--- src/block/NetherVines.php
+++ src/block/NetherVines.php
@@ -176,7 +176,7 @@ abstract class NetherVines extends Flowable{
 	}
 
 	public function getDropsForCompatibleTool(Item $item) : array{
-		if($item->getBlockToolType() === BlockToolType::SHEARS || mt_rand(0, 99) < 33){
+		if(($item->getBlockToolType() & BlockToolType::SHEARS) !== 0 || mt_rand(0, 99) < 33){
 			return [$this->asItem()];
 		}
 		return [];
