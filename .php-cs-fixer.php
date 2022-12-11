818e1164f9feb40519ea33296a0887c4d99ed6c9
Rush2929 <76860328+Rush2929@users.noreply.github.com>
2022-12-11 23:59:54 +0900

    single quoteeeeeeeeeeeeee

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -64,8 +64,8 @@ the Free Software Foundation, either version 3 of the License, or
 BODY,
 			'location' => 'after_open'
 		],
-		"native_constant_invocation" => [
-			"scope" => "namespaced"
+		'native_constant_invocation' => [
+			'scope' => 'namespaced'
 		],
 		'indentation_type' => true,
 		'logical_operators' => true,
