2f3fcef97c4596765873c21bb4e7d2f5c3c47f79
Dylan K. Taylor <dktapps@pmmp.io>
2023-01-12 19:36:23 +0000

    Fixed blocks incorrectly using blockClicked for support checks this caused some interesting bugs, such as being able to place floating pressure plates by clicking on the side of a solid block halfway up a wall.

--- src/block/PressurePlate.php
+++ src/block/PressurePlate.php
@@ -45,7 +45,7 @@ abstract class PressurePlate extends Transparent{
 	}
 
 	public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
-		if($this->canBeSupportedBy($blockClicked)){
+		if($this->canBeSupportedBy($blockReplace->getSide(Facing::DOWN))){
 			return parent::place($tx, $item, $blockReplace, $blockClicked, $face, $clickVector, $player);
 		}
 		return false;
