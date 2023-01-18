1d9336ed677ce5df380c75da11d779d13ea2c0ce
Dylan K. Taylor <dktapps@pmmp.io>
2023-01-18 16:15:43 +0000

    Release 4.13.0-BETA1

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,9 +31,9 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "PocketMine-MP";
-	public const BASE_VERSION = "4.12.11";
-	public const IS_DEVELOPMENT_BUILD = true;
-	public const BUILD_CHANNEL = "stable";
+	public const BASE_VERSION = "4.13.0-BETA1";
+	public const IS_DEVELOPMENT_BUILD = false;
+	public const BUILD_CHANNEL = "beta";
 
 	private function __construct(){
 		//NOOP
