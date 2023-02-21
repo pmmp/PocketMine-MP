2feb9ca9037ef0dd4b9bf8f4b905a5b22ed58661
Dylan K. Taylor <dktapps@pmmp.io>
2023-02-21 16:12:44 +0000

    ItemIdMetaUpgrader: consistency of API method naming

--- src/data/bedrock/item/upgrade/ItemIdMetaUpgrader.php
+++ src/data/bedrock/item/upgrade/ItemIdMetaUpgrader.php
@@ -45,11 +45,11 @@ final class ItemIdMetaUpgrader{
 		array $idMetaUpgradeSchemas,
 	){
 		foreach($idMetaUpgradeSchemas as $schema){
-			$this->addIdMetaUpgradeSchema($schema);
+			$this->addSchema($schema);
 		}
 	}
 
-	public function addIdMetaUpgradeSchema(ItemIdMetaUpgradeSchema $schema) : void{
+	public function addSchema(ItemIdMetaUpgradeSchema $schema) : void{
 		if(isset($this->idMetaUpgradeSchemas[$schema->getSchemaId()])){
 			throw new \InvalidArgumentException("Already have a schema with priority " . $schema->getSchemaId());
 		}
