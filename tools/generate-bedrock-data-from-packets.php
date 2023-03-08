7c95a65aded7f0430536950181103ae8b8f45fa9
Dylan K. Taylor <dktapps@pmmp.io>
2023-03-08 23:14:11 +0000

    ...

--- tools/generate-bedrock-data-from-packets.php
+++ tools/generate-bedrock-data-from-packets.php
@@ -53,7 +53,6 @@ use pocketmine\network\mcpe\protocol\types\CacheableNbt;
 use pocketmine\network\mcpe\protocol\types\inventory\CreativeContentEntry;
 use pocketmine\network\mcpe\protocol\types\inventory\ItemStack;
 use pocketmine\network\mcpe\protocol\types\ItemTypeEntry;
-use pocketmine\network\mcpe\protocol\types\recipe\ComplexAliasItemDescriptor;
 use pocketmine\network\mcpe\protocol\types\recipe\FurnaceRecipe;
 use pocketmine\network\mcpe\protocol\types\recipe\IntIdMetaItemDescriptor;
 use pocketmine\network\mcpe\protocol\types\recipe\MolangItemDescriptor;
@@ -283,8 +282,6 @@ class ParserPacketHandler extends PacketHandler{
 		}elseif($descriptor instanceof MolangItemDescriptor){
 			$data->molang_expression = $descriptor->getMolangExpression();
 			$data->molang_version = $descriptor->getMolangVersion();
-		}elseif($descriptor instanceof ComplexAliasItemDescriptor){
-			$data->name = $descriptor->getAlias();
 		}else{
 			throw new \UnexpectedValueException("Unknown item descriptor type " . get_class($descriptor));
 		}
