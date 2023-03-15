781b07f6b9d0bd9692953bed50c6c35988b4d3df
Dylan T <odigiman@gmail.com>
2023-03-15 22:17:14 +0000

    Update DataPacketPreReceiveEvent.php

--- src/event/server/DataPacketPreReceiveEvent.php
+++ src/event/server/DataPacketPreReceiveEvent.php
@@ -28,8 +28,8 @@ use pocketmine\event\CancellableTrait;
 use pocketmine\network\mcpe\NetworkSession;
 
 /**
- * Called right before a Packet is handled by a NetworkSession {@see NetworkSession::handleDataPacket()}
- * Canceling this event will prevent the packet from being handled.
+ * Called before a packet is decoded and handled by the network session.
+ * Cancelling this packet will drop the packet without decoding it, minimizing wasted CPU time.
  */
 class DataPacketPreReceiveEvent extends ServerEvent implements Cancellable{
 	use CancellableTrait;
