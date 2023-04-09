4133794ebf5a2525473505888a20dd059025025e
HugoLeBoss91 <55756021+HugoLeBoss91@users.noreply.github.com>
2023-04-09 19:13:32 +0200

    Update BlockGrowEvent.php

--- src/event/block/BlockGrowEvent.php
+++ src/event/block/BlockGrowEvent.php
@@ -29,21 +29,23 @@ use pocketmine\player\Player;
 /**
  * Called when plants or crops grow.
  */
-class BlockGrowEvent extends BaseBlockChangeEvent{
+class BlockGrowEvent extends BaseBlockChangeEvent
+{
 
 	public function __construct(
 		Block $block,
 		Block $newState,
 		private ?Player $player = null
-	){
-		parent::__construct($block, $newState); 
+	) {
+		parent::__construct($block, $newState);
 	}
-	
+
 	/**
 	 * It returns the player which grows the crop.
 	 * It returns null when the crop grows by itself.
 	 */
-	public function getPlayer() : ?Player{
+	public function getPlayer(): ?Player
+	{
 		return $this->player;
 	}
 }
