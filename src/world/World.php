8102616ff469a44a488bafc11cce20a6b3125372
Dylan K. Taylor <dktapps@pmmp.io>
2023-04-26 17:05:31 +0100

    Added ticking chunk count to /status
    closes #5716

--- src/world/World.php
+++ src/world/World.php
@@ -104,6 +104,7 @@ use pocketmine\world\utils\SubChunkExplorer;
 use function abs;
 use function array_filter;
 use function array_key_exists;
+use function array_keys;
 use function array_map;
 use function array_merge;
 use function array_sum;
@@ -1153,6 +1154,16 @@ class World implements ChunkManager{
 	 */
 	public function setChunkTickRadius(int $radius) : void{
 		$this->chunkTickRadius = $radius;
+	}
+
+	/**
+	 * Returns a list of chunk position hashes (as returned by World::chunkHash()) which are currently registered for
+	 * ticking.
+	 *
+	 * @return int[]
+	 */
+	public function getTickingChunks() : array{
+		return array_keys($this->tickingChunks);
 	}
 
 	/**
