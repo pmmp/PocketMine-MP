8b16e811b7f4e10a0a5687a19b578ba1531ee644
HugoLeBoss91 <55756021+HugoLeBoss91@users.noreply.github.com>
2023-04-29 18:23:45 +0200

    Fix style

--- src/event/block/BlockGrowEvent.php
+++ src/event/block/BlockGrowEvent.php
@@ -1,17 +1,22 @@
 <?php
 
 /*
+ *
  *  ____            _        _   __  __ _                  __  __ ____
  * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
  * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
  * @author PocketMine Team
  * @link http://www.pocketmine.net/
+ *
+ *
  */
 
 declare(strict_types=1);
@@ -24,8 +29,7 @@ use pocketmine\player\Player;
 /**
  * Called when plants or crops grow.
  */
-class BlockGrowEvent extends BaseBlockChangeEvent
-{
+class BlockGrowEvent extends BaseBlockChangeEvent {
 
 	public function __construct(
 		Block $block,
@@ -39,8 +43,7 @@ class BlockGrowEvent extends BaseBlockChangeEvent
 	 * It returns the player which grows the crop.
 	 * It returns null when the crop grows by itself.
 	 */
-	public function getPlayer() : ?Player
-	{
+	public function getPlayer() : ?Player{
 		return $this->player;
 	}
 }
