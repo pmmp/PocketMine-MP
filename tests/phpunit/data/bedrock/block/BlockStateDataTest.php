73ee94b62cba997e4738d08372c9b6ee37751c34
Dylan K. Taylor <dktapps@pmmp.io>
2023-06-07 21:58:21 +0100

    Fixed BlockStateData::CURRENT_VERSION

---
+++ tests/phpunit/data/bedrock/block/BlockStateDataTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+ */
+
+declare(strict_types=1);
+
+namespace phpunit\data\bedrock\block;
+
+use PHPUnit\Framework\TestCase;
+use pocketmine\data\bedrock\block\BlockStateData;
+use pocketmine\data\bedrock\block\upgrade\BlockStateUpgradeSchemaUtils;
+use Symfony\Component\Filesystem\Path;
+use const PHP_INT_MAX;
+use const pocketmine\BEDROCK_BLOCK_UPGRADE_SCHEMA_PATH;
+
+final class BlockStateDataTest extends TestCase{
+
+	public function testCurrentVersion() : void{
+		foreach(BlockStateUpgradeSchemaUtils::loadSchemas(
+			Path::join(BEDROCK_BLOCK_UPGRADE_SCHEMA_PATH, 'nbt_upgrade_schema'),
+				PHP_INT_MAX
+		) as $schema){
+			$expected = BlockStateData::CURRENT_VERSION;
+			$actual = $schema->getVersionId();
+			self::assertLessThanOrEqual($expected, $actual, "Schema version $actual is newer than the current version $expected");
+		}
+	}
+}
