57f7cd269b1e503692cd2c953979bfa9953ff4a2
Joseph Williamson <j.williamson@gambler-labs.com>
2023-07-21 02:30:00 +0000

    cleanup

--- src/block/Hopper.php
+++ src/block/Hopper.php
@@ -34,7 +34,6 @@ use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\world\BlockTransaction;
-use pocketmine\Server;
 
 class Hopper extends Transparent implements HopperInteractable{
 	use PoweredByRedstoneTrait;
