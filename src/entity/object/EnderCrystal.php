e36452b017ce2e511e9d56790c45d7d63b139595
ipad54 <mcpe_server@mail.ru>
2023-07-25 16:51:23 +0300

    Added missing beam position & fixed non-vanilla exlosion

--- src/entity/object/EnderCrystal.php
+++ src/entity/object/EnderCrystal.php
@@ -28,19 +28,26 @@ use pocketmine\entity\EntitySizeInfo;
 use pocketmine\entity\Explosive;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityPreExplodeEvent;
+use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\network\mcpe\protocol\types\BlockPosition;
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataCollection;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataFlags;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
 use pocketmine\world\Explosion;
-use pocketmine\world\Position;
 
 class EnderCrystal extends Entity implements Explosive{
 	private const TAG_SHOWBASE = "ShowBottom"; //TAG_Byte
 
+	private const TAG_BLOCKTARGET_X = "BlockTargetX"; //TAG_Int
+	private const TAG_BLOCKTARGET_Y = "BlockTargetY"; //TAG_Int
+	private const TAG_BLOCKTARGET_Z = "BlockTargetZ"; //TAG_Int
+
 	public static function getNetworkTypeId() : string{ return EntityIds::ENDER_CRYSTAL; }
 
 	protected bool $showBase = false;
+	protected ?Vector3 $beamPosition = null;
 
 	protected function getInitialSizeInfo() : EntitySizeInfo{ return new EntitySizeInfo(2.0, 2.0); }
 
@@ -58,6 +65,15 @@ class EnderCrystal extends Entity implements Explosive{
 
 	public function setShowBase(bool $showBase) : void{
 		$this->showBase = $showBase;
+		$this->networkPropertiesDirty = true;
+	}
+
+	public function getBeamPosition() : ?Vector3{
+		return $this->beamPosition;
+	}
+
+	public function setBeamPosition(?Vector3 $beamPosition) : void{
+		$this->beamPosition = $beamPosition;
 		$this->networkPropertiesDirty = true;
 	}
 
@@ -80,12 +96,25 @@ class EnderCrystal extends Entity implements Explosive{
 		$this->setHealth(1);
 
 		$this->setShowBase($nbt->getByte(self::TAG_SHOWBASE, 0) === 1);
+
+		$beamX = $nbt->getInt(self::TAG_BLOCKTARGET_X, 0);
+		$beamY = $nbt->getInt(self::TAG_BLOCKTARGET_Y, 0);
+		$beamZ = $nbt->getInt(self::TAG_BLOCKTARGET_Z, 0);
+
+		if($beamX !== 0 || $beamY !== 0 || $beamZ !== 0){
+			$this->setBeamPosition(new Vector3($beamX, $beamY, $beamZ));
+		}
 	}
 
 	public function saveNBT() : CompoundTag{
 		$nbt = parent::saveNBT();
 
 		$nbt->setByte(self::TAG_SHOWBASE, $this->showBase ? 1 : 0);
+		if($this->beamPosition !== null){
+			$nbt->setInt(self::TAG_BLOCKTARGET_X, $this->beamPosition->getX());
+			$nbt->setInt(self::TAG_BLOCKTARGET_Y, $this->beamPosition->getY());
+			$nbt->setInt(self::TAG_BLOCKTARGET_Z, $this->beamPosition->getZ());
+		}
 		return $nbt;
 	}
 
@@ -93,7 +122,7 @@ class EnderCrystal extends Entity implements Explosive{
 		$ev = new EntityPreExplodeEvent($this, 6);
 		$ev->call();
 		if(!$ev->isCancelled()){
-			$explosion = new Explosion(Position::fromObject($this->location->add(0, $this->size->getHeight() / 2, 0), $this->getWorld()), $ev->getRadius(), $this);
+			$explosion = new Explosion($this->getPosition(), $ev->getRadius(), $this);
 			if($ev->isBlockBreaking()){
 				$explosion->explodeA();
 			}
@@ -105,5 +134,8 @@ class EnderCrystal extends Entity implements Explosive{
 		parent::syncNetworkData($properties);
 
 		$properties->setGenericFlag(EntityMetadataFlags::SHOWBASE, $this->showBase);
+		if($this->beamPosition !== null){
+			$properties->setBlockPos(EntityMetadataProperties::BLOCK_TARGET, BlockPosition::fromVector3($this->beamPosition));
+		}
 	}
 }
