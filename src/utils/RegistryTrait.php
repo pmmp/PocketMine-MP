581f97d1706559e60f1039a89c4049b7ea76dc3f
Artem Vasyagin <58974140+Lunarelly@users.noreply.github.com>
2023-07-26 16:26:41 +0300

    define RegistryTrait::__callStatic() var types

--- src/utils/RegistryTrait.php
+++ src/utils/RegistryTrait.php
@@ -104,7 +104,7 @@ trait RegistryTrait{
 	 *
 	 * @return object
 	 */
-	public static function __callStatic($name, $arguments){
+	public static function __callStatic(string $name, array $arguments){
 		if(count($arguments) > 0){
 			throw new \ArgumentCountError("Expected exactly 0 arguments, " . count($arguments) . " passed");
 		}
