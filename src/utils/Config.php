67658990d41a9f1226c20b1954bbda6cc625f110
Mirvin01 <48881324+Mirvin01@users.noreply.github.com>
2023-08-12 00:45:02 +0300

    Update Config.php
    corrected

--- src/utils/Config.php
+++ src/utils/Config.php
@@ -54,6 +54,7 @@ use const CASE_LOWER;
 use const JSON_BIGINT_AS_STRING;
 use const JSON_PRETTY_PRINT;
 use const JSON_THROW_ON_ERROR;
+use const JSON_UNESCAPED_UNICODE;
 use const YAML_UTF8_ENCODING;
 
 /**
