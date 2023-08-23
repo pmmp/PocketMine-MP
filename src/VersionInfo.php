c8100480ac494562fff12bbe905d73be109e1f12
Dylan K. Taylor <dktapps@pmmp.io>
2023-08-23 17:09:34 +0100

    Release 5.5.0-BETA1

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,9 +31,9 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "PocketMine-MP";
-	public const BASE_VERSION = "5.4.3";
-	public const IS_DEVELOPMENT_BUILD = true;
-	public const BUILD_CHANNEL = "stable";
+	public const BASE_VERSION = "5.5.0-BETA1";
+	public const IS_DEVELOPMENT_BUILD = false;
+	public const BUILD_CHANNEL = "beta";
 
 	/**
 	 * PocketMine-MP-specific version ID for world data. Used to determine what fixes need to be applied to old world
