2912e7ca2962495aaf7e0d2e54fa13b572186055
Dylan K. Taylor <dktapps@pmmp.io>
2023-08-25 13:27:07 +0100

    ...

--- build/generate-pocketmine-yml-property-consts.php
+++ build/generate-pocketmine-yml-property-consts.php
@@ -62,9 +62,34 @@ if($file === false){
 	fwrite(STDERR, "Failed to open output file\n");
 	exit(1);
 }
-fwrite($file, "<?php\n\n");
+fwrite($file, "<?php\n");
+fwrite($file, <<<'HEADER'
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+ */
+
+
+HEADER
+);
 fwrite($file, "declare(strict_types=1);\n\n");
 fwrite($file, "namespace pocketmine;\n\n");
+
 fwrite($file, "final class YmlServerProperties{\n");
 foreach(Utils::stringifyKeys($constants) as $constName => $propertyName){
 	fwrite($file, "\tpublic const $constName = '$propertyName';\n");
