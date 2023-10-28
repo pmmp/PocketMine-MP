15008b2bdc430457657576b2b5caa516de1b1087
pandaa-be <135014092+pandaa-be@users.noreply.github.com>
2023-10-28 11:27:37 +0200

    little changes
    just the last dev update, but still not tested - would be nice if you review

--- src/item/Elytra.php
+++ src/item/Elytra.php
@@ -26,60 +26,126 @@ namespace pocketmine\item;
 use pocketmine\entity\Living;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\math\Vector2;
+use pocketmine\math\Vector3;
 
 class Elytra extends Armor{
 
-	private const APPLY_DAMAGE_INTERVAL = 20;
-	private int $applyDamageTime = self::APPLY_DAMAGE_INTERVAL;
+	private const DAMAGE_INTERVAL = 20;
+	private const MAX_DAMAGE = 20;
+
+	private int $damageTimer = self::DAMAGE_INTERVAL;
+
+	private Vector3|null $previousPosition = null;
 
 	public function onTickWorn(Living $entity) : bool {
-		if ($entity->isGliding()) {
 
-			$direction = $entity->getDirectionVector();
-			$yVel = $direction->y;
-
-			if($yVel < 0.5){
-				$entity->resetFallDistance();
-			}
+		if($entity->isGliding()){
 
 			//Apply damage to elytra.
-			$this->applyDamageTime--;
-			if($this->applyDamageTime <= 0){
+			$this->damageTimer--;
+			if($this->damageTimer <= 0){
 				$this->applyDamage(1);
-				$this->applyDamageTime = self::APPLY_DAMAGE_INTERVAL;
+				$this->damageTimer = self::DAMAGE_INTERVAL;
 			}
 
-			// If the player is on the ground, reset the fall distance and disable gliding.
-			if ($entity->isOnGround()) {
-				$entity->resetFallDistance();
+			if($entity->isOnGround()){
 				$entity->setGliding(false);
-				return true;
 			}
 
-			//Calculate kinetic energy.
-			$xzVector = new Vector2($direction->x, $direction->z);
-			$horizontalSpeed = $xzVector->length();
+			$currentPosition = $entity->getPosition()->asVector3();
 
-			$eyePos = $entity->getEyePos();
-			$viewVector = $direction->normalize();
-
-			$block = $entity->getWorld()->getBlock($eyePos->addVector($viewVector->multiply($horizontalSpeed)));
-
-			if($block->isSolid()){
-				$mass = $entity->getHealth() * 10;
-				$kineticEnergy = 0.5 * $mass * $horizontalSpeed ** 2;
-
-				$ev = new EntityDamageEvent($entity, EntityDamageEvent::CAUSE_CONTACT, $kineticEnergy);
-				$entity->attack($ev);
+			if($this->previousPosition === null){
+				// First tick, set previous position
+				$this->previousPosition = $currentPosition;
 			}
+
+			// Calculate velocity vector between previous and current position
+			$velocity = $currentPosition->subtractVector($this->previousPosition);
+
+			$yVelocity = $velocity->y;
+			$xzVelocity = $this->getHorizontalVelocity($velocity);
+
+			if($yVelocity < 0.5){
+				// Reset fall distance if vertical velocity is low
+				$entity->resetFallDistance();
+			}
+
+			$speed = $xzVelocity->length(); // Horizontal speed
+
+			if($this->shouldCheckCollision($entity, $speed)){
+
+				// Calculate kinetic energy and damage
+				$kineticEnergy = $this->calculateKineticEnergy($speed);
+				$damage = $this->calculateDamage($kineticEnergy);
+
+				if($damage > 0){
+					// Deal damage if kinetic energy is above threshold
+					$ev = new EntityDamageEvent($entity, EntityDamageEvent::CAUSE_CUSTOM, $damage);
+					$entity->attack($ev);
+				}
+
+			}
+
+			// Update previous position for next tick
+			$this->previousPosition = $currentPosition;
 
 			return true;
 		}
-
 		return false;
 	}
 
-	protected function onBroken() : void{
+	private function getHorizontalVelocity(Vector3 $velocity) : Vector2 {
+		return new Vector2($velocity->x, $velocity->z);
+	}
+
+	private function shouldCheckCollision(Living $entity, float $speed) : bool {
+
+		$eyePosition = $entity->getEyePos();
+		$direction = $entity->getDirectionVector()->normalize();
+
+		// Trace entity's eye position + direction * speed
+		$traceEnd = $eyePosition->addVector($direction->multiply($speed));
+
+		$block = $entity->getWorld()->getBlock($traceEnd);
+
+		// Check if block is solid
+		if(!$block->isSolid()){
+			return false;
+		}
+
+		// Check if distance is within 1-2 blocks
+		$traceDistance = $traceEnd->distance($eyePosition);
+		if($traceDistance > 2){
+			return false;
+		}
+
+		return true; // Collision ahead
+	}
+
+	private function calculateKineticEnergy(float $speed) : float {
+		//10 = mass
+		return 0.5 * 10 * $speed ** 2;
+	}
+
+	private function calculateDamage(float $kineticEnergy) : int {
+
+		$damage = 0;
+
+		if($kineticEnergy > 100) {
+			return 50; // Instant kill
+		}
+
+		if($kineticEnergy < 5) {
+			return $damage; // No damage below threshold
+		}
+
+		$damage = (int)($kineticEnergy - 5); // Subtract threshold
+
+		return min($damage, self::MAX_DAMAGE); // Cap damage
+
+	}
+
+	protected function onBroken() : void {
 		//NOOP
 	}
 }
