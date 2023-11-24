4749262e4ac153e7e56a2814d1587c4d20c809cd
ace <diamond-gold-ace@outlook.com>
2023-11-24 18:19:17 +0800

    Make PitcherPod final

--- src/item/PitcherPod.php
+++ src/item/PitcherPod.php
@@ -26,7 +26,7 @@ namespace pocketmine\item;
 use pocketmine\block\Block;
 use pocketmine\block\VanillaBlocks;
 
-class PitcherPod extends Item{
+final class PitcherPod extends Item{
 
 	public function getBlock(?int $clickedFace = null) : Block{
 		return VanillaBlocks::PITCHER_CROP();
