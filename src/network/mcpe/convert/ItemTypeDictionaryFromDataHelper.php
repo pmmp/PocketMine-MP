5c449d8bc925cf7df1e39c96f823fc36a9ea384a
Adam <116978956+b1zeyofficial@users.noreply.github.com>
2023-12-08 01:56:52 +0600

    Update ItemTypeDictionaryFromDataHelper.php

--- src/network/mcpe/convert/ItemTypeDictionaryFromDataHelper.php
+++ src/network/mcpe/convert/ItemTypeDictionaryFromDataHelper.php
@@ -23,16 +23,31 @@ declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\convert;
 
+use pocketmine\data\bedrock\BedrockDataFiles;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\network\mcpe\protocol\serializer\ItemTypeDictionary;
 use pocketmine\network\mcpe\protocol\types\ItemTypeEntry;
 use pocketmine\utils\AssumptionFailedError;
+use pocketmine\utils\Filesystem;
 use function is_array;
 use function is_bool;
 use function is_int;
 use function is_string;
 use function json_decode;
+use function str_replace;
 
 final class ItemTypeDictionaryFromDataHelper{
+
+	private const PATHS = [
+		ProtocolInfo::CURRENT_PROTOCOL => "",
+		ProtocolInfo::PROTOCOL_1_20_30 => "",
+		ProtocolInfo::PROTOCOL_1_20_10 => "-1.20.10",
+		ProtocolInfo::PROTOCOL_1_20_0 => "-1.20.0",
+	];
+
+	public static function loadFromProtocolId(int $protocolId) : ItemTypeDictionary{
+		return self::loadFromString(Filesystem::fileGetContents(str_replace(".json", self::PATHS[$protocolId] . ".json", BedrockDataFiles::REQUIRED_ITEM_LIST_JSON)));
+	}
 
 	public static function loadFromString(string $data) : ItemTypeDictionary{
 		$table = json_decode($data, true);
