a9dcebd32fba6d01c2d869355ed7b224acbcb668
Dylan K. Taylor <dktapps@pmmp.io>
2024-02-05 13:07:31 +0000

    moar test cases

--- tests/phpunit/promise/PromiseTest.php
+++ tests/phpunit/promise/PromiseTest.php
@@ -61,6 +61,7 @@ class PromiseTest extends TestCase{
 				self::fail("Promise was rejected");
 			}
 		);
+		self::assertFalse($done);
 		$resolver->resolve(1);
 		self::assertTrue($done);
 	}
@@ -79,5 +80,73 @@ class PromiseTest extends TestCase{
 			}
 		);
 		self::assertTrue($done);
+	}
+
+	public function testAllResolve() : void{
+		$resolver1 = new PromiseResolver();
+		$resolver2 = new PromiseResolver();
+
+		$allPromise = Promise::all([$resolver1->getPromise(), $resolver2->getPromise()]);
+		$done = false;
+		$allPromise->onCompletion(
+			function($value) use (&$done) : void{
+				$done = true;
+				self::assertEquals([1, 2], $value);
+			},
+			function() use (&$done) : void{
+				$done = true;
+				self::fail("Promise was rejected");
+			}
+		);
+		self::assertFalse($done);
+		$resolver1->resolve(1);
+		self::assertFalse($done);
+		$resolver2->resolve(2);
+		self::assertTrue($done);
+	}
+
+	public function testAllPartialReject() : void{
+		$resolver1 = new PromiseResolver();
+		$resolver2 = new PromiseResolver();
+
+		$allPromise = Promise::all([$resolver1->getPromise(), $resolver2->getPromise()]);
+		$done = false;
+		$allPromise->onCompletion(
+			function($value) use (&$done) : void{
+				$done = true;
+				self::fail("Promise was unexpectedly resolved");
+			},
+			function() use (&$done) : void{
+				$done = true;
+			}
+		);
+		self::assertFalse($done);
+		$resolver2->reject();
+		self::assertTrue($done, "All promise should be rejected immediately after the first constituent rejection");
+		$resolver1->resolve(1);
+	}
+
+	/**
+	 * Promise::all() should return a rejected promise if any of the input promises were rejected at the call time
+	 */
+	public function testAllPartialPreReject() : void{
+		$resolver1 = new PromiseResolver();
+		$resolver2 = new PromiseResolver();
+		$resolver2->reject();
+
+		$allPromise = Promise::all([$resolver1->getPromise(), $resolver2->getPromise()]);
+		self::assertTrue($allPromise->isResolved());
+		$done = false;
+		$allPromise->onCompletion(
+			function($value) use (&$done) : void{
+				$done = true;
+				self::fail("Promise was unexpectedly resolved");
+			},
+			function() use (&$done) : void{
+				$done = true;
+			}
+		);
+		self::assertTrue($done, "All promise should be rejected immediately after the first constituent rejection");
+		$resolver1->resolve(1);
 	}
 }
