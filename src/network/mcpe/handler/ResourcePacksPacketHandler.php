cee89f7aa464c1a2857e8f23930556e2ab360b50
jasonw4331 <jasonwynn10@me.com>
2024-02-12 10:53:55 -0500

    Move assert

--- src/network/mcpe/handler/ResourcePacksPacketHandler.php
+++ src/network/mcpe/handler/ResourcePacksPacketHandler.php
@@ -64,10 +64,11 @@ class ResourcePacksPacketHandler extends PacketHandler{
 		private NetworkSession $session,
 		private ResourcePackManager $resourcePackManager,
 		private \Closure $completionCallback
-	){}
+	){
+		assert($this->session->getPlayerInfo() !== null, "PlayerInfo should be set before ResourcePacksPacketHandler is used");
+	}
 
 	public function setUp() : void{
-		assert($this->session->getPlayerInfo() !== null, "PlayerInfo should be set before ResourcePacksPacketHandler is used");
 		$event = new PlayerResourcePackOfferEvent($this->session->getPlayerInfo(), $this->resourcePackManager->getResourceStack(), $this->resourcePackManager->resourcePacksRequired());
 		$event->call();
 		$resourcePackEntries = array_map(function(ResourcePack $pack) : ResourcePackInfoEntry{
