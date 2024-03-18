0ec38cda099b9298eb3bcfa3c6c2a1eac295c05e
Dylan K. Taylor <dktapps@pmmp.io>
2024-03-18 16:20:43 +0000

    Phar cache is now cleaned up by new server instances on startup this ensures that dead caches are properly removed, regardless of what caused the termination of the previous process.

--- build/server-phar-stub.php
+++ build/server-phar-stub.php
@@ -23,30 +23,93 @@ declare(strict_types=1);
 
 namespace pocketmine\server_phar_stub;
 
+use function clearstatcache;
 use function copy;
+use function fclose;
+use function fflush;
+use function flock;
+use function fopen;
+use function fwrite;
+use function getmypid;
 use function hrtime;
+use function is_dir;
 use function is_file;
+use function mkdir;
 use function number_format;
-use function register_shutdown_function;
 use function str_replace;
+use function stream_get_contents;
 use function sys_get_temp_dir;
 use function tempnam;
 use function unlink;
 use const DIRECTORY_SEPARATOR;
+use const LOCK_EX;
+use const LOCK_NB;
+use const LOCK_UN;
 
 /**
- * Prepares a decompressed .tar of PocketMine-MP.phar in the system temp directory for loading code from.
- *
- * @return string[] path to the temporary decompressed phar (actually a .tar) + path to tmpfile used to reserve the name
+ * Finds the appropriate tmp directory to store the decompressed phar cache, accounting for potential file name
+ * collisions.
  */
-function preparePharCache() : array{
-	$tmp = tempnam(sys_get_temp_dir(), "PMMP");
-	if($tmp === false){
-		throw new \RuntimeException("Failed to create temporary file");
+function preparePharCacheDirectory() : string{
+	clearstatcache();
+
+	$i = 0;
+	do{
+		$tmpPath = sys_get_temp_dir() . '/PocketMine-MP-phar-cache.' . $i;
+		$i++;
+	}while(is_file($tmpPath));
+	if(!@mkdir($tmpPath) && !is_dir($tmpPath)){
+		throw new \RuntimeException("Failed to create temporary directory $tmpPath. Please ensure the disk has enough space and that the current user has permission to write to this location.");
 	}
 
-	$tmpPharPath = $tmp . ".phar";
-	copy(__FILE__, $tmpPharPath);
+	return $tmpPath;
+}
+
+/**
+ * Deletes caches left behind by previous server instances.
+ * This ensures that the tmp directory doesn't get flooded by servers crashing in restart loops.
+ */
+function cleanupPharCache(string $tmpPath) : void{
+	clearstatcache();
+
+	/** @var string[] $matches */
+	foreach(new \RegexIterator(
+		new \FilesystemIterator(
+			$tmpPath,
+			\FilesystemIterator::CURRENT_AS_PATHNAME | \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::UNIX_PATHS
+		),
+		'/(.+)\.lock$/',
+		\RegexIterator::GET_MATCH
+	) as $matches){
+		$lockFilePath = $matches[0];
+		$baseTmpPath = $matches[1];
+
+		$file = fopen($lockFilePath, "rb");
+		if($file === false){
+			continue;
+		}
+
+		if(flock($file, LOCK_EX | LOCK_NB)){
+			//this tmpfile is no longer in use
+			flock($file, LOCK_UN);
+			fclose($file);
+
+			unlink($lockFilePath);
+			unlink($baseTmpPath . ".tar");
+			unlink($baseTmpPath);
+			echo "Deleted stale phar cache at $baseTmpPath\n";
+		}else{
+			$pid = stream_get_contents($file);
+			fclose($file);
+
+			echo "Phar cache at $baseTmpPath is still in use by PID $pid\n";
+		}
+	}
+}
+
+function convertPharToTar(string $tmpName, string $pharPath) : string{
+	$tmpPharPath = $tmpName . ".phar";
+	copy($pharPath, $tmpPharPath);
 
 	$phar = new \Phar($tmpPharPath);
 	//phar requires phar.readonly=0, and zip doesn't support disabling compression - tar is the only viable option
@@ -55,18 +118,50 @@ function preparePharCache() : array{
 	unset($phar);
 	\Phar::unlinkArchive($tmpPharPath);
 
-	return [$tmp . '.tar', $tmp];
+	return $tmpName . ".tar";
 }
 
+/**
+ * Locks a phar tmp cache to prevent it from being deleted by other server instances.
+ * This code looks similar to Filesystem::createLockFile(), but we can't use that because it's inside the compressed
+ * phar.
+ */
+function lockPharCache(string $lockFilePath) : void{
+	//this static variable will keep the file(s) locked until the process ends
+	static $lockFiles = [];
+
+	$lockFile = fopen($lockFilePath, "wb");
+	if($lockFile === false){
+		throw new \RuntimeException("Failed to open temporary file");
+	}
+	flock($lockFile, LOCK_EX); //this tells other server instances not to delete this cache file
+	fwrite($lockFile, (string) getmypid()); //maybe useful for debugging
+	fflush($lockFile);
+	$lockFiles[$lockFilePath] = $lockFile;
+}
+
+/**
+ * Prepares a decompressed .tar of PocketMine-MP.phar in the system temp directory for loading code from.
+ *
+ * @return string path to the temporary decompressed phar (actually a .tar)
+ */
+function preparePharCache(string $tmpPath, string $pharPath) : string{
+	clearstatcache();
+
+	$tmpName = tempnam($tmpPath, "PMMP");
+	if($tmpName === false){
+		throw new \RuntimeException("Failed to create temporary file");
+	}
+
+	lockPharCache($tmpName . ".lock");
+	return convertPharToTar($tmpName, $pharPath);
+}
+
+$tmpDir = preparePharCacheDirectory();
+cleanupPharCache($tmpDir);
 echo "Preparing PocketMine-MP.phar decompressed cache...\n";
 $start = hrtime(true);
-[$cacheName, $tmpReserveName] = preparePharCache();
+$cacheName = preparePharCache($tmpDir, __FILE__);
 echo "Cache ready at $cacheName in " . number_format((hrtime(true) - $start) / 1e9, 2) . "s\n";
-register_shutdown_function(static function() use ($cacheName, $tmpReserveName) : void{
-	if(is_file($cacheName)){
-		\Phar::unlinkArchive($cacheName);
-		unlink($tmpReserveName);
-	}
-});
 
 require 'phar://' . str_replace(DIRECTORY_SEPARATOR, '/', $cacheName) . '/src/PocketMine.php';
