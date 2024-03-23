7fc71f56c9f696df31977a2879aeaab56b9b5cbd
codeNinja223 <164689564+codeNinja223@users.noreply.github.com>
2024-03-24 01:23:46 +0300

    Delete .idea directory

--- .idea/fileTemplates/code/PHP Constructor.php
+++
@@ -1,8 +0,0 @@
-#if(${THROWS_DOC} != "")
-/**
-${THROWS_DOC}
-*/
-#end
-public function __construct(${PARAM_LIST}) {
-${BODY}
-}
--- .idea/fileTemplates/code/PHP Fluent Setter Method.php
+++
@@ -1,6 +0,0 @@
-/** @return $this */
-public function set${NAME}(#if (${SCALAR_TYPE_HINT})${SCALAR_TYPE_HINT} #else#end$${PARAM_NAME})#if(${RETURN_TYPE}): self#else#end
-{
-    $this->${FIELD_NAME} = $${PARAM_NAME};
-    return $this;
-}
