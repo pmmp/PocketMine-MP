pmmp/PocketMine-MP

commit 09904dc5196ed2753336dcd79d15e3e7dac41e8c

workaround for callback-validator not understanding arrow functions

src/entity/Living.php
@@ -158,7 +158,9 @@ abstract class Living extends Entity{
 			}
 		};
 		$this->armorInventory->getListeners()->add(new CallbackInventoryListener(
-			fn(Inventory $inventory, int $slot, Item $oldItem) => $playArmorSound($inventory->getItem($slot), $oldItem),
+			function(Inventory $inventory, int $slot, Item $oldItem) : void{
+				$playArmorSound($inventory->getItem($slot), $oldItem);
+			},
 			function(Inventory $inventory, array $oldContents) use ($playArmorSound) : void{
 				foreach($oldContents as $slot => $oldItem){
 					$playArmorSound($inventory->getItem($slot), $oldItem);

commit f691a44713ac27bdd92bcd4d3525a8a1b81fb976

Fix syntax error introduced in 09904dc5196ed2753336dcd79d15e3e7dac41e8c

src/entity/Living.php
@@ -158,7 +158,7 @@ abstract class Living extends Entity{
 			}
 		};
 		$this->armorInventory->getListeners()->add(new CallbackInventoryListener(
-			function(Inventory $inventory, int $slot, Item $oldItem) : void{
+			function(Inventory $inventory, int $slot, Item $oldItem) use ($playArmorSound) : void{
 				$playArmorSound($inventory->getItem($slot), $oldItem);
 			},
 			function(Inventory $inventory, array $oldContents) use ($playArmorSound) : void{