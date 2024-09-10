cc34adab54de058dacc8a433a073d5dfe966b2c7
dadodasyra <44753923+dadodasyra@users.noreply.github.com>
2024-09-10 16:19:19 +0000

    EnderPearls now teleport to the correct world

--- src/entity/projectile/EnderPearl.php
+++ src/entity/projectile/EnderPearl.php
@@ -28,6 +28,7 @@ use pocketmine\event\entity\ProjectileHitEvent;
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 use pocketmine\world\particle\EndermanTeleportParticle;
 use pocketmine\world\sound\EndermanTeleportSound;
+use pocketmine\world\Position;
 
 class EnderPearl extends Throwable{
 	public static function getNetworkTypeId() : string{ return EntityIds::ENDER_PEARL; }
@@ -38,9 +39,10 @@ class EnderPearl extends Throwable{
 			//TODO: check end gateways (when they are added)
 			//TODO: spawn endermites at origin
 
+			$target = $event->getRayTraceResult()->getHitVector();
 			$this->getWorld()->addParticle($origin = $owner->getPosition(), new EndermanTeleportParticle());
 			$this->getWorld()->addSound($origin, new EndermanTeleportSound());
-			$owner->teleport($target = $event->getRayTraceResult()->getHitVector());
+			$owner->teleport(Position::fromObject($target, $this->getWorld()));
 			$this->getWorld()->addSound($target, new EndermanTeleportSound());
 
 			$owner->attack(new EntityDamageEvent($owner, EntityDamageEvent::CAUSE_FALL, 5));
