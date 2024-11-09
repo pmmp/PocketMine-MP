787b90d23f76f3dbbb7a25906f4139225ec40694
AzaleeX <azaleemcpe@gmail.com>
2024-11-09 16:14:48 +0100

    ouppss

--- src/block/NetherRoots.php
+++ src/block/NetherRoots.php
@@ -35,7 +35,7 @@ final class NetherRoots extends Flowable{
 		return
 			$supportBlock->hasTypeTag(BlockTypeTags::DIRT) ||
 			$supportBlock->hasTypeTag(BlockTypeTags::MUD) ||
-			$supportBlock->getTypeId() === BlockTypeTags::NYLIUM ||
+			$supportBlock->hasTypeTag(BlockTypeTags::NYLIUM) ||
 			$supportBlock->getTypeId() === BlockTypeIds::SOUL_SOIL;
 	}
 }
