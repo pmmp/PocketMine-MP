ff6fb7fc0a71bc4f3d0bcc2d720906b741815fed
AzaleeX <azaleemcpe@gmail.com>
2024-11-17 11:02:37 +0100

    fix1:  phpstan

--- src/world/generator/object/NetherGrass.php
+++ src/world/generator/object/NetherGrass.php
@@ -32,7 +32,7 @@ use function count;
 
 class NetherGrass{
 	public static function growGrass(ChunkManager $world, Vector3 $pos, Random $random, int $count = 8, int $radius = 5) : void{
-		$nyliumType = $world->getBlockAt($pos->x, $pos->y, $pos->z)->getTypeId();
+		$nyliumType = $world->getBlockAt((int) $pos->x, (int) $pos->y, (int) $pos->z)->getTypeId();
 		$blocksToGrow = [];
 
 		//Todo: Add more blocks to grow on nether nylium
@@ -56,17 +56,19 @@ class NetherGrass{
 		$blocksCount = count($blocksToGrow) - 1;
 		if ($blocksCount >= 0) {
 			for ($c = 0; $c < $count; ++$c) {
-				$x = $random->nextRange($pos->x - $radius, $pos->x + $radius);
-				$z = $random->nextRange($pos->z - $radius, $pos->z + $radius);
-				$blockBelow = $world->getBlockAt($x, $pos->y, $z);
-				if ($world->getBlockAt($x, $pos->y + 1, $z)->getTypeId() === BlockTypeIds::AIR && $blockBelow->getTypeId() !== BlockTypeIds::AIR) {
-					$blockToGrow = $blocksToGrow[$random->nextRange(0, $blocksCount)];
-					if ($blockToGrow === VanillaBlocks::CRIMSON_ROOTS()) {
-						if ($random->nextFloat() < 0.1) { // 10% chance for rare blocks
-							$world->setBlockAt($x, $pos->y + 1, $z, $blockToGrow);
+				$x = $random->nextRange((int) ($pos->x - $radius), (int) ($pos->x + $radius));
+				$z = $random->nextRange((int) ($pos->z - $radius), (int) ($pos->z + $radius));
+				$blockBelow = $world->getBlockAt($x, (int) $pos->y, $z);
+				if ($world->getBlockAt($x, (int) ($pos->y + 1), $z)->getTypeId() === BlockTypeIds::AIR && $blockBelow->getTypeId() !== BlockTypeIds::AIR) {
+					if (isset($blocksToGrow[$random->nextRange(0, $blocksCount)])) {
+						$blockToGrow = $blocksToGrow[$random->nextRange(0, $blocksCount)];
+						if ($blockToGrow === VanillaBlocks::CRIMSON_ROOTS()) {
+							if ($random->nextFloat() < 0.1) { // 10% chance for rare blocks
+								$world->setBlockAt($x, (int) ($pos->y + 1), $z, $blockToGrow);
+							}
+						} else {
+							$world->setBlockAt($x, (int) ($pos->y + 1), $z, $blockToGrow);
 						}
-					} else {
-						$world->setBlockAt($x, $pos->y + 1, $z, $blockToGrow);
 					}
 				}
 			}
