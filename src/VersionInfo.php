1959bd26e6974abd51d145f3cf30007aaca77554
Dylan K. Taylor <dktapps@pmmp.io>
2024-12-04 13:33:05 +0000

    Prepare 5.22.0 release

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,8 +31,8 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "PocketMine-MP";
-	public const BASE_VERSION = "5.21.3";
-	public const IS_DEVELOPMENT_BUILD = true;
+	public const BASE_VERSION = "5.22.0";
+	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 
 	/**
