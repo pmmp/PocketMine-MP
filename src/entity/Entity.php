5cc99e1c4a1ac066121838acf1e7375e62d6b935
leolee3914 <15855635+leolee3914@users.noreply.github.com>
2025-02-28 16:47:07 +0800

    fixed entity spawn position

--- src/entity/Entity.php
+++ src/entity/Entity.php
@@ -1495,7 +1495,7 @@ abstract class Entity{
 			$this->getId(), //TODO: actor unique ID
 			$this->getId(),
 			static::getNetworkTypeId(),
-			$this->location->asVector3(),
+			$this->getOffsetPosition($this->location->asVector3()),
 			$this->getMotion(),
 			$this->location->pitch,
 			$this->location->yaw,
