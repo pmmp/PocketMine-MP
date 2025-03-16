ea831cd5944c813ad748f6116a09bfff94d2a901
Adam <116978956+b1zeyofficial@users.noreply.github.com>
2025-03-16 15:26:48 +0600

    my bad

--- src/event/block/BlockPreExplodeEvent.php
+++ src/event/block/BlockPreExplodeEvent.php
@@ -27,7 +27,7 @@ use pocketmine\block\Block;
 use pocketmine\event\Cancellable;
 use pocketmine\event\CancellableTrait;
 use pocketmine\player\Player;
-use function is_finite;СЫ
+use function is_finite;
 
 /**
  * Event triggered before a block explosion, allowing modifications to the explosion radius, block destruction, and fire chances.
