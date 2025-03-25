ef5ff7ba9089af391e49618b3ec9d73dac52a2ee
Adam <116978956+b1zeyofficial@users.noreply.github.com>
2025-03-25 22:52:37 +0600

    Update Explosion.php

--- src/world/Explosion.php
+++ src/world/Explosion.php
@@ -57,6 +57,7 @@ class Explosion{
 	private int $rays = 16;
 	public World $world;
 
+	/** @var Block[] */
 	/** @phpstan-var array<int, Block> */
 	public array $affectedBlocks = [];
 	public float $stepLen = 0.3;
