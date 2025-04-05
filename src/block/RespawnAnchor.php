2eaa3a2dd11af322aab678df0a5f08370f660af4
b1zeyofficial <kingkyrgyzvoprosy@gmail.com>
2025-04-06 03:49:01 +0600

    Update RespawnAnchor.php

--- src/block/RespawnAnchor.php
+++ src/block/RespawnAnchor.php
@@ -29,7 +29,6 @@ use pocketmine\event\player\PlayerRespawnAnchorUseEvent;
 use pocketmine\item\Item;
 use pocketmine\item\ItemTypeIds;
 use pocketmine\math\Vector3;
-use pocketmine\network\mcpe\protocol\types\DimensionIds;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat;
 use pocketmine\world\Explosion;
@@ -79,10 +78,6 @@ final class RespawnAnchor extends Opaque{
 
 			$ev = new PlayerRespawnAnchorUseEvent($player, $this, PlayerRespawnAnchorUseEvent::ACTION_EXPLODE);
 
-			if($this->position->getWorld()->getId() === DimensionIds::NETHER){
-				$ev->setAction(PlayerRespawnAnchorUseEvent::ACTION_SET_SPAWN);
-			}
-
 			$ev->call();
 			if($ev->isCancelled()){
 				return false;
@@ -94,7 +89,7 @@ final class RespawnAnchor extends Opaque{
 					return false;
 
 				case PlayerRespawnAnchorUseEvent::ACTION_SET_SPAWN:
-					if($this->charges >= 4){
+					if($this->charges > self::MIN_CHARGES){
 						if($player->getSpawn() !== null && $player->getSpawn()->equals($this->position)){
 							return true;
 						}
