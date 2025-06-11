4390c0a9154aafc1fc79d7cebf7316fa6220c322
Dylan T. <dktapps@pmmp.io>
2025-06-11 20:57:50 +0100

    Update ChunkLoader.php

--- src/world/ChunkLoader.php
+++ src/world/ChunkLoader.php
@@ -32,4 +32,6 @@ namespace pocketmine\world;
  * WARNING: When moving this object around in the world or destroying it,
  * be sure to unregister the loader from chunks you're not using, otherwise you'll leak memory.
  */
-final class ChunkLoader{}
+final class ChunkLoader{
+
+}
