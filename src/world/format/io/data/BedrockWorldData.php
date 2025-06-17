e92a0c1c7a44f7dcad7f501e3cf0ca7681a80f27
TheColorDroid <jangannakalbodoh2@gmail.com>
2025-06-17 18:51:26 +0700

    ...

--- src/world/format/io/data/BedrockWorldData.php
+++ src/world/format/io/data/BedrockWorldData.php
@@ -51,11 +51,11 @@ use function time;
 class BedrockWorldData extends BaseNbtWorldData{
 
 	public const CURRENT_STORAGE_VERSION = 10;
-	public const CURRENT_STORAGE_NETWORK_VERSION = 800;
+	public const CURRENT_STORAGE_NETWORK_VERSION = 818;
 	public const CURRENT_CLIENT_VERSION_TARGET = [
 		1, //major
 		21, //minor
-		80, //patch
+		90, //patch
 		3, //revision
 		0 //is beta
 	];
