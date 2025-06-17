8f7ab3cf0605f99556590a355e307ac14ea35940
TheColorDroid <jangannakalbodoh2@gmail.com>
2025-06-17 15:12:39 +0700

    CS fix

--- src/network/mcpe/handler/PreSpawnPacketHandler.php
+++ src/network/mcpe/handler/PreSpawnPacketHandler.php
@@ -40,7 +40,6 @@ use pocketmine\network\mcpe\protocol\types\Experiments;
 use pocketmine\network\mcpe\protocol\types\LevelSettings;
 use pocketmine\network\mcpe\protocol\types\NetworkPermissions;
 use pocketmine\network\mcpe\protocol\types\PlayerMovementSettings;
-use pocketmine\network\mcpe\protocol\types\ServerAuthMovementMode;
 use pocketmine\network\mcpe\protocol\types\SpawnSettings;
 use pocketmine\player\Player;
 use pocketmine\Server;
