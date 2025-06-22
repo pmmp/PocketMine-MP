db4d4d4c4631a5e1e948fd00e4268674060e1d4d
ItzxDwi <107537435+ItzxDwi@users.noreply.github.com>
2025-06-23 00:09:05 +0800

    feat: implements a way to enable vibrant visuals

--- src/event/player/PlayerResourcePackOfferEvent.php
+++ src/event/player/PlayerResourcePackOfferEvent.php
@@ -46,7 +46,8 @@ class PlayerResourcePackOfferEvent extends Event{
 		private readonly PlayerInfo $playerInfo,
 		private array $resourcePacks,
 		private array $encryptionKeys,
-		private bool $mustAccept
+		private bool $mustAccept,
+		private bool $forceDisableVibrantVisuals = true
 	){}
 
 	public function getPlayerInfo() : PlayerInfo{
@@ -102,5 +103,19 @@ class PlayerResourcePackOfferEvent extends Event{
 
 	public function mustAccept() : bool{
 		return $this->mustAccept;
+	}
+
+	/**
+	 * Sets whether vibrant visuals should be forcibly disabled.
+	 */
+	public function setForceDisableVibrantVisuals(bool $forceDisable) : void{
+		$this->forceDisableVibrantVisuals = $forceDisable;
+	}
+
+	/**
+	 * Returns whether vibrant visuals should be forcibly disabled.
+	 */
+	public function forceDisableVibrantVisuals() : bool{
+		return $this->forceDisableVibrantVisuals;
 	}
 }
