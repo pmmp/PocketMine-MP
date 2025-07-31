e00c99b97ed46e62d8a1eed082a29f4adcaebf4f
ShockedPlot7560 <no-reply@tchallon.fr>
2025-07-31 08:36:41 +0200

    Fix style

--- src/data/bedrock/block/convert/BlockObjectToStateSerializer.php
+++ src/data/bedrock/block/convert/BlockObjectToStateSerializer.php
@@ -189,6 +189,7 @@ use pocketmine\data\bedrock\block\convert\BlockStateWriter as Writer;
 use pocketmine\math\Axis;
 use pocketmine\math\Facing;
 use function get_class;
+use function is_callable;
 
 final class BlockObjectToStateSerializer implements BlockStateSerializer{
 	/**
