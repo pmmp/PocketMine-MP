7603345cf26bea48e05e1c77992def02f9929c00
Dascia <stddaria0@ya.ru>
2025-08-22 17:32:02 +0300

    Added the getSneakOffset() for Living, sneak offset implementation for Human. Removed sneak offset accounting for eyes

--- src/entity/Human.php
+++ src/entity/Human.php
@@ -247,6 +247,10 @@ class Human extends Living implements ProjectileSource, InventoryHolder{
 		return $this->enderInventory;
 	}
 
+	public function getSneakOffset() : float{
+		return 1.49;
+	}
+
 	/**
 	 * For Human entities which are not players, sets their properties such as nametag, skin and UUID from NBT.
 	 */
