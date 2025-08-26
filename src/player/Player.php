c68e265ebeaf9051942f5884e924254599cd59cc
Zwuiix <71025924+Zwuiix-cmd@users.noreply.github.com>
2025-08-27 00:41:57 +0200

    Cast chunk coordinates to int in chunk operations

--- src/player/Player.php
+++ src/player/Player.php
@@ -885,13 +885,13 @@ class Player extends Human implements CommandSender, ChunkListener, IPlayer{
 			$this->usedChunks[$index] = UsedChunkStatus::REQUESTED_GENERATION;
 			$this->activeChunkGenerationRequests[$index] = true;
 			unset($this->loadQueue[$index]);
-			$world->registerChunkLoader($this->chunkLoader, $X, $Z, true);
-			$world->registerChunkListener($this, $X, $Z);
+			$world->registerChunkLoader($this->chunkLoader, (int)$X, (int)$Z, true);
+			$world->registerChunkListener($this, (int)$X, (int)$Z);
 			if(isset($this->tickingChunks[$index])){
-				$world->registerTickingChunk($this->chunkTicker, $X, $Z);
+				$world->registerTickingChunk($this->chunkTicker, (int)$X, (int)$Z);
 			}
 
-			$world->requestChunkPopulation($X, $Z, $this->chunkLoader)->onCompletion(
+			$world->requestChunkPopulation((int)$X, (int)$Z, $this->chunkLoader)->onCompletion(
 				function() use ($X, $Z, $index, $world) : void{
 					if(!$this->isConnected() || !isset($this->usedChunks[$index]) || $world !== $this->getWorld()){
 						return;
@@ -905,10 +905,10 @@ class Player extends Human implements CommandSender, ChunkListener, IPlayer{
 					unset($this->activeChunkGenerationRequests[$index]);
 					$this->usedChunks[$index] = UsedChunkStatus::REQUESTED_SENDING;
 
-					$this->getNetworkSession()->startUsingChunk($X, $Z, function() use ($X, $Z, $index) : void{
+					$this->getNetworkSession()->startUsingChunk((int)$X, (int)$Z, function() use ($X, $Z, $index) : void{
 						$this->usedChunks[$index] = UsedChunkStatus::SENT;
 						if($this->spawnChunkLoadCount === -1){
-							$this->spawnEntitiesOnChunk($X, $Z);
+							$this->spawnEntitiesOnChunk((int)$X, (int)$Z);
 						}elseif($this->spawnChunkLoadCount++ === $this->spawnThreshold){
 							$this->spawnChunkLoadCount = -1;
 
@@ -916,7 +916,7 @@ class Player extends Human implements CommandSender, ChunkListener, IPlayer{
 
 							$this->getNetworkSession()->notifyTerrainReady();
 						}
-						(new PlayerPostChunkSendEvent($this, $X, $Z))->call();
+						(new PlayerPostChunkSendEvent($this, (int)$X, (int)$Z))->call();
 					});
 				},
 				static function() : void{
