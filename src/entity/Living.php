b64648c7d94c5543aabbfbf3205b5b89af93f5d0
Darya Markova <122279000+Dasciam@users.noreply.github.com>
2025-09-02 11:48:25 +0300

    Update Living.php: apply sneak offset to eye height.

--- src/entity/Living.php
+++ src/entity/Living.php
@@ -296,7 +296,7 @@ abstract class Living extends Entity{
 			$width = $size->getWidth();
 			$this->setSize((new EntitySizeInfo($width, $width, $width * 0.9))->scale($this->getScale()));
 		}elseif($this->isSneaking()){
-			$this->setSize((new EntitySizeInfo($size->getHeight() - $this->getSneakOffset(), $size->getWidth(), 3 / 4 * $size->getEyeHeight()))->scale($this->getScale()));
+			$this->setSize((new EntitySizeInfo($size->getHeight() - $this->getSneakOffset(), $size->getWidth(), $size->getEyeHeight() - $this->getSneakOffset()))->scale($this->getScale()));
 		}else{
 			$this->setSize($size->scale($this->getScale()));
 		}
