a93b2beb50fb5a19a3e700ee3efaa496fee9f6b7
Masaki Aida <51500566+aida0710@users.noreply.github.com>
2025-09-23 19:37:30 +0900

    refactor(handler): remove unused Frost Walker logic

--- src/entity/Living.php
+++ src/entity/Living.php
@@ -25,8 +25,6 @@ namespace pocketmine\entity;
 
 use pocketmine\block\Block;
 use pocketmine\block\BlockTypeIds;
-use pocketmine\block\VanillaBlocks;
-use pocketmine\block\Water;
 use pocketmine\data\bedrock\EffectIdMap;
 use pocketmine\entity\animation\DeathAnimation;
 use pocketmine\entity\animation\HurtAnimation;
@@ -38,7 +36,6 @@ use pocketmine\event\entity\EntityDamageByChildEntityEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDeathEvent;
-use pocketmine\event\entity\EntityFrostWalkerEvent;
 use pocketmine\inventory\ArmorInventory;
 use pocketmine\inventory\CallbackInventoryListener;
 use pocketmine\inventory\Inventory;
@@ -715,39 +712,7 @@ abstract class Living extends Entity{
 	}
 
 	protected function applyFrostWalker(int $level) : void{
-		$radius = $level + 2;
-		$world = $this->getWorld();
-
-		$baseX = $this->location->getFloorX();
-		$y = $this->location->getFloorY() - 1;
-		$baseZ = $this->location->getFloorZ();
-
-		$liquid = VanillaBlocks::WATER();
-		$targetBlock = VanillaBlocks::FROSTED_ICE();
-		if(EntityFrostWalkerEvent::hasHandlers()){
-			$ev = new EntityFrostWalkerEvent($this, $radius, $liquid, $targetBlock);
-			$ev->call();
-			if($ev->isCancelled()){
-				return;
-			}
-			$radius = $ev->getRadius();
-			$liquid = $ev->getLiquid();
-			$targetBlock = $ev->getTargetBlock();
-		}
-
-		for($x = $baseX - $radius; $x <= $baseX + $radius; $x++){
-			for($z = $baseZ - $radius; $z <= $baseZ + $radius; $z++){
-				$block = $world->getBlockAt($x, $y, $z);
-				if(
-					!$block->isSameState($liquid) ||
-					$world->getBlockAt($x, $y + 1, $z)->getTypeId() !== BlockTypeIds::AIR ||
-					count($world->getNearbyEntities(AxisAlignedBB::one()->offset($x, $y, $z))) !== 0
-				){
-					continue;
-				}
-				$world->setBlockAt($x, $y, $z, $targetBlock);
-			}
-		}
+		// 氷渡りを使えないように
 	}
 
 	public function getFrostWalkerLevel() : int{
