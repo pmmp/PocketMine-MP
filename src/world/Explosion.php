e2600f5f9522bcb4105824e50de3596afa359bf5
Wraith <wraith0x1@gmail.com>
2025-10-06 14:14:38 +0100

    Update src/world/Explosion.php
    Co-authored-by: Dylan T. <odigiman@gmail.com>

--- src/world/Explosion.php
+++ src/world/Explosion.php
@@ -277,7 +277,7 @@ class Explosion{
 	public function getExposure(World $world, Vector3 $origin, Entity $entity) : float{
 		$bb = $entity->getBoundingBox();
 
-		$diff = new Vector3($bb->getXLength(), $bb->getYLength(), $bb->getZLength())->multiply(2)->add(1, 1, 1);
+		$diff = (new Vector3($bb->getXLength(), $bb->getYLength(), $bb->getZLength()))->multiply(2)->add(1, 1, 1);
 		$step = new Vector3(1.0 / $diff->x, 1.0 / $diff->y, 1.0 / $diff->z);
 
 		$xOffset = (1.0 - floor($diff->x) / $diff->x) / 2.0;
