69b2b167bfe3c744c5ada5b4b0f634e7914a3dba
Renz <renzaja11@gmail.com>
2025-10-09 23:05:00 +0700

    Add unit tests for StringToItemParser and fix alias override

--- tests/phpunit/item/StringToItemParserTest.php
+++ tests/phpunit/item/StringToItemParserTest.php
@@ -5,7 +5,7 @@
  *  ____            _        _   __  __ _                  __  __ ____
  * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | |  __/_____| |  | |  __/
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
  * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,6 +16,7 @@
  * @author PocketMine Team
  * @link http://www.pocketmine.net/
  *
+ *
  */
 
 declare(strict_types=1);
@@ -23,8 +24,6 @@ declare(strict_types=1);
 namespace pocketmine\item;
 
 use PHPUnit\Framework\TestCase;
-use pocketmine\item\VanillaItems;
-use pocketmine\item\StringToItemParser;
 
 class StringToItemParserTest extends TestCase{
 
