e1f3cd60725bb03f209ead42ec69f89311d5f1b1
Remminiscent <fadijalal20@gmail.com>
2025-10-19 19:27:55 +0300

    use floor methods

--- src/block/TallGrass.php
+++ src/block/TallGrass.php
@@ -54,7 +54,7 @@ class TallGrass extends Flowable{
 	public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null, array &$returnedItems = []) : bool{
 		$world = $this->position->getWorld();
 		$upPos = $this->position->getSide(Facing::UP);
-		if(!$world->isInWorld($upPos->x, $upPos->y, $upPos->z) || $this->getSide(Facing::UP)->getTypeId() !== BlockTypeIds::AIR){
+		if(!$world->isInWorld($upPos->getFloorX(), $upPos->getFloorY(), $upPos->getFloorZ()) || $this->getSide(Facing::UP)->getTypeId() !== BlockTypeIds::AIR){
 			return false;
 		}
 
