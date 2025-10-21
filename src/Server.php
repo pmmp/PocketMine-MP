fbb305776924e2a651ac2280f35a82f7fba67244
Remminiscent <fadijalal20@gmail.com>
2025-10-21 19:12:59 +0300

    bruh

--- src/Server.php
+++ src/Server.php
@@ -1504,7 +1504,7 @@ class Server{
 			if(TimingsHandler::isEnabled() && (($sender = $this->consoleSender) !== null)){
 				TimingsHandler::createReportFile($sender)->onCompletion(
 					function(string $timingsFile) use ($sender) : void{
-						Command::broadcastCommandMessage($sender, KnownTranslationFactory::pocketmine_command_timings_timingsWrite($timingsFile));	
+						Command::broadcastCommandMessage($sender, KnownTranslationFactory::pocketmine_command_timings_timingsWrite($timingsFile));
 						TimingsHandler::setEnabled(false);
 					},
 					fn() => throw new \AssertionError("This promise is not expected to be rejected")
