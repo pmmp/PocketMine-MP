24a3f8b508c33f753877a37e916d628f6fe2e1b5
Remminiscent <fadijalal20@gmail.com>
2025-10-22 16:59:41 +0300

    cs ig

--- src/timings/TimingsHandler.php
+++ src/timings/TimingsHandler.php
@@ -390,9 +390,9 @@ class TimingsHandler{
 				$timingsFile = Path::join($filePath, $timingsName . ".txt");
 				while(file_exists($timingsFile)){
 					if(!is_file($timingsFile)){
-                    	$resolver->reject();
-                    	return;
-                	}
+						$resolver->reject();
+						return;
+					}
 					$timingsFile = Path::join($filePath, $timingsName . (++$index) . ".txt");
 				}
 				$handle = ErrorToExceptionHandler::trapAndRemoveFalse(fn() => fopen($timingsFile, "a+b"));
