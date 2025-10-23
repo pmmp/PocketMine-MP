882d9d51aa4edd1890bde240992a21224d548ed8
Valres <104681026+ValresMC@users.noreply.github.com>
2025-10-24 00:43:09 +0200

    woops, idk why this stuff changes

--- build/server-phar.php
+++ build/server-phar.php
@@ -142,7 +142,7 @@ function main() : void{
 	if(isset($opts["git"])){
 		$gitHash = $opts["git"];
 	}else{
-		$gitHash = Git::getRepositoryStatePretty(__dir__);
+		$gitHash = Git::getRepositoryStatePretty(dirname(__DIR__));
 		echo "Git hash detected as $gitHash" . PHP_EOL;
 	}
 	if(isset($opts["build"])){
