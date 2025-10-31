a8a33ea7b749efa7b95fc3c938b25c33410e48f0
Ayrz <dogrumucahit@hotmail.com>
2025-10-31 22:46:38 +0300

    Update Server.php

--- src/Server.php
+++ src/Server.php
@@ -1110,6 +1110,10 @@ class Server{
 			$this->configGroup->save();
 
 			$this->logger->info($this->language->translate(KnownTranslationFactory::pocketmine_server_defaultGameMode($this->getGamemode()->getTranslatableName())));
+<<<<<<< Updated upstream
+=======
+			// $this->logger->info($this->language->translate(KnownTranslationFactory::pocketmine_server_donate(TextFormat::AQUA . "https://patreon.com/pocketminemp" . TextFormat::RESET)));
+>>>>>>> Stashed changes
 			$this->logger->info($this->language->translate(KnownTranslationFactory::pocketmine_server_startFinished(strval(round(microtime(true) - $this->startTime, 3)))));
 
 			$forwarder = new BroadcastLoggerForwarder($this, $this->logger, $this->language);
