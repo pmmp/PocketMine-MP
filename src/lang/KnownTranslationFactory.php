ec9d372a847f02e9e52af71e68f0501e32c7bd4a
bonbionseker <167460154+bonbionseker@users.noreply.github.com>
2025-11-29 17:57:19 +0300

    Add lava chicken description translation

--- src/lang/KnownTranslationFactory.php
+++ src/lang/KnownTranslationFactory.php
@@ -1055,6 +1055,10 @@ final class KnownTranslationFactory{
 		return new Translatable(KnownTranslationKeys::ITEM_RECORD_FAR_DESC, []);
 	}
 
+	public static function item_record_lava_chicken_desc() : Translatable{
+		return new Translatable(KnownTranslationKeys::ITEM_RECORD_LAVA_CHICKEN_DESC, []);
+	}
+
 	public static function item_record_mall_desc() : Translatable{
 		return new Translatable(KnownTranslationKeys::ITEM_RECORD_MALL_DESC, []);
 	}
@@ -1073,10 +1077,6 @@ final class KnownTranslationFactory{
 
 	public static function item_record_precipice_desc() : Translatable{
 		return new Translatable(KnownTranslationKeys::ITEM_RECORD_PRECIPICE_DESC, []);
-	}
-
-	public static function item_record_lava_chicken_desc() : Translatable{
-		return new Translatable(KnownTranslationKeys::ITEM_RECORD_LAVA_CHICKEN_DESC, []);
 	}
 
 	public static function item_record_relic_desc() : Translatable{
