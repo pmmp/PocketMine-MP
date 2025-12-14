73aff4e1522ba0e5fa608ddfc53e265747505f2f
Aaa <wortelsmpofficial@gmail.com>
2025-12-15 05:28:30 +0700

    ..

--- src/entity/Entity.php
+++ src/entity/Entity.php
@@ -738,7 +738,7 @@ abstract class Entity{
 		}
 
 		$world = $this->getWorld();
-		if($world->isRaining($this->position)){
+		if($world->isRaining($this->location)){
 			$this->extinguish(EntityExtinguishEvent::CAUSE_RAIN);
 		}
 
