4e0432fe24ea44d2f1dce60b252e7dda3031da43
xRookieFight <me@xrookiefight.com>
2026-02-16 20:48:49 +0300

    PhpStorm issue

--- src/player/GameMode.php
+++ src/player/GameMode.php
@@ -80,8 +80,7 @@ enum GameMode{
 			self::SURVIVAL => ["Survival", KnownTranslationFactory::gameMode_survival(), ["survival", "s", "0"]],
 			self::CREATIVE => ["Creative", KnownTranslationFactory::gameMode_creative(), ["creative", "c", "1"]],
 			self::ADVENTURE => ["Adventure", KnownTranslationFactory::gameMode_adventure(), ["adventure", "a", "2"]],
-			self::SPECTATOR => ["Spectator", KnownTranslationFactory::gameMode_spectator(), ["spectator", "v", "view", "3"]],
-			self::NATIVE_SPECTATOR => throw new \Exception('To be implemented')
+			self::SPECTATOR, self::NATIVE_SPECTATOR => ["Spectator", KnownTranslationFactory::gameMode_spectator(), ["spectator", "v", "view", "3"]]
 		};
 	}
 
