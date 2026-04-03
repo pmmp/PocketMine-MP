2f617759625f80ce38e72cc2cf2fb6ec45be3a17
Masaki Aida <51500566+aida0710@users.noreply.github.com>
2026-04-03 10:29:41 +0900

    バージョン情報を更新し、開発ビルドを無効にしました

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -30,9 +30,9 @@ use function is_int;
 use function str_repeat;
 
 final class VersionInfo{
-	public const NAME = "PocketMine-MP";
+	public const NAME = "Ymg-PocketMine-MP";
 	public const BASE_VERSION = "5.42.1";
-	public const IS_DEVELOPMENT_BUILD = true;
+	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 	public const GITHUB_URL = "https://github.com/pmmp/PocketMine-MP";
 
