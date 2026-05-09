83ebbb9d4cb31a50ce37453abb68b0076200adfc
Dylan T. <dktapps@pmmp.io>
2026-05-09 16:22:07 +0100

    Prepare 5.43.0 release

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,8 +31,8 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "PocketMine-MP";
-	public const BASE_VERSION = "5.42.2";
-	public const IS_DEVELOPMENT_BUILD = true;
+	public const BASE_VERSION = "5.43.0";
+	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 	public const GITHUB_URL = "https://github.com/pmmp/PocketMine-MP";
 
